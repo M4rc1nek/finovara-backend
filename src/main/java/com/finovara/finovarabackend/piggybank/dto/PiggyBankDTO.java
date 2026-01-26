ae3567676c4e63350e027d296e57c0ae526ad3d4
M4rc1nekDev <developer@op.pl>
2026-01-26 12:13:05 +0100

    added: piggy bank name validation

--- src/main/java/com/finovara/finovarabackend/piggybank/dto/PiggyBankDTO.java
+++ src/main/java/com/finovara/finovarabackend/piggybank/dto/PiggyBankDTO.java
@@ -1,6 +1,8 @@
 package com.finovara.finovarabackend.piggybank.dto;
 
 import com.finovara.finovarabackend.piggybank.model.GoalType;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
@@ -9,7 +11,11 @@ public record PiggyBankDTO(
         Long id,
         Long userId,
 
+        @Size(min = 3, max = 15)
+        @NotBlank
         String name,
+
+
         BigDecimal amount,
         LocalDate createdAt,
 
