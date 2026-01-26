0ab74988a30f0255bc373857d626be9b75d6b90b
M4rc1nekDev <developer@op.pl>
2026-01-26 12:11:01 +0100

    added password validation

--- src/main/java/com/finovara/finovarabackend/user/dto/UserRegisterLoginDTO.java
+++ src/main/java/com/finovara/finovarabackend/user/dto/UserRegisterLoginDTO.java
@@ -2,18 +2,24 @@ package com.finovara.finovarabackend.user.dto;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
 public record UserRegisterLoginDTO(
         Long id,
-        @Size(min = 3, max = 13) String username,
-
+        @Size(min = 3, max = 13)
+        String username,
 
         @NotBlank
+        @Size(min = 8)
+        @Pattern(
+                regexp = "^(?=.*[A-Z])(?=.*\\d)(?=.*[^a-zA-Z0-9]).*$",
+                message = "Hasło musi mieć min. 8 znaków, jedną dużą literę, jedną cyfrę i jeden znak specjalny"
+        )
         String password,
 
         @Email
+        @NotBlank
         String email,
 
         String jwtToken
