2b3234ae70457c0d5cde4845c40c9de106aa6b25
M4rc1nekDev <developer@op.pl>
2026-01-31 12:56:44 +0100

    fix: resolve circular dependency between PiggyBankService and GoalCompletionService

--- src/main/java/com/finovara/finovarabackend/util/service/piggybank/PiggyBankCheckGoalCompletion.java
+++ src/main/java/com/finovara/finovarabackend/util/service/piggybank/PiggyBankCheckGoalCompletion.java
@@ -1,4 +1,16 @@
 package com.finovara.finovarabackend.util.service.piggybank;
 
+import com.finovara.finovarabackend.piggybank.model.PiggyBank;
+import org.springframework.stereotype.Service;
+
+@Service
 public class PiggyBankCheckGoalCompletion {
+
+    public boolean isGoalCompleted(PiggyBank piggyBank) {
+        if (piggyBank.getGoalAmount() == null) {
+            return false;
+        }
+        return piggyBank.getAmount().compareTo(piggyBank.getGoalAmount()) >= 0;
+    }
+
 }
