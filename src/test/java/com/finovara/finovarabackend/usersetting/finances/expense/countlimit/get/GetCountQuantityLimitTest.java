46cdc272b06df6ba9e27577403d4d83a08fbce51
M4rc1nekDev <developer@op.pl>
2026-03-14 11:52:56 +0100

    add get count quantity limit test

---
+++ src/test/java/com/finovara/finovarabackend/usersetting/finances/expense/countlimit/get/GetCountQuantityLimitTest.java
@@ -0,0 +1,70 @@
+package com.finovara.finovarabackend.usersetting.finances.expense.countlimit.get;
+
+import com.finovara.finovarabackend.user.model.User;
+import com.finovara.finovarabackend.usersetting.finances.expense.countlimit.dto.CountQuantityLimitDto;
+import com.finovara.finovarabackend.usersetting.finances.expense.countlimit.model.CountQuantityLimitStrategy;
+import com.finovara.finovarabackend.usersetting.finances.expense.countlimit.service.CountQuantityLimitService;
+import com.finovara.finovarabackend.usersetting.finances.expense.model.ExpenseSettings;
+import com.finovara.finovarabackend.util.service.user.service.UserManagerService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class GetCountQuantityLimitTest {
+
+    @Mock
+    private UserManagerService userManagerService;
+
+    @InjectMocks
+    private CountQuantityLimitService countQuantityLimitService;
+
+    private User user;
+    private ExpenseSettings expenseSettings;
+
+    private final String EMAIL = "test@test.com";
+
+    @BeforeEach
+    void setup() {
+        user = new User();
+        user.setId(1L);
+        expenseSettings = new ExpenseSettings();
+        user.setExpenseSettings(expenseSettings);
+    }
+
+    @Test
+    void shouldReturnEnabledLimit() {
+        expenseSettings.setExpenseCountQuantityLimitEnabled(true);
+        expenseSettings.setCountQuantityLimitStrategy(CountQuantityLimitStrategy.DAILY);
+        expenseSettings.setNumberOfQuantityLimit(5);
+
+        when(userManagerService.getUserByEmailOrThrow(EMAIL)).thenReturn(user);
+
+        CountQuantityLimitDto dto = countQuantityLimitService.getCountQuantityLimit(EMAIL);
+
+        assertEquals(true, dto.expenseCountLimitEnabled());
+        assertEquals(CountQuantityLimitStrategy.DAILY, dto.countQuantityLimitStrategy());
+        assertEquals(5, dto.numberOfQuantityLimit());
+    }
+
+    @Test
+    void shouldReturnDisabledLimit() {
+        expenseSettings.setExpenseCountQuantityLimitEnabled(false);
+        expenseSettings.setCountQuantityLimitStrategy(CountQuantityLimitStrategy.WEEKLY);
+        expenseSettings.setNumberOfQuantityLimit(10);
+
+        when(userManagerService.getUserByEmailOrThrow(EMAIL)).thenReturn(user);
+
+        CountQuantityLimitDto dto = countQuantityLimitService.getCountQuantityLimit(EMAIL);
+
+        assertEquals(false, dto.expenseCountLimitEnabled());
+        assertEquals(CountQuantityLimitStrategy.WEEKLY, dto.countQuantityLimitStrategy());
+        assertEquals(10, dto.numberOfQuantityLimit());
+    }
+}
