c928ee0de64d25c4cb5a48619c049a04db679002
M4rc1nekDev <developer@op.pl>
2026-03-14 12:22:03 +0100

    add get smart scan test

---
+++ src/test/java/com/finovara/finovarabackend/usersetting/finances/expense/smartscan/service/get/GetSmartScanTest.java
@@ -0,0 +1,57 @@
+package com.finovara.finovarabackend.usersetting.finances.expense.smartscan.service.get;
+
+import com.finovara.finovarabackend.user.model.User;
+import com.finovara.finovarabackend.usersetting.finances.expense.model.ExpenseSettings;
+import com.finovara.finovarabackend.usersetting.finances.expense.smartscan.dto.SmartScanDto;
+import com.finovara.finovarabackend.usersetting.finances.expense.smartscan.service.SmartScanService;
+import com.finovara.finovarabackend.util.service.user.service.UserManagerService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class GetSmartScanTest {
+
+    @Mock
+    private UserManagerService userManagerService;
+
+    @InjectMocks
+    private SmartScanService smartScanService;
+
+    private ExpenseSettings expenseSettings;
+    private final String EMAIL = "test@test.com";
+
+    @BeforeEach
+    void setup() {
+        User user = new User();
+        expenseSettings = new ExpenseSettings();
+        user.setExpenseSettings(expenseSettings);
+
+        when(userManagerService.getUserByEmailOrThrow(EMAIL)).thenReturn(user);
+    }
+
+    @Test
+    void shouldReturnEnabledTrue() {
+        expenseSettings.setSmartScanEnabled(true);
+
+        SmartScanDto result = smartScanService.getSmartScan(EMAIL);
+
+        assertTrue(result.smartScanEnabled());
+    }
+
+    @Test
+    void shouldReturnEnabledFalse() {
+        expenseSettings.setSmartScanEnabled(false);
+
+        SmartScanDto result = smartScanService.getSmartScan(EMAIL);
+
+        assertFalse(result.smartScanEnabled());
+    }
+}
