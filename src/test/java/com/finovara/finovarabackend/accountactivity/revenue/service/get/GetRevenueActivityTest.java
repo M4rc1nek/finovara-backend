72facd5b568607b9b98ae06184d81dc7760f419e
M4rc1nekDev <developer@op.pl>
2026-03-16 10:02:01 +0100

    add get revenue activity test

---
+++ src/test/java/com/finovara/finovarabackend/accountactivity/revenue/service/get/GetRevenueActivityTest.java
@@ -0,0 +1,84 @@
+package com.finovara.finovarabackend.accountactivity.revenue.service.get;
+
+import com.finovara.finovarabackend.accountactivity.revenue.dto.RevenueActivityDto;
+import com.finovara.finovarabackend.accountactivity.revenue.mapper.RevenueActivityMapper;
+import com.finovara.finovarabackend.accountactivity.revenue.model.RevenueActivity;
+import com.finovara.finovarabackend.accountactivity.revenue.model.RevenueActivitySort;
+import com.finovara.finovarabackend.accountactivity.revenue.model.RevenueActivityType;
+import com.finovara.finovarabackend.accountactivity.revenue.repository.RevenueActivityRepository;
+import com.finovara.finovarabackend.accountactivity.revenue.service.RevenueActivityService;
+import com.finovara.finovarabackend.revenue.model.RevenueCategory;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Pageable;
+import org.springframework.test.util.ReflectionTestUtils;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class GetRevenueActivityTest {
+
+    @Mock
+    private RevenueActivityRepository revenueActivityRepository;
+    @Mock
+    private RevenueActivityMapper revenueActivityMapper;
+
+    @InjectMocks
+    private RevenueActivityService revenueActivityService;
+
+    private final String EMAIL = "test@mail.com";
+
+    @BeforeEach
+    void setUp() {
+        ReflectionTestUtils.setField(revenueActivityService, "pageSize", 10);
+    }
+
+    @Test
+    void ShouldReturnActivitiesSortedByNewest() {
+
+        RevenueActivity activity = new RevenueActivity();
+        RevenueActivityDto dto = new RevenueActivityDto(
+                RevenueActivityType.ADDED_REVENUE,
+                new BigDecimal("100"),
+                null,
+                RevenueCategory.SALARY,
+                null,
+                LocalDateTime.now()
+        );
+
+        when(revenueActivityRepository.findByUserAssignedEmail(eq(EMAIL), any(Pageable.class))).thenReturn(List.of(activity));
+
+        when(revenueActivityMapper.mapToRevenueActivity(activity)).thenReturn(dto);
+
+        List<RevenueActivityDto> result = revenueActivityService.getRevenueActivity(EMAIL, RevenueActivitySort.NEWEST);
+
+        assertEquals(1, result.size());
+        assertEquals(dto, result.getFirst());
+
+        verify(revenueActivityRepository).findByUserAssignedEmail(eq(EMAIL), any(Pageable.class));
+        verify(revenueActivityMapper).mapToRevenueActivity(activity);
+    }
+
+    @Test
+    void ShouldReturnEmptyListWhenUserHasNoActivities() {
+
+        when(revenueActivityRepository.findByUserAssignedEmail(eq(EMAIL), any(Pageable.class))).thenReturn(List.of());
+
+        List<RevenueActivityDto> result = revenueActivityService.getRevenueActivity(EMAIL, RevenueActivitySort.OLDEST);
+
+        assertEquals(0, result.size());
+
+        verify(revenueActivityRepository).findByUserAssignedEmail(eq(EMAIL), any(Pageable.class));
+        verifyNoInteractions(revenueActivityMapper);
+    }
+}
