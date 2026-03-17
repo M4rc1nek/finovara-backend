5f8e2cfc1477cec7e6a3ed85e4ddd96da49bf784
M4rc1nekDev <developer@op.pl>
2026-03-17 10:18:57 +0100

    add revenue manager test

--- src/test/java/com/finovara/finovarabackend/util/manager/service/expense/ExpenseManagerTest.java
+++ src/test/java/com/finovara/finovarabackend/util/manager/service/expense/ExpenseManagerTest.java
@@ -17,7 +17,7 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
-class ExpenseManagerServiceTest {
+class ExpenseManagerTest {
 
     @Mock
     private ExpenseRepository expenseRepository;
