ece754b9faa68b81dd689c3fa854cf9f9bac05ba
M4rc1nekDev <developer@op.pl>
2026-03-17 10:28:19 +0100

    add email template test

---
+++ src/test/java/com/finovara/finovarabackend/util/manager/service/user/accountmanagment/accountpolicy/emailtemplate/EmailTemplateTest.java
@@ -0,0 +1,43 @@
+package com.finovara.finovarabackend.util.manager.service.user.accountmanagment.accountpolicy.emailtemplate;
+
+import com.finovara.finovarabackend.user.model.User;
+import com.finovara.finovarabackend.util.service.user.accountmanagment.emailtemplate.EmailTemplateService;
+import jakarta.mail.internet.MimeMessage;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.test.util.ReflectionTestUtils;
+
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class EmailTemplateServiceTest {
+
+    @Mock
+    private JavaMailSender javaMailSender;
+
+    @InjectMocks
+    private EmailTemplateService emailTemplateService;
+
+    @Mock
+    private MimeMessage mimeMessage;
+
+    @Test
+    void shouldCallJavaMailSenderSend() {
+        ReflectionTestUtils.setField(emailTemplateService, "senderAddress", "test@mail.com");
+
+        User user = new User();
+        user.setUsername("john_doe");
+        user.setEmail("john@example.com");
+
+        when(javaMailSender.createMimeMessage()).thenReturn(mimeMessage);
+
+        emailTemplateService.sendEmail(user, "Test Subject", "email/account-deleted.html", "john_doe", "john@example.com");
+
+        verify(javaMailSender).send(mimeMessage);
+    }
+}
