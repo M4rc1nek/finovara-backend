b5736100267dff02e26713db19ac6c44747a229e
M4rc1nekDev <developer@op.pl>
2026-03-17 10:29:24 +0100

    add password change email test

---
+++ src/test/java/com/finovara/finovarabackend/util/manager/service/user/accountmanagment/accountpolicy/passwordpolicy/PasswordChangeEmailTest.java
@@ -0,0 +1,39 @@
+package com.finovara.finovarabackend.util.manager.service.user.accountmanagment.accountpolicy.passwordpolicy;
+
+import com.finovara.finovarabackend.user.model.User;
+import com.finovara.finovarabackend.util.service.user.accountmanagment.emailtemplate.EmailTemplateService;
+import com.finovara.finovarabackend.util.service.user.accountmanagment.passwordpolicy.PasswordChangeEmailService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+class PasswordChangeEmailServiceTest {
+
+    @Mock
+    private EmailTemplateService emailTemplateService;
+
+    @InjectMocks
+    private PasswordChangeEmailService passwordChangeEmailService;
+
+    @Test
+    void shouldCallSendEmailWithCorrectParameters() {
+        User user = new User();
+        user.setUsername("john_doe");
+        user.setEmail("john@example.com");
+
+        passwordChangeEmailService.sendEmail(user);
+
+        verify(emailTemplateService).sendEmail(
+                user,
+                "Finovara - Zmiana hasła",
+                "email/password-changed.html",
+                "john_doe",
+                null
+        );
+    }
+}
