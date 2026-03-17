0e68e991d5a98b7a66115c18ab0490038bb103ee
M4rc1nekDev <developer@op.pl>
2026-03-17 10:13:06 +0100

    add get client data test

---
+++ src/test/java/com/finovara/finovarabackend/util/clientdata/metadata/service/GetClientDataTest.java
@@ -0,0 +1,61 @@
+package com.finovara.finovarabackend.util.clientdata.metadata.service;
+
+import com.finovara.finovarabackend.util.clientdata.browser.UserBrowser;
+import com.finovara.finovarabackend.util.clientdata.ip.ClientIp;
+import com.finovara.finovarabackend.util.clientdata.location.UserLocation;
+import com.finovara.finovarabackend.util.clientdata.metadata.ClientData;
+import jakarta.servlet.http.HttpServletRequest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class GetClientDataTest {
+
+    @Mock
+    private UserLocation userLocation;
+
+    @Mock
+    private ClientIp clientIp;
+
+    @Mock
+    private UserBrowser userBrowser;
+
+    @InjectMocks
+    private ClientData clientData;
+
+    @Mock
+    private HttpServletRequest request;
+
+    @Test
+    void shouldReturnClientIpFromClientIpService() {
+        when(clientIp.getClientIpAddress(request)).thenReturn("192.168.1.10");
+
+        String result = clientData.getClientIp(request);
+
+        assertEquals("192.168.1.10", result);
+    }
+
+    @Test
+    void shouldReturnUserLocationFromUserLocationService() {
+        when(userLocation.getLocationFromIp("8.8.8.8")).thenReturn("Warsaw, Poland");
+
+        String result = clientData.getUserLocation("8.8.8.8");
+
+        assertEquals("Warsaw, Poland", result);
+    }
+
+    @Test
+    void shouldReturnUserBrowserFromUserBrowserService() {
+        when(userBrowser.getBrowser(request)).thenReturn("Chrome");
+
+        String result = clientData.getUserBrowser(request);
+
+        assertEquals("Chrome", result);
+    }
+}
