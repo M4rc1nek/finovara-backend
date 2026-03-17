916d59856870066f4d4ba05f2054a39d0d3a69e0
M4rc1nekDev <developer@op.pl>
2026-03-17 10:25:25 +0100

    add account deleted email test

---
+++ src/test/java/com/finovara/finovarabackend/util/manager/service/user/accountmanagment/accountpolicy/accountdeleted/AccountDeletedEmailTest.java
@@ -0,0 +1,39 @@
+package com.finovara.finovarabackend.util.manager.service.user.accountmanagment.accountpolicy.accountdeleted;
+
+import com.finovara.finovarabackend.user.model.User;
+import com.finovara.finovarabackend.util.service.user.accountmanagment.emailtemplate.EmailTemplateService;
+import com.finovara.finovarabackend.util.service.user.accountmanagment.accountpolicy.accountdeleted.AccountDeletedEmailService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+class AccountDeletedEmailTest {
+
+    @Mock
+    private EmailTemplateService emailTemplateService;
+
+    @InjectMocks
+    private AccountDeletedEmailService accountDeletedEmailService;
+
+    @Test
+    void shouldCallSendEmailWithCorrectParameters() {
+        User user = new User();
+        user.setUsername("john_doe");
+        user.setEmail("john@example.com");
+
+        accountDeletedEmailService.sendEmail(user);
+
+        verify(emailTemplateService).sendEmail(
+                user,
+                "Finovara - Usunięcie konta",
+                "email/account-deleted.html",
+                "john_doe",
+                "john@example.com"
+        );
+    }
+}
