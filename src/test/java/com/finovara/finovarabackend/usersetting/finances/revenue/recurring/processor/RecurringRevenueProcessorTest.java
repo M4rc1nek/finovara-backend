39f767cee13291aa2fcca7578059b126c3f7981c
M4rc1nekDev <developer@op.pl>
2026-03-20 13:25:13 +0100

    add reccurring revenue processor test

---
+++ src/test/java/com/finovara/finovarabackend/usersetting/finances/revenue/recurring/processor/RecurringRevenueProcessorTest.java
@@ -0,0 +1,110 @@
+package com.finovara.finovarabackend.usersetting.finances.revenue.recurring.processor;
+
+import com.finovara.finovarabackend.revenue.dto.RevenueDTO;
+import com.finovara.finovarabackend.revenue.model.RevenueCategory;
+import com.finovara.finovarabackend.revenue.service.RevenueService;
+import com.finovara.finovarabackend.user.model.User;
+import com.finovara.finovarabackend.user.repository.UserRepository;
+import com.finovara.finovarabackend.usersetting.finances.revenue.model.RevenueSettings;
+import com.finovara.finovarabackend.usersetting.finances.revenue.recurring.model.RecurringStrategy;
+import com.finovara.finovarabackend.util.service.time.SpentInPeriodService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
+
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class RecurringRevenueProcessorTest {
+
+    @Mock
+    private UserRepository userRepository;
+    @Mock
+    private SpentInPeriodService spentInPeriodService;
+    @Mock
+    private RevenueService revenueService;
+    @InjectMocks
+    private RecurringRevenueProcessor recurringRevenueProcessor;
+
+    private final String EMAIL = "test@example.com";
+
+    @Test
+    void shouldGenerateRecurringRevenuesSuccessfully() {
+        User user = new User();
+        user.setId(1L);
+        user.setEmail(EMAIL);
+
+        RevenueSettings settings = RevenueSettings.builder()
+                .recurringRevenuesEnable(true)
+                .nextExecutionDate(LocalDate.of(2026, 3, 20))
+                .recurringAmount(new BigDecimal(100))
+                .revenueCategory(RevenueCategory.SALARY)
+                .recurringStrategy(RecurringStrategy.DAILY)
+                .userAssigned(user)
+                .build();
+
+        user.setRevenueSettings(settings);
+
+        when(userRepository.findAll()).thenReturn(List.of(user));
+        when(spentInPeriodService.today()).thenReturn(LocalDate.of(2026, 3, 20));
+
+        recurringRevenueProcessor.generateRecurringRevenues();
+
+        verify(revenueService).addRevenue(
+                new RevenueDTO(
+                        null,
+                        user.getId(),
+                        new BigDecimal(100),
+                        RevenueCategory.SALARY,
+                        LocalDate.of(2026, 3, 20),
+                        "Cykliczny przychód"
+                ),
+                user.getEmail()
+        );
+    }
+
+    @Test
+    void shouldGenerateMultipleRevenuesUntilToday() {
+        User user = new User();
+        user.setId(1L);
+        user.setEmail(EMAIL);
+
+        RevenueSettings settings = RevenueSettings.builder()
+                .recurringRevenuesEnable(true)
+                .nextExecutionDate(LocalDate.of(2026, 3, 17))
+                .recurringAmount(new BigDecimal(100))
+                .revenueCategory(RevenueCategory.SALARY)
+                .recurringStrategy(RecurringStrategy.DAILY)
+                .userAssigned(user)
+                .build();
+
+        user.setRevenueSettings(settings);
+
+        when(userRepository.findAll()).thenReturn(List.of(user));
+        when(spentInPeriodService.today()).thenReturn(LocalDate.of(2026, 3, 20));
+
+        recurringRevenueProcessor.generateRecurringRevenues();
+
+        verify(revenueService, times(4)) // 17,18,19,20
+                .addRevenue(any(RevenueDTO.class), eq(user.getEmail()));
+    }
+
+    @Test
+    void shouldSkipUserWithoutSettings() {
+        User user = new User();
+        user.setEmail(EMAIL);
+
+        when(userRepository.findAll()).thenReturn(List.of(user));
+        when(spentInPeriodService.today()).thenReturn(LocalDate.now());
+
+        recurringRevenueProcessor.generateRecurringRevenues();
+
+        verify(revenueService, never()).addRevenue(any(), any());
+    }
+}
