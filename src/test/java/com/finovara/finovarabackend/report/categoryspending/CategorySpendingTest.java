c4c71df080887bd09383b64ebabbf7f3b4162b70
M4rc1nekDev <developer@op.pl>
2026-03-29 09:56:34 +0200

    add category spending test

--- src/test/java/com/finovara/finovarabackend/report/categoryspending/CategorySpendingTest.java
+++ src/test/java/com/finovara/finovarabackend/report/categoryspending/CategorySpendingTest.java
@@ -2,25 +2,27 @@ package com.finovara.finovarabackend.report.categoryspending;
 
 import com.finovara.finovarabackend.expense.model.Expense;
 import com.finovara.finovarabackend.expense.model.ExpenseCategory;
-import com.finovara.finovarabackend.expense.repository.ExpenseRepository;
 import com.finovara.finovarabackend.report.finances.categoryspending.dto.CategorySpendingDto;
 import com.finovara.finovarabackend.report.finances.categoryspending.service.CategorySpendingService;
+import com.finovara.finovarabackend.user.exception.notfound.UserNotFoundException;
 import com.finovara.finovarabackend.user.model.User;
+import com.finovara.finovarabackend.util.model.PeriodType;
+import com.finovara.finovarabackend.util.service.periodbalance.FinancialPeriodService;
 import com.finovara.finovarabackend.util.service.user.service.UserManagerService;
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.math.BigDecimal;
-import java.time.LocalDate;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -30,10 +32,69 @@ class CategorySpendingTest {
     private UserManagerService userManagerService;
 
     @Mock
-    private ExpenseRepository expenseRepository;
+    private FinancialPeriodService financialPeriodService;
 
     @InjectMocks
     private CategorySpendingService categorySpendingService;
 
-    private User user;
+    private String email;
+
+    @BeforeEach
+    void setUp() {
+        email = "test@email.com";
+    }
+
+    @ParameterizedTest
+    @EnumSource(PeriodType.class)
+    void shouldGetCategorySpendingReport(PeriodType periodType) {
+        User user = new User();
+        user.setId(1L);
+
+        BigDecimal summedExpenses = BigDecimal.valueOf(100);
+
+        Expense expense = new Expense();
+        expense.setAmount(BigDecimal.valueOf(20));
+
+        Expense expense2 = new Expense();
+        expense2.setAmount(BigDecimal.valueOf(30));
+
+        List<Expense> expenseCategory = List.of(expense, expense2);
+
+        when(userManagerService.getUserByEmailOrThrow(email)).thenReturn(user);
+        when(financialPeriodService.getSpent(user.getId(), periodType)).thenReturn(summedExpenses);
+        when(financialPeriodService.getExpensesInPeriodByCategory(user.getId(), periodType, ExpenseCategory.CLOTHING))
+                .thenReturn(expenseCategory);
+
+        CategorySpendingDto result = categorySpendingService.getCategorySpendingReport(email, ExpenseCategory.CLOTHING, periodType);
+
+        assertThat(result.percentage()).isEqualByComparingTo(BigDecimal.valueOf(50));
+        assertThat(result.category()).isEqualTo(ExpenseCategory.CLOTHING);
+    }
+
+    @ParameterizedTest
+    @EnumSource(PeriodType.class)
+    void shouldReturnZeroPercentageWhenExpensesIsZero(PeriodType periodType) {
+        User user = new User();
+        user.setId(1L);
+
+        when(userManagerService.getUserByEmailOrThrow(email)).thenReturn(user);
+        when(financialPeriodService.getSpent(user.getId(), periodType)).thenReturn(BigDecimal.ZERO);
+
+        when(financialPeriodService.getExpensesInPeriodByCategory(user.getId(), periodType, ExpenseCategory.CLOTHING))
+                .thenReturn(List.of());
+
+        CategorySpendingDto result = categorySpendingService.getCategorySpendingReport(email, ExpenseCategory.CLOTHING, periodType);
+
+        assertThat(result.percentage()).isEqualByComparingTo(BigDecimal.ZERO);
+    }
+
+    @Test
+    void shouldThrowExceptionWhenUserDoesNotExist() {
+
+        when(userManagerService.getUserByEmailOrThrow(email)).thenThrow(new UserNotFoundException("User not found"));
+
+        assertThrows(UserNotFoundException.class, () ->
+                categorySpendingService.getCategorySpendingReport(email, ExpenseCategory.FOOD, PeriodType.MONTHLY));
+    }
+
 }
