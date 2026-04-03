679da3b6a09f3ecf2b89d4e539c87088aed999d5
M4rc1nekDev <developer@op.pl>
2026-04-03 11:55:43 +0200

    reports average revenue  test improvement

--- src/test/java/com/finovara/finovarabackend/report/finances/service/average/ReportsAverageRevenueTest.java
+++ src/test/java/com/finovara/finovarabackend/report/finances/service/average/ReportsAverageRevenueTest.java
@@ -1,12 +1,9 @@
 package com.finovara.finovarabackend.report.finances.service.average;
 
-import com.finovara.finovarabackend.revenue.model.Revenue;
-import com.finovara.finovarabackend.revenue.repository.RevenueRepository;
 import com.finovara.finovarabackend.report.dto.ReportDto;
 import com.finovara.finovarabackend.report.finances.average.service.ReportAverageService;
 import com.finovara.finovarabackend.util.model.PeriodType;
 import com.finovara.finovarabackend.util.service.periodbalance.FinancialPeriodService;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.EnumSource;
@@ -15,7 +12,6 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.math.BigDecimal;
-import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
@@ -26,42 +22,32 @@ class ReportsAverageRevenueTest {
     @Mock
     private FinancialPeriodService financialPeriodService;
 
-    @Mock
-    private RevenueRepository revenueRepository;
-
     @InjectMocks
     private ReportAverageService reportAverageService;
 
-
     @ParameterizedTest
     @EnumSource(PeriodType.class)
-    void shouldCalculateAverageRevenue(PeriodType periodType){
+    void shouldCalculateAverageRevenue(PeriodType periodType) {
         Long userId = 1L;
-        List<Revenue> revenues = List.of(new Revenue(), new Revenue());
 
-        when(revenueRepository.findAllByUserAssignedId(userId)).thenReturn(revenues);
-        when(financialPeriodService.getRevenueSum(userId, periodType)).thenReturn(BigDecimal.valueOf(200));
+        when(financialPeriodService.getAverageRevenue(userId, periodType)).thenReturn(BigDecimal.valueOf(70));
 
         ReportDto result = reportAverageService.calculateAverageRevenue(userId, periodType);
 
-        assertThat(result.amount()).isEqualByComparingTo(BigDecimal.valueOf(100));
-
+        assertThat(result.amount()).isEqualByComparingTo(BigDecimal.valueOf(70));
         assertThat(result.periodType()).isEqualTo(periodType);
     }
 
-    @Test
-    void shouldReturnZeroWhenNoRevenues() {
+    @ParameterizedTest
+    @EnumSource(PeriodType.class)
+    void shouldReturnZeroWhenNoExpenses(PeriodType periodType) {
         Long userId = 1L;
 
-        when(revenueRepository.findAllByUserAssignedId(userId)).thenReturn(List.of());
+        when(financialPeriodService.getAverageRevenue(userId, periodType)).thenReturn(BigDecimal.ZERO);
 
-        when(financialPeriodService.getRevenueSum(userId, PeriodType.DAILY)).thenReturn(BigDecimal.ZERO);
-
-        ReportDto result = reportAverageService.calculateAverageRevenue(userId, PeriodType.DAILY);
+        ReportDto result = reportAverageService.calculateAverageRevenue(userId, periodType);
 
         assertThat(result.amount()).isEqualByComparingTo(BigDecimal.ZERO);
-
-        assertThat(result.periodType()).isEqualTo(PeriodType.DAILY);
     }
 
 }
