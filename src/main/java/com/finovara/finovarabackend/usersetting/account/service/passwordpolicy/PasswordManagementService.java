2ac9a6a92c93afbde245f7caa8a93098fb3e39dd
M4rc1nekDev <developer@op.pl>
2026-04-06 08:06:21 +0200

    add password management service

---
+++ src/main/java/com/finovara/finovarabackend/usersetting/account/service/passwordpolicy/PasswordManagementService.java
@@ -0,0 +1,33 @@
+package com.finovara.finovarabackend.usersetting.account.service.passwordpolicy;
+
+import com.finovara.finovarabackend.accountactivity.accountchange.activities.model.AccountChangesActivityType;
+import com.finovara.finovarabackend.accountactivity.accountchange.activities.service.AccountChangesActivityService;
+import com.finovara.finovarabackend.user.model.User;
+import com.finovara.finovarabackend.user.repository.UserRepository;
+import com.finovara.finovarabackend.usersetting.notification.passwordchange.service.NotifyPasswordChangeService;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class PasswordManagementService {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final AccountChangesActivityService accountChangesActivityService;
+    private final NotifyPasswordChangeService notifyPasswordChangeService;
+
+    public void updatePassword(User user, String newPassword, HttpServletRequest request) {
+        user.setPassword(passwordEncoder.encode(newPassword));
+        userRepository.save(user);
+
+        createActivity(user, request);
+
+        notifyPasswordChangeService.sendEmailOnPasswordChange(user);
+    }
+
+    private void createActivity(User user, HttpServletRequest request) {
+        accountChangesActivityService.createAccountChangesActivity(user.getEmail(), AccountChangesActivityType.PASSWORD_CHANGED, request);
+    }
+}
