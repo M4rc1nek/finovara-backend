17ff494365a891ff8a2d64d3841bcae27d713743
M4rc1nekDev <developer@op.pl>
2026-04-09 07:25:01 +0200

    add NotifyUsernameChangeService test

---
+++ src/test/java/com/finovara/finovarabackend/usersetting/notificationemail/usernamechange/service/sendemail/NotifyUsernameChangeServiceTest.java
@@ -0,0 +1,35 @@
+package com.finovara.finovarabackend.usersetting.notificationemail.usernamechange.service.sendemail;
+
+import com.finovara.finovarabackend.user.model.User;
+import com.finovara.finovarabackend.usersetting.account.service.passwordpolicy.util.NotificationEmailSender;
+import com.finovara.finovarabackend.usersetting.notificationemail.usernamechange.service.NotifyUsernameChangeService;
+import com.finovara.finovarabackend.util.user.accountmanagment.usernamepolicy.UsernameChangeEmailService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+class NotifyUsernameChangeServiceTest {
+    @Mock
+    private NotificationEmailSender notificationEmailSender;
+
+    @Mock
+    private UsernameChangeEmailService usernameChangeEmailService;
+
+    @InjectMocks
+    private NotifyUsernameChangeService notifyUsernameChangeService;
+
+    @Test
+    void shouldCallSenderToSendEmail() {
+        User user = new User();
+        notifyUsernameChangeService.sendEmailOnUsernameChange(user);
+        verify(notificationEmailSender).sendIfEnabled(eq(user), any(), any());
+    }
+
+}
