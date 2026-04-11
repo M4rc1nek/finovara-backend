cfe4483bf204c38f49c83d6c44b3c9509a6b6419
M4rc1nekDev <developer@op.pl>
2026-04-11 17:46:30 +0200

    add JsonTypeName annotation to LimitNotificationDto

--- src/main/java/com/finovara/finovarabackend/notification/dto/limit/LimitNotificationDto.java
+++ src/main/java/com/finovara/finovarabackend/notification/dto/limit/LimitNotificationDto.java
@@ -1,5 +1,6 @@
 package com.finovara.finovarabackend.notification.dto.limit;
 
+import com.fasterxml.jackson.annotation.JsonTypeName;
 import com.finovara.finovarabackend.notification.dto.NotificationResponse;
 import com.finovara.finovarabackend.notification.model.NotificationType;
 import com.finovara.finovarabackend.util.model.PeriodType;
@@ -7,9 +8,11 @@ import com.finovara.finovarabackend.util.model.PeriodType;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
+@JsonTypeName("LIMIT_EXCEEDED_WARNING")
 public record LimitNotificationDto(
         NotificationType type,
         LocalDate createdAt,
         BigDecimal limitPercentage,
         PeriodType limitPeriodType
-) implements NotificationResponse {}
+) implements NotificationResponse {
+}
