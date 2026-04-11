ca35c5147da1bc7f49aa84c997b6afcf7e9d695a
M4rc1nekDev <developer@op.pl>
2026-04-11 17:44:49 +0200

    add polymorphic JSON support for NotificationResponse

--- src/main/java/com/finovara/finovarabackend/notification/dto/NotificationResponse.java
+++ src/main/java/com/finovara/finovarabackend/notification/dto/NotificationResponse.java
@@ -1,9 +1,17 @@
 package com.finovara.finovarabackend.notification.dto;
 
+import com.finovara.finovarabackend.notification.dto.limit.LimitNotificationDto;
 import com.finovara.finovarabackend.notification.model.NotificationType;
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
 
 import java.time.LocalDate;
 
+
+@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.PROPERTY, property = "type")
+@JsonSubTypes({
+        @JsonSubTypes.Type(value = LimitNotificationDto.class, name = "LIMIT_EXCEEDED_WARNING")
+})
 public interface NotificationResponse {
     NotificationType type();
 
