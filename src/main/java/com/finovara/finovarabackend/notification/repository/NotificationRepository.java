c71b7ec82d90115b4c1702ca889d44a02e078c43
M4rc1nekDev <developer@op.pl>
2026-04-15 15:25:08 +0200

    add repository annotation in NotificationRepository

--- src/main/java/com/finovara/finovarabackend/notification/repository/NotificationRepository.java
+++ src/main/java/com/finovara/finovarabackend/notification/repository/NotificationRepository.java
@@ -3,10 +3,12 @@ package com.finovara.finovarabackend.notification.repository;
 import com.finovara.finovarabackend.notification.model.Notification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Set;
 
+@Repository
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
 
     @Query("SELECT n FROM Notification n WHERE n.userAssigned.id = :userId")
