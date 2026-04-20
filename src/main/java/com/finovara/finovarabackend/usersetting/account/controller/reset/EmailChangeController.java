04e99cadd474d17f0ebf4d5fc2034ab2e6feb69f
M4rc1nekDev <developer@op.pl>
2026-04-20 18:01:27 +0200

    rename controller EmailResetController to EmailChangeController

--- src/main/java/com/finovara/finovarabackend/usersetting/account/controller/reset/EmailChangeController.java
+++ src/main/java/com/finovara/finovarabackend/usersetting/account/controller/reset/EmailChangeController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/email-change")
 @RequiredArgsConstructor
-public class EmailResetController {
+public class EmailChangeController {
 
     private final EmailChangeService emailChangeService;
 
