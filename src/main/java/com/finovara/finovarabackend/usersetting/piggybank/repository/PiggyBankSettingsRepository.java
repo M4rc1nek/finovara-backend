9936e885370dff6c696db305f9026ad3d69ecb29
M4rc1nekDev <developer@op.pl>
2026-04-21 09:38:24 +0200

    add Repository annotation to PiggyBankSettingsRepository

--- src/main/java/com/finovara/finovarabackend/usersetting/piggybank/repository/PiggyBankSettingsRepository.java
+++ src/main/java/com/finovara/finovarabackend/usersetting/piggybank/repository/PiggyBankSettingsRepository.java
@@ -2,7 +2,9 @@ package com.finovara.finovarabackend.usersetting.piggybank.repository;
 
 import com.finovara.finovarabackend.usersetting.piggybank.model.PiggyBankSettings;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PiggyBankSettingsRepository extends JpaRepository<PiggyBankSettings, Long> {
 
 }
