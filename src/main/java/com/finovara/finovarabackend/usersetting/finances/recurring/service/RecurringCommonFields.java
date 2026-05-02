af7e56d3c8f6a6ce94260132ac700263b84703fe
M4rc1nekDev <developer@op.pl>
2026-05-02 10:34:48 +0200

    add validation metadata to recurring common fields

--- src/main/java/com/finovara/finovarabackend/usersetting/finances/recurring/service/RecurringCommonFields.java
+++ src/main/java/com/finovara/finovarabackend/usersetting/finances/recurring/service/RecurringCommonFields.java
@@ -1,14 +1,18 @@
 package com.finovara.finovarabackend.usersetting.finances.recurring.service;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.finovara.finovarabackend.util.model.PeriodType;
+import jakarta.validation.constraints.DecimalMax;
+import jakarta.validation.constraints.DecimalMin;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
 public record RecurringCommonFields(
         Boolean enable,
-        BigDecimal amount,
+        @DecimalMin("1") @DecimalMax("5000000") BigDecimal amount,
         PeriodType periodType,
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
         LocalDate startDate
 ) {
 }
