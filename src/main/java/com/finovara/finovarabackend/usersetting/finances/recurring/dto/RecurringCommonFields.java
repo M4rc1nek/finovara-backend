9f83439f391872572912bf12ef7cfb6e37d8e02e
M4rc1nekDev <developer@op.pl>
2026-05-02 12:24:00 +0200

    add NotNull annotation to periodType in RecurringCommonFields

--- src/main/java/com/finovara/finovarabackend/usersetting/finances/recurring/dto/RecurringCommonFields.java
+++ src/main/java/com/finovara/finovarabackend/usersetting/finances/recurring/dto/RecurringCommonFields.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import com.finovara.finovarabackend.util.model.PeriodType;
 import jakarta.validation.constraints.DecimalMax;
 import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotNull;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
@@ -11,7 +12,7 @@ import java.time.LocalDate;
 public record RecurringCommonFields(
         Boolean enable,
         @DecimalMin("1") @DecimalMax("5000000") BigDecimal amount,
-        PeriodType periodType,
+        @NotNull PeriodType periodType,
         @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
         LocalDate startDate
 ) {
