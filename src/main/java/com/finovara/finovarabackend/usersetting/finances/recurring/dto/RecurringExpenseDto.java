3e972491020b58b39f0a72a290ce09f78344e22f
M4rc1nekDev <developer@op.pl>
2026-05-02 10:34:39 +0200

    add validation metadata to recurring expense dto

--- src/main/java/com/finovara/finovarabackend/usersetting/finances/recurring/dto/RecurringExpenseDto.java
+++ src/main/java/com/finovara/finovarabackend/usersetting/finances/recurring/dto/RecurringExpenseDto.java
@@ -1,20 +1,21 @@
 package com.finovara.finovarabackend.usersetting.finances.recurring.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.finovara.finovarabackend.expense.model.ExpenseCategory;
 import com.finovara.finovarabackend.util.model.PeriodType;
 import jakarta.validation.constraints.DecimalMax;
 import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.NotNull;
-import org.springframework.boot.context.properties.bind.DefaultValue;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
 public record RecurringExpenseDto(
         Boolean enable,
-        @DecimalMin("1") @DecimalMax("5000000") @DefaultValue("1") BigDecimal amount,
+        @DecimalMin("1") @DecimalMax("5000000") BigDecimal amount,
         ExpenseCategory expenseCategory,
         @NotNull PeriodType periodType,
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
         LocalDate startDate,
         LocalDate nextExecutionDate
 ) {
