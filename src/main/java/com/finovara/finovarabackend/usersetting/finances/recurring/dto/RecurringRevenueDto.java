72844af0678b0bcc825738930b648cf127b0d0f3
M4rc1nekDev <developer@op.pl>
2026-05-02 10:34:43 +0200

    add validation metadata to recurring revenue dto

--- src/main/java/com/finovara/finovarabackend/usersetting/finances/recurring/dto/RecurringRevenueDto.java
+++ src/main/java/com/finovara/finovarabackend/usersetting/finances/recurring/dto/RecurringRevenueDto.java
@@ -1,20 +1,21 @@
 package com.finovara.finovarabackend.usersetting.finances.recurring.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.finovara.finovarabackend.revenue.model.RevenueCategory;
 import com.finovara.finovarabackend.util.model.PeriodType;
 import jakarta.validation.constraints.DecimalMax;
 import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.NotNull;
-import org.springframework.boot.context.properties.bind.DefaultValue;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
 public record RecurringRevenueDto(
         Boolean enable,
-        @DecimalMin("1") @DecimalMax("5000000") @DefaultValue("1") BigDecimal amount,
+        @DecimalMin("1") @DecimalMax("5000000") BigDecimal amount,
         RevenueCategory revenueCategory,
         @NotNull PeriodType periodType,
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
         LocalDate startDate,
         LocalDate nextExecutionDate
 
