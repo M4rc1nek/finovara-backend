ec99c8bcf73b96ccf6f412afc97bcb435d13901c
M4rc1nekDev <developer@op.pl>
2026-05-05 16:24:17 +0200

    add nbp exchange rates client

---
+++ src/main/java/com/finovara/finovarabackend/nbpintegration/dto/NbpTableDto.java
@@ -0,0 +1,33 @@
+package com.finovara.finovarabackend.nbpintegration.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.List;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record NbpTableDto(
+        @JsonProperty("table")
+        String tableType,
+
+        @JsonProperty("no")
+        String tableNumber,
+
+        @JsonProperty("effectiveDate")
+        String publishDate,
+        @JsonProperty("rates")
+        List<Rate> rates
+) {
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public record Rate(
+            @JsonProperty("currency")
+            String name,
+
+            @JsonProperty("code")
+            String currencyCode,
+
+            @JsonProperty("mid")
+            double averageRate
+    ) {
+    }
+}
