b3cc265fda1036f94d5b649e6ee9135518f4c7d7
Marcin Parśniak <marcinekpar92@op.pl>
2026-05-08 14:21:02 +0200

    add pdf report type model

---
+++ src/main/java/com/finovara/finovarabackend/pdfexport/report/model/PdfReportType.java
@@ -0,0 +1,10 @@
+package com.finovara.finovarabackend.pdfexport.report.model;
+
+public enum PdfReportType {
+    SUM_FINANCES,
+    AVERAGE_FINANCES,
+    HIGHEST_EXPENSES,
+    HIGHEST_REVENUES,
+    PERCENTAGE_OF_EXPENSES,
+    PERCENTAGE_OF_REVENUES
+}
