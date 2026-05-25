50aeed8c691a0c03b5324774b8ee0fed495e8345
Marcin Parśniak <marcinekpar92@op.pl>
2026-05-25 20:52:11 +0200

    add security activity core

---
+++ activity-log-backend/src/main/java/com/finovara/activityservice/activity_log/accountactivity/secure/core/SecurityActivityCore.java
@@ -0,0 +1,34 @@
+package com.finovara.activityservice.activity_log.accountactivity.secure.core;
+
+import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+public abstract class SecurityActivityCore<A, AR> {
+
+    protected abstract void saveActivity(A activity);
+
+    protected abstract long countActivities(Long userId);
+
+    protected abstract List<A> findActivitiesToArchive(Long userId, int pageSize);
+
+    protected abstract AR mapToArchive(A activity);
+
+    protected abstract void archive(List<AR> archives);
+
+    protected abstract void deleteActivities(List<A> activities);
+
+    protected void moveToArchive(Long userId, int pageSize) {
+        long count = countActivities(userId);
+
+        if (count > pageSize) {
+            List<A> toMove = findActivitiesToArchive(userId, pageSize);
+            List<AR> archives = toMove.stream().map(this::mapToArchive).toList();
+
+            archive(archives);
+            deleteActivities(toMove);
+        }
+    }
+}
+
