00382c133adf1c32d221f65de1b487715ffb71e4
Marcin Parśniak <marcinekpar92@op.pl>
2026-05-25 20:54:37 +0200

    extract account change activity log from core backend to activity log microservice

--- core-backend/src/main/java/com/finovara/finovarabackend/accountactivity/secure/accountchange/activity/model/AccountChangesActivityType.java
+++
@@ -1,9 +0,0 @@
-package com.finovara.finovarabackend.accountactivity.secure.accountchange.activity.model;
-
-public enum AccountChangesActivityType {
-    PASSWORD_CHANGED,
-    USERNAME_CHANGED,
-    EMAIL_CHANGED,
-    PROFILE_IMG_CHANGED,
-    PROFILE_IMG_DELETED,
-}
