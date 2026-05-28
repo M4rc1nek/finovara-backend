81af85cb948d0abb4b887717021ab5c8f5e72e1b
Marcin Parśniak <marcinekpar92@op.pl>
2026-05-28 08:32:13 +0200

    update notify email change service test

--- core-backend/src/test/java/com/finovara/corebackend/usersetting/notificationemail/action/emailchange/service/NotifyEmailChangeServiceTest.java
+++ core-backend/src/test/java/com/finovara/corebackend/usersetting/notificationemail/action/emailchange/service/NotifyEmailChangeServiceTest.java
@@ -1,7 +1,7 @@
 package com.finovara.corebackend.usersetting.notificationemail.action.emailchange.service;
 
-import com.finovara.activityservice.contracts.model.activity.SettingActivityStatus;
-import com.finovara.activityservice.contracts.event.settings.SettingsActivityEvent;
+import com.finovara.contracts.model.activity.SettingActivityStatus;
+import com.finovara.contracts.event.settings.SettingsActivityEvent;
 import com.finovara.corebackend.user.model.User;
 import com.finovara.corebackend.usersetting.notificationemail.dto.NotificationEmailDto;
 import com.finovara.corebackend.usersetting.notificationemail.model.NotificationEmailSettings;
