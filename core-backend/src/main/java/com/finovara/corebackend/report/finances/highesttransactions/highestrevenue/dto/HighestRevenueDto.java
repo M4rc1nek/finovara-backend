0c51d0aadaa6c09e32e8645a58e09a34df95c016
Marcin Parśniak <marcinekpar92@op.pl>
2026-05-28 08:26:23 +0200

    update highest revenue dto

--- core-backend/src/main/java/com/finovara/corebackend/report/finances/highesttransactions/highestrevenue/dto/HighestRevenueDto.java
+++ core-backend/src/main/java/com/finovara/corebackend/report/finances/highesttransactions/highestrevenue/dto/HighestRevenueDto.java
@@ -1,6 +1,6 @@
 package com.finovara.corebackend.report.finances.highesttransactions.highestrevenue.dto;
 
-import com.finovara.activityservice.contracts.model.transaction.RevenueCategory;
+import com.finovara.contracts.model.transaction.RevenueCategory;
 
 import java.math.BigDecimal;
 
