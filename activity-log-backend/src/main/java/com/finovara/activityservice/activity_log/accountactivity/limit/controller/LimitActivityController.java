c236f1603df991d502d8c086fe4ffb2c40fff275
Marcin Parśniak <marcinekpar92@op.pl>
2026-05-28 08:06:29 +0200

    update limit activity controller

--- activity-log-backend/src/main/java/com/finovara/activityservice/activity_log/accountactivity/limit/controller/LimitActivityController.java
+++ activity-log-backend/src/main/java/com/finovara/activityservice/activity_log/accountactivity/limit/controller/LimitActivityController.java
@@ -3,7 +3,7 @@ package com.finovara.activityservice.activity_log.accountactivity.limit.controll
 import com.finovara.activityservice.activity_log.accountactivity.limit.dto.LimitActivityDto;
 import com.finovara.activityservice.activity_log.accountactivity.limit.service.LimitActivityService;
 import com.finovara.activityservice.security.SecurityUtils;
-import com.finovara.activityservice.contracts.model.SortType;
+import com.finovara.contracts.model.SortType;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
