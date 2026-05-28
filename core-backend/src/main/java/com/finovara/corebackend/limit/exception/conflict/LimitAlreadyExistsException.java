9b5314d82550f4bf10ef16d4cbaed00030a9ea70
Marcin Parśniak <marcinekpar92@op.pl>
2026-05-28 08:25:56 +0200

    remove limit already exists exception from core-backend

--- core-backend/src/main/java/com/finovara/corebackend/limit/exception/conflict/LimitAlreadyExistsException.java
+++
@@ -1,7 +0,0 @@
-package com.finovara.corebackend.limit.exception.conflict;
-
-public class LimitAlreadyExistsException extends RuntimeException {
-    public LimitAlreadyExistsException(String message) {
-        super(message);
-    }
-}
