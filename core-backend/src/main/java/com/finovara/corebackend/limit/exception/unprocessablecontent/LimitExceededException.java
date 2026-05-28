5e99c2fe53305acf0a80b1809b8a65ff8a98a42a
Marcin Parśniak <marcinekpar92@op.pl>
2026-05-28 08:25:57 +0200

    remove limit exceeded exception from core-backend

--- core-backend/src/main/java/com/finovara/corebackend/limit/exception/unprocessablecontent/LimitExceededException.java
+++
@@ -1,7 +0,0 @@
-package com.finovara.corebackend.limit.exception.unprocessablecontent;
-
-public class LimitExceededException extends RuntimeException {
-    public LimitExceededException(String message) {
-        super(message);
-    }
-}
