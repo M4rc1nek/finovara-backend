fddb0da7570d9e62945cabbd7b9379eefb1bcd63
Marcin Parśniak <marcinekpar92@op.pl>
2026-05-28 08:26:06 +0200

    update report pdf handler

--- core-backend/src/main/java/com/finovara/corebackend/pdfexport/report/service/ReportPdfHandler.java
+++ core-backend/src/main/java/com/finovara/corebackend/pdfexport/report/service/ReportPdfHandler.java
@@ -2,7 +2,7 @@ package com.finovara.corebackend.pdfexport.report.service;
 
 import com.finovara.corebackend.pdfexport.report.document.PdfReportDocument;
 import com.finovara.corebackend.pdfexport.report.model.PdfReportType;
-import com.finovara.activityservice.contracts.model.PeriodType;
+import com.finovara.contracts.model.PeriodType;
 
 import java.io.IOException;
 
