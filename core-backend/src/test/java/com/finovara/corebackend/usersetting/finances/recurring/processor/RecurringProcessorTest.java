6ba1e5dc9250423f8f1b1d48fa062e7dedf62e54
Marcin Parśniak <marcinekpar92@op.pl>
2026-05-28 08:32:07 +0200

    update recurring processor test

--- core-backend/src/test/java/com/finovara/corebackend/usersetting/finances/recurring/processor/RecurringProcessorTest.java
+++ core-backend/src/test/java/com/finovara/corebackend/usersetting/finances/recurring/processor/RecurringProcessorTest.java
@@ -4,7 +4,7 @@ import com.finovara.corebackend.user.model.User;
 import com.finovara.corebackend.usersetting.finances.recurring.model.RecurringSettings;
 import com.finovara.corebackend.usersetting.finances.recurring.repository.RecurringSettingsRepository;
 import com.finovara.corebackend.usersetting.finances.recurring.service.execution.RecurringExecutionService;
-import com.finovara.activityservice.contracts.model.PeriodType;
+import com.finovara.contracts.model.PeriodType;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
