baae7e6b60768b21501a64539e2df30538361e77
Marcin Parśniak <marcinekpar92@op.pl>
2026-05-28 08:25:45 +0200

    remove not authorized exception from core-backend

--- core-backend/src/main/java/com/finovara/corebackend/exception/forbidden/NotAuthorizedException.java
+++
@@ -1,7 +0,0 @@
-package com.finovara.corebackend.exception.forbidden;
-
-public class NotAuthorizedException extends RuntimeException {
-    public NotAuthorizedException(String message) {
-        super(message);
-    }
-}
