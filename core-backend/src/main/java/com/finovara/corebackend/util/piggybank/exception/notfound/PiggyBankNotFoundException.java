1695ec792a0a4753ed3deb6086a4e3aa630df391
Marcin Parśniak <marcinekpar92@op.pl>
2026-05-28 08:26:40 +0200

    remove piggy bank not found exception from core-backend

--- core-backend/src/main/java/com/finovara/corebackend/util/piggybank/exception/notfound/PiggyBankNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.finovara.corebackend.util.piggybank.exception.notfound;
-
-public class PiggyBankNotFoundException extends RuntimeException {
-    public PiggyBankNotFoundException(String message) {
-        super(message);
-    }
-}
