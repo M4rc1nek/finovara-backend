9aca56a82ab1214164c392847287f49ea249eaf6
Marcin Parśniak <marcinekpar92@op.pl>
2026-05-28 08:25:46 +0200

    remove service unavailable exception from core-backend

--- core-backend/src/main/java/com/finovara/corebackend/exception/serviceunavailable/ServiceUnavailableException.java
+++
@@ -1,7 +0,0 @@
-package com.finovara.corebackend.exception.serviceunavailable;
-
-public class ServiceUnavailableException extends RuntimeException {
-    public ServiceUnavailableException(String message, Exception exception) {
-        super(message, exception);
-    }
-}
