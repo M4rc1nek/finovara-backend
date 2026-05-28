44f1af512e065196455ee67f2b12a2afb941ad42
Marcin Parśniak <marcinekpar92@op.pl>
2026-05-28 08:26:05 +0200

    update pdf report controller

--- core-backend/src/main/java/com/finovara/corebackend/pdfexport/report/controller/PdfReportController.java
+++ core-backend/src/main/java/com/finovara/corebackend/pdfexport/report/controller/PdfReportController.java
@@ -2,7 +2,7 @@ package com.finovara.corebackend.pdfexport.report.controller;
 
 import com.finovara.corebackend.pdfexport.report.model.PdfReportType;
 import com.finovara.corebackend.pdfexport.report.service.PdfExecutionService;
-import com.finovara.activityservice.contracts.model.PeriodType;
+import com.finovara.contracts.model.PeriodType;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ContentDisposition;
