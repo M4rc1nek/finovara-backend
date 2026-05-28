ef88ce951acd5f4b0a35654c04cc6adf02a49195
Marcin Parśniak <marcinekpar92@op.pl>
2026-05-28 08:26:31 +0200

    update security properties

--- core-backend/src/main/java/com/finovara/corebackend/security/SecurityProperties.java
+++ core-backend/src/main/java/com/finovara/corebackend/security/SecurityProperties.java
@@ -3,13 +3,11 @@ package com.finovara.corebackend.security;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @ConfigurationProperties(prefix = "security")
-@Component
 @Getter
 @Setter
 public class SecurityProperties {
