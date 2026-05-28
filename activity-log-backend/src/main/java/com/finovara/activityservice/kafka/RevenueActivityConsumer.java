b4ca8a90492fcc3f5c10b0e744d0c56d80b36b0a
Marcin Parśniak <marcinekpar92@op.pl>
2026-05-28 08:06:49 +0200

    update revenue activity consumer

--- activity-log-backend/src/main/java/com/finovara/activityservice/kafka/RevenueActivityConsumer.java
+++ activity-log-backend/src/main/java/com/finovara/activityservice/kafka/RevenueActivityConsumer.java
@@ -1,7 +1,7 @@
 package com.finovara.activityservice.kafka;
 
 import com.finovara.activityservice.activity_log.accountactivity.revenue.service.RevenueActivityService;
-import com.finovara.activityservice.contracts.event.revenue.RevenueActivityEvent;
+import com.finovara.contracts.event.revenue.RevenueActivityEvent;
 import lombok.RequiredArgsConstructor;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
