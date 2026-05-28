5b613b3437b78e1aad3650542fb3031c7dd6e009
Marcin Parśniak <marcinekpar92@op.pl>
2026-05-28 08:06:49 +0200

    update piggy bank activity consumer

--- activity-log-backend/src/main/java/com/finovara/activityservice/kafka/PiggyBankActivityConsumer.java
+++ activity-log-backend/src/main/java/com/finovara/activityservice/kafka/PiggyBankActivityConsumer.java
@@ -1,7 +1,7 @@
 package com.finovara.activityservice.kafka;
 
 import com.finovara.activityservice.activity_log.accountactivity.piggybank.service.PiggyBankActivityService;
-import com.finovara.activityservice.contracts.event.piggybank.PiggyBankActivityEvent;
+import com.finovara.contracts.event.piggybank.PiggyBankActivityEvent;
 import lombok.RequiredArgsConstructor;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
