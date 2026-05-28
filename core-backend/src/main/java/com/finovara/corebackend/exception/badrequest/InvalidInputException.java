50084bbf00e3e2b4bd66d6863385e23b7cf94832
Marcin Parśniak <marcinekpar92@op.pl>
2026-05-28 08:23:27 +0200

    remove invalid input exception from core-backend

--- core-backend/src/main/java/com/finovara/corebackend/exception/badrequest/InvalidInputException.java
+++
@@ -1,7 +0,0 @@
-package com.finovara.corebackend.exception.badrequest;
-
-public class InvalidInputException extends RuntimeException {
-    public InvalidInputException(String message) {
-        super(message);
-    }
-}
