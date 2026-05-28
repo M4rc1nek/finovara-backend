68da10af304ce37ef3d35ba71531e59978c58f1e
Marcin Parśniak <marcinekpar92@op.pl>
2026-05-28 08:06:44 +0200

    add core backend feign client

--- activity-log-backend/src/main/java/com/finovara/activityservice/feignclient/CoreBackendClient.java
+++ activity-log-backend/src/main/java/com/finovara/activityservice/feignclient/CoreBackendClient.java
@@ -1,4 +1,14 @@
 package com.finovara.activityservice.feignclient;
 
-public class CoreBackendClient {
+import com.finovara.contracts.dto.ConfirmPasswordDto;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+
+@FeignClient(name = "core-backend", url = "${core-backend.url}")
+public interface CoreBackendClient {
+
+    @PostMapping("/internal/verify-password")
+    void verifyPassword(@RequestHeader("X-User-Id") Long userId, @RequestBody ConfirmPasswordDto dto);
 }
