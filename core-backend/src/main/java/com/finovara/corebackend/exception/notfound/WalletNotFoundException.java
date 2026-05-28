c4388178071fef03a87d8a1394e1caa554293f99
Marcin Parśniak <marcinekpar92@op.pl>
2026-05-28 08:25:45 +0200

    remove wallet not found exception from core-backend

--- core-backend/src/main/java/com/finovara/corebackend/exception/notfound/WalletNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.finovara.corebackend.exception.notfound;
-
-public class WalletNotFoundException extends RuntimeException {
-    public WalletNotFoundException(String message) {
-        super(message);
-    }
-}
