1c1de71bb54faee5d4392933837fa9261658e532
Marcin Parśniak <marcinekpar92@op.pl>
2026-05-28 08:23:28 +0200

    remove name already exists exception from core-backend

--- core-backend/src/main/java/com/finovara/corebackend/exception/conflict/NameAlreadyExistsException.java
+++
@@ -1,7 +0,0 @@
-package com.finovara.corebackend.exception.conflict;
-
-public class NameAlreadyExistsException extends RuntimeException {
-    public NameAlreadyExistsException(String message) {
-        super(message);
-    }
-}
