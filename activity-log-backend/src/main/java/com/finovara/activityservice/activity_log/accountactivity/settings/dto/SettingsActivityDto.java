1f98a46c4821c0f647dea5bebbaa11047051bf83
Marcin Parśniak <marcinekpar92@op.pl>
2026-05-28 08:06:43 +0200

    update settings activity dto

--- activity-log-backend/src/main/java/com/finovara/activityservice/activity_log/accountactivity/settings/dto/SettingsActivityDto.java
+++ activity-log-backend/src/main/java/com/finovara/activityservice/activity_log/accountactivity/settings/dto/SettingsActivityDto.java
@@ -1,7 +1,7 @@
 package com.finovara.activityservice.activity_log.accountactivity.settings.dto;
 
-import com.finovara.activityservice.contracts.model.activity.SettingActivityStatus;
-import com.finovara.activityservice.contracts.model.activity.SettingType;
+import com.finovara.contracts.model.activity.SettingActivityStatus;
+import com.finovara.contracts.model.activity.SettingType;
 
 import java.time.LocalDateTime;
 
