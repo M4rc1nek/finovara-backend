b90c9808084e49218c0c9047eab2e0b8b913bdfd
Marcin Parśniak <marcinekpar92@op.pl>
2026-05-28 08:23:29 +0200

    remove state conflict exception from core-backend

--- core-backend/src/main/java/com/finovara/corebackend/exception/conflict/StateConflictException.java
+++
@@ -1,7 +0,0 @@
-package com.finovara.corebackend.exception.conflict;
-
-public class StateConflictException extends RuntimeException {
-    public  StateConflictException(String message) {
-        super(message);
-    }
-}
