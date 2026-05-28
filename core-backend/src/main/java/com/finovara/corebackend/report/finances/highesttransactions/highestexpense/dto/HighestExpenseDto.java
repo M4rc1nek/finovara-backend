470545cf446a47c30078e7d9cc1cc469989c314d
Marcin Parśniak <marcinekpar92@op.pl>
2026-05-28 08:26:22 +0200

    update highest expense dto

--- core-backend/src/main/java/com/finovara/corebackend/report/finances/highesttransactions/highestexpense/dto/HighestExpenseDto.java
+++ core-backend/src/main/java/com/finovara/corebackend/report/finances/highesttransactions/highestexpense/dto/HighestExpenseDto.java
@@ -1,6 +1,6 @@
 package com.finovara.corebackend.report.finances.highesttransactions.highestexpense.dto;
 
-import com.finovara.activityservice.contracts.model.transaction.ExpenseCategory;
+import com.finovara.contracts.model.transaction.ExpenseCategory;
 
 import java.math.BigDecimal;
 
