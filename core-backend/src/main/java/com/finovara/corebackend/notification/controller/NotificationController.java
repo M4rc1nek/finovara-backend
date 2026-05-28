c23704bf00ca9e63e8f6e7f5cc1c4abc6a7ef1ba
Marcin Parśniak <marcinekpar92@op.pl>
2026-05-28 08:26:01 +0200

    update notification controller

--- core-backend/src/main/java/com/finovara/corebackend/notification/controller/NotificationController.java
+++ core-backend/src/main/java/com/finovara/corebackend/notification/controller/NotificationController.java
@@ -3,7 +3,7 @@ package com.finovara.corebackend.notification.controller;
 import com.finovara.corebackend.notification.dto.NotificationResponse;
 import com.finovara.corebackend.notification.service.NotificationPersistenceService;
 import com.finovara.corebackend.notification.service.NotificationService;
-import com.finovara.activityservice.contracts.model.SortType;
+import com.finovara.contracts.model.SortType;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
