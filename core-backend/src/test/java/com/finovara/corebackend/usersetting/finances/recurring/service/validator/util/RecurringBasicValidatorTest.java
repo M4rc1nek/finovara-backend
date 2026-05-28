c2a970a92307592030cf8945098b43582663b82f
Marcin Parśniak <marcinekpar92@op.pl>
2026-05-28 08:32:12 +0200

    update recurring basic validator test

--- core-backend/src/test/java/com/finovara/corebackend/usersetting/finances/recurring/service/validator/util/RecurringBasicValidatorTest.java
+++ core-backend/src/test/java/com/finovara/corebackend/usersetting/finances/recurring/service/validator/util/RecurringBasicValidatorTest.java
@@ -1,9 +1,9 @@
 package com.finovara.corebackend.usersetting.finances.recurring.service.validator.util;
 
-import com.finovara.corebackend.exception.badrequest.InvalidInputException;
+import com.finovara.contracts.exception.badrequest.InvalidInputException;
 import com.finovara.corebackend.user.model.User;
 import com.finovara.corebackend.usersetting.finances.recurring.model.RecurringSettings;
-import com.finovara.activityservice.contracts.model.PeriodType;
+import com.finovara.contracts.model.PeriodType;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
