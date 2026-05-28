6226fed4986b7f2b15caa2c644368cb67294db56
Marcin Parśniak <marcinekpar92@op.pl>
2026-05-28 08:06:48 +0200

    update login activity consumer

--- activity-log-backend/src/main/java/com/finovara/activityservice/kafka/LoginActivityConsumer.java
+++ activity-log-backend/src/main/java/com/finovara/activityservice/kafka/LoginActivityConsumer.java
@@ -1,7 +1,7 @@
 package com.finovara.activityservice.kafka;
 
 import com.finovara.activityservice.activity_log.accountactivity.secure.login.activity.service.LoginActivityService;
-import com.finovara.activityservice.contracts.event.secure.login.activity.LoginActivityEvent;
+import com.finovara.contracts.event.secure.login.activity.LoginActivityEvent;
 import lombok.RequiredArgsConstructor;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
