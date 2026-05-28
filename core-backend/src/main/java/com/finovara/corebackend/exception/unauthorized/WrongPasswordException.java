b8c2206a2843a0911240da7f662469284073cd62
Marcin Parśniak <marcinekpar92@op.pl>
2026-05-28 08:25:47 +0200

    remove wrong password exception from core-backend

--- core-backend/src/main/java/com/finovara/corebackend/exception/unauthorized/WrongPasswordException.java
+++
@@ -1,7 +0,0 @@
-package com.finovara.corebackend.exception.unauthorized;
-
-public class WrongPasswordException extends RuntimeException {
-    public WrongPasswordException(String message) {
-        super(message);
-    }
-}
