ed944b282ee2c8e7ac9fc0fbcfa2ccdb00c48162
Marcin Parśniak <marcinekpar92@op.pl>
2026-05-28 08:26:39 +0200

    update password update service

--- core-backend/src/main/java/com/finovara/corebackend/usersetting/account/service/passwordpolicy/change/PasswordUpdateService.java
+++ core-backend/src/main/java/com/finovara/corebackend/usersetting/account/service/passwordpolicy/change/PasswordUpdateService.java
@@ -1,14 +1,11 @@
 package com.finovara.corebackend.usersetting.account.service.passwordpolicy.change;
 
-import com.finovara.activityservice.contracts.clientdata.browser.UserBrowser;
-import com.finovara.activityservice.contracts.clientdata.ip.ClientIp;
-import com.finovara.activityservice.contracts.clientdata.location.UserLocation;
-import com.finovara.activityservice.contracts.event.secure.accountchange.activity.AccountChangesActivityEvent;
-import com.finovara.activityservice.contracts.model.activity.AccountChangesActivityType;
+import com.finovara.contracts.event.secure.accountchange.activity.AccountChangesActivityEvent;
+import com.finovara.contracts.model.activity.AccountChangesActivityType;
 
-import static com.finovara.activityservice.contracts.clientdata.browser.UserBrowser.getBrowser;
-import static com.finovara.activityservice.contracts.clientdata.ip.ClientIp.getClientIpAddress;
-import static com.finovara.activityservice.contracts.clientdata.location.UserLocation.getLocationFromIp;
+import static com.finovara.contracts.clientdata.browser.UserBrowser.getBrowser;
+import static com.finovara.contracts.clientdata.ip.ClientIp.getClientIpAddress;
+import static com.finovara.contracts.clientdata.location.UserLocation.getLocationFromIp;
 import com.finovara.corebackend.user.model.User;
 import com.finovara.corebackend.user.repository.UserRepository;
 import com.finovara.corebackend.usersetting.notificationemail.action.passwordchange.service.NotifyPasswordChangeService;
