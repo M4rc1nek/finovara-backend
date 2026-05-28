5c330480106de2510c939b679222682f84cb0de8
Marcin Parśniak <marcinekpar92@op.pl>
2026-05-28 08:25:48 +0200

    remove missing requirement exception from core-backend

--- core-backend/src/main/java/com/finovara/corebackend/exception/unprocessablecontent/MissingRequirementException.java
+++
@@ -1,7 +0,0 @@
-package com.finovara.corebackend.exception.unprocessablecontent;
-
-public class MissingRequirementException extends RuntimeException {
-    public MissingRequirementException(String message) {
-        super(message);
-    }
-}
