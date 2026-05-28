b9eeb2524900be2df582d3df56d083f77297e816
Marcin Parśniak <marcinekpar92@op.pl>
2026-05-28 08:26:17 +0200

    update category percentage controller

--- core-backend/src/main/java/com/finovara/corebackend/report/finances/categorypercentage/controller/CategoryPercentageController.java
+++ core-backend/src/main/java/com/finovara/corebackend/report/finances/categorypercentage/controller/CategoryPercentageController.java
@@ -1,13 +1,13 @@
 package com.finovara.corebackend.report.finances.categorypercentage.controller;
 
-import com.finovara.activityservice.contracts.model.transaction.ExpenseCategory;
+import com.finovara.contracts.model.transaction.ExpenseCategory;
 import com.finovara.corebackend.report.finances.categorypercentage.revenue.dto.RevenueCategoryPercentageDto;
 import com.finovara.corebackend.report.finances.categorypercentage.revenue.service.RevenueCategoryPercentageService;
 import com.finovara.corebackend.report.finances.categorypercentage.expense.dto.ExpenseCategoryPercentageDto;
 import com.finovara.corebackend.report.finances.categorypercentage.expense.service.ExpenseCategoryPercentageService;
-import com.finovara.activityservice.contracts.model.transaction.RevenueCategory;
+import com.finovara.contracts.model.transaction.RevenueCategory;
 import com.finovara.corebackend.security.SecurityUtils;
-import com.finovara.activityservice.contracts.model.PeriodType;
+import com.finovara.contracts.model.PeriodType;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
