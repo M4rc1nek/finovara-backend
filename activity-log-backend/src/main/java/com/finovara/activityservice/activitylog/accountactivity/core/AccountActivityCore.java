e4df338143b4a5ce26df2843f610232a3c27e8bc
Marcin Parśniak <marcinekpar92@op.pl>
2026-05-29 20:52:14 +0200

    remove @RequiredArgsConstructor  from AccountActivityCore

--- activity-log-backend/src/main/java/com/finovara/activityservice/activitylog/accountactivity/core/AccountActivityCore.java
+++ activity-log-backend/src/main/java/com/finovara/activityservice/activitylog/accountactivity/core/AccountActivityCore.java
@@ -1,23 +1,21 @@
 package com.finovara.activityservice.activitylog.accountactivity.core;
 
 import com.finovara.contracts.model.SortType;
-import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
-@RequiredArgsConstructor
 public abstract class AccountActivityCore<T, D> {
 
-	public List<D> getActivities(Long userId, SortType sort, int pageSize) {
-		Pageable pageable = sort.getPageable(pageSize);
-		return getRepositoryFindByUserId(userId, pageable)
-				.stream()
-				.map(this::mapToDto)
-				.toList();
-	}
+    public List<D> getActivities(Long userId, SortType sort, int pageSize) {
+        Pageable pageable = sort.getPageable(pageSize);
+        return getRepositoryFindByUserId(userId, pageable)
+                .stream()
+                .map(this::mapToDto)
+                .toList();
+    }
 
-	protected abstract List<T> getRepositoryFindByUserId(Long userId, Pageable pageable);
+    protected abstract List<T> getRepositoryFindByUserId(Long userId, Pageable pageable);
 
-	protected abstract D mapToDto(T entity);
+    protected abstract D mapToDto(T entity);
 }
