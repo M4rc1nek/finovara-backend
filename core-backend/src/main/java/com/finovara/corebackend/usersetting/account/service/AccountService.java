00d0d0eebde53de3e5d6ed57b02d254fe0b5986b
Marcin Parśniak <marcinekpar92@op.pl>
2026-06-01 17:43:45 +0200

    publish account deletion notification email events

--- core-backend/src/main/java/com/finovara/corebackend/usersetting/account/service/AccountService.java
+++ core-backend/src/main/java/com/finovara/corebackend/usersetting/account/service/AccountService.java
@@ -1,17 +1,16 @@
 package com.finovara.corebackend.usersetting.account.service;
 
-import com.finovara.contracts.event.secure.accountchange.activity.AccountChangesActivityEvent;
+import com.finovara.contracts.event.activity.secure.accountchange.activity.AccountChangesActivityEvent;
 import com.finovara.contracts.model.activity.AccountChangesActivityType;
 
 import static com.finovara.contracts.clientdata.browser.UserBrowser.getBrowser;
 import static com.finovara.contracts.clientdata.ip.ClientIp.getClientIpAddress;
 import static com.finovara.contracts.clientdata.location.UserLocation.getLocationFromIp;
 import com.finovara.contracts.exception.conflict.EntityAlreadyExistsException;
+import com.finovara.corebackend.notification.email.NotificationEmailEventPublisher;
 import com.finovara.corebackend.user.model.User;
 import com.finovara.corebackend.user.repository.UserRepository;
 import com.finovara.corebackend.usersetting.account.dto.AccountSettingsDto;
-import com.finovara.corebackend.usersetting.notificationemail.action.accountdeleted.service.NotifyOnAccountDeletedService;
-import com.finovara.corebackend.usersetting.notificationemail.action.usernamechange.service.NotifyUsernameChangeService;
 import com.finovara.contracts.dto.ConfirmPasswordDto;
 import com.finovara.corebackend.util.confirmationpassword.service.PasswordValidator;
 import com.finovara.corebackend.util.profile.ProfileImageUrlBuilder;
@@ -33,8 +32,7 @@ public class AccountService {
     private final UserManagerService userManagerService;
     private final PasswordValidator passwordValidator;
     private final KafkaTemplate<String, Object> kafkaTemplate;
-    private final NotifyUsernameChangeService notifyUsernameChangeService;
-    private final NotifyOnAccountDeletedService notifyOnAccountDeletedService;
+    private final NotificationEmailEventPublisher notificationEmailEventPublisher;
 
     @Transactional
     public AccountSettingsDto updateUsername(AccountSettingsDto accountSettingsDto, Long userId, HttpServletRequest request) {
@@ -48,7 +46,7 @@ public class AccountService {
         userRepository.save(user);
         String ipAddress = getClientIpAddress(request);
         kafkaTemplate.send("activity.account-changes", new AccountChangesActivityEvent(userId, AccountChangesActivityType.USERNAME_CHANGED, getBrowser(request), ipAddress, getLocationFromIp(ipAddress), LocalDateTime.now()));
-        notifyUsernameChangeService.sendEmail(user);
+        notificationEmailEventPublisher.sendUsernameChanged(user);
         return accountSettingsDto;
     }
 
@@ -57,9 +55,9 @@ public class AccountService {
         User user = userManagerService.getUserByIdOrThrow(userId);
 
         passwordValidator.validatePassword(userId, confirmPasswordDto);
+        notificationEmailEventPublisher.sendAccountDeleted(user);
         userRepository.delete(user);
         log.info("User account has been deleted. User email: {}", user.getEmail());
-        notifyOnAccountDeletedService.sendEmail(user);
     }
 
     @Transactional
