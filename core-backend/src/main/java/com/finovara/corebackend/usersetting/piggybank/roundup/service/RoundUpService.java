6d1067116b60fede505271fd256e75e284d1ed19
Marcin Parśniak <marcinekpar92@op.pl>
2026-06-05 18:57:57 +0200

    replace jakarta @Transactional with Spring @Transactional in round up service

--- core-backend/src/main/java/com/finovara/corebackend/usersetting/piggybank/roundup/service/RoundUpService.java
+++ core-backend/src/main/java/com/finovara/corebackend/usersetting/piggybank/roundup/service/RoundUpService.java
@@ -19,7 +19,7 @@ import com.finovara.corebackend.util.piggybank.manager.PiggyBankManagerService;
 import com.finovara.corebackend.util.user.service.UserManagerService;
 import com.finovara.corebackend.wallet.model.Wallet;
 import com.finovara.corebackend.wallet.repository.WalletRepository;
-import jakarta.transaction.Transactional;
+import org.springframework.transaction.annotation.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
