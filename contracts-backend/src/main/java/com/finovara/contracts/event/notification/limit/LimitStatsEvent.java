392cd650f2f63da72e2144661a49e2084bea40f5
Marcin Parśniak <marcinekpar92@op.pl>
2026-06-08 14:05:42 +0200

    define LimitStatsEvent kafka record with userId, limitId, percentage, and periodType

--- contracts-backend/src/main/java/com/finovara/contracts/event/notification/limit/LimitStatsEvent.java
+++ contracts-backend/src/main/java/com/finovara/contracts/event/notification/limit/LimitStatsEvent.java
@@ -1,4 +1,13 @@
 package com.finovara.contracts.event.notification.limit;
 
-public class LimitStatsEvent {
+import com.finovara.contracts.model.PeriodType;
+
+import java.math.BigDecimal;
+
+public record LimitStatsEvent(
+        Long userId,
+        Long limitId,
+        BigDecimal percentage,
+        PeriodType periodType
+) {
 }
