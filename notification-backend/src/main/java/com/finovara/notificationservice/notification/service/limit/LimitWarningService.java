b7fca7ec7a9e04e1c8471ff1683b716f1b8a3384
Marcin Parśniak <marcinekpar92@op.pl>
2026-06-08 14:06:14 +0200

    fix limit warning topic typo and add dedicated kafka consumer group

--- notification-backend/src/main/java/com/finovara/notificationservice/notification/service/limit/LimitWarningService.java
+++ notification-backend/src/main/java/com/finovara/notificationservice/notification/service/limit/LimitWarningService.java
@@ -1,10 +1,11 @@
-package com.finovara.corebackend.notification.service.limit;
+package com.finovara.notificationservice.notification.service.limit;
 
 import com.finovara.contracts.event.notification.limit.LimitStatsEvent;
 import com.finovara.contracts.model.NotificationType;
-import com.finovara.corebackend.notification.dto.limit.LimitWarningDto;
-import com.finovara.corebackend.notification.service.NotificationPersistenceService;
+import com.finovara.notificationservice.notification.dto.limit.LimitWarningDto;
+import com.finovara.notificationservice.notification.service.NotificationPersistenceService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
@@ -12,12 +13,13 @@ import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 @Service
+@Slf4j
 @RequiredArgsConstructor
 public class LimitWarningService {
 
     private final NotificationPersistenceService notificationPersistenceService;
 
-    @KafkaListener(topics = "llimit.calculate-stats")
+    @KafkaListener(topics = "limit.calculate-stats",  groupId = "notification-limit-warning")
     public void handle(LimitStatsEvent event) {
         boolean isWarning = event.percentage().compareTo(BigDecimal.valueOf(75)) >= 0;
         boolean isBelowLimit = event.percentage().compareTo(BigDecimal.valueOf(100)) < 0;
@@ -31,6 +33,7 @@ public class LimitWarningService {
                     event.limitId(),
                     BigDecimal.valueOf(75)
             ));
+            log.info("Limit usage warning for userId={}, limitId={}, periodType={}, usage={}%", event.userId(), event.limitId(), event.periodType(), event.percentage());
         }
     }
 }
