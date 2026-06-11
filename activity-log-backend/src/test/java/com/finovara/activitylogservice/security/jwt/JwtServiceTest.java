17d89ebb9fdb1a5946c73754a4dda8e22929a89f
Marcin Parśniak <marcinekpar92@op.pl>
2026-06-11 12:49:18 +0200

    rename JwtServiceTest package to activitylogservice

--- activity-log-backend/src/test/java/com/finovara/activitylogservice/security/jwt/JwtServiceTest.java
+++ activity-log-backend/src/test/java/com/finovara/activitylogservice/security/jwt/JwtServiceTest.java
@@ -1,4 +1,4 @@
-package com.finovara.activityservice.security.jwt;
+package com.finovara.activitylogservice.security.jwt;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
