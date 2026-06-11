2bd4f49a4c46bfa24668db9346ecd0d15db5c1cc
Marcin Parśniak <marcinekpar92@op.pl>
2026-06-11 12:49:36 +0200

    move TotalCashFlowChartService from core-backend to report-backend

--- report-backend/src/main/java/com/finovara/reportservice/report/finances/chart/cashflow/service/TotalCashFlowChartService.java
+++ report-backend/src/main/java/com/finovara/reportservice/report/finances/chart/cashflow/service/TotalCashFlowChartService.java
@@ -1,10 +1,9 @@
-package com.finovara.corebackend.report.finances.chart.cashflow.service;
+package com.finovara.reportservice.report.finances.chart.cashflow.service;
 
-import com.finovara.corebackend.expense.repository.ExpenseRepository;
-import com.finovara.corebackend.report.finances.chart.builder.CashFlowChartService;
-import com.finovara.corebackend.report.finances.chart.dto.CashFlowDto;
-import com.finovara.corebackend.report.finances.chart.dto.DailyCashDto;
-import com.finovara.corebackend.revenue.repository.RevenueRepository;
+import com.finovara.reportservice.feignclient.CoreBackendReportClient;
+import com.finovara.reportservice.report.finances.chart.builder.CashFlowChartService;
+import com.finovara.reportservice.report.finances.chart.dto.CashFlowDto;
+import com.finovara.contracts.transaction.report.dto.DailyCashDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -13,13 +12,13 @@ import java.util.List;
 @Service
 @RequiredArgsConstructor
 public class TotalCashFlowChartService {
-    private final RevenueRepository revenueRepository;
-    private final ExpenseRepository expenseRepository;
+
+    private final CoreBackendReportClient reportClient;
     private final CashFlowChartService cashFlowChartService;
 
     public List<CashFlowDto> getCashFlowChart(Long userId) {
-        List<DailyCashDto> summedExpenses = expenseRepository.sumExpensesGroupedByDate(userId);
-        List<DailyCashDto> summedRevenues = revenueRepository.sumRevenuesGroupedByDate(userId);
-        return cashFlowChartService.getCashFlowChart(summedExpenses, summedRevenues);
+        List<DailyCashDto> expenses = reportClient.expensesGroupedByDate(userId);
+        List<DailyCashDto> revenues = reportClient.revenuesGroupedByDate(userId);
+        return cashFlowChartService.getCashFlowChart(expenses, revenues);
     }
 }
