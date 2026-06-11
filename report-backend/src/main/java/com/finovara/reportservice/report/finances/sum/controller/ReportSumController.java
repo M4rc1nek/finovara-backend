eec7bda6bad38f7d2c87da1f83757bc4e4915057
Marcin Parśniak <marcinekpar92@op.pl>
2026-06-11 12:49:40 +0200

    move ReportSumController from core-backend to report-backend

--- report-backend/src/main/java/com/finovara/reportservice/report/finances/sum/controller/ReportSumController.java
+++ report-backend/src/main/java/com/finovara/reportservice/report/finances/sum/controller/ReportSumController.java
@@ -1,8 +1,8 @@
-package com.finovara.corebackend.report.finances.sum.controller;
+package com.finovara.reportservice.report.finances.sum.controller;
 
-import com.finovara.corebackend.report.dto.ReportDto;
+import com.finovara.reportservice.report.dto.ReportDto;
 import com.finovara.contracts.model.PeriodType;
-import com.finovara.corebackend.report.finances.sum.service.ReportSummaryService;
+import com.finovara.reportservice.report.finances.sum.service.ReportSummaryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
