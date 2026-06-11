46b4402f5d7673b92857743e9f646df61ac879aa
Marcin Parśniak <marcinekpar92@op.pl>
2026-06-11 12:49:16 +0200

    rename SettingsActivityServiceTest package to activitylogservice

--- activity-log-backend/src/test/java/com/finovara/activitylogservice/accountactivity/settings/service/SettingsActivityServiceTest.java
+++ activity-log-backend/src/test/java/com/finovara/activitylogservice/accountactivity/settings/service/SettingsActivityServiceTest.java
@@ -1,10 +1,10 @@
-package com.finovara.activityservice.accountactivity.settings.service;
+package com.finovara.activitylogservice.accountactivity.settings.service;
 
-import com.finovara.activityservice.activitylog.accountactivity.settings.dto.SettingsActivityDto;
-import com.finovara.activityservice.activitylog.accountactivity.settings.mapper.SettingsActivityMapper;
-import com.finovara.activityservice.activitylog.accountactivity.settings.model.SettingsActivity;
-import com.finovara.activityservice.activitylog.accountactivity.settings.repository.SettingsActivityRepository;
-import com.finovara.activityservice.activitylog.accountactivity.settings.service.SettingsActivityService;
+import com.finovara.activitylogservice.activitylog.accountactivity.settings.dto.SettingsActivityDto;
+import com.finovara.activitylogservice.activitylog.accountactivity.settings.mapper.SettingsActivityMapper;
+import com.finovara.activitylogservice.activitylog.accountactivity.settings.model.SettingsActivity;
+import com.finovara.activitylogservice.activitylog.accountactivity.settings.repository.SettingsActivityRepository;
+import com.finovara.activitylogservice.activitylog.accountactivity.settings.service.SettingsActivityService;
 import com.finovara.contracts.event.activity.settings.SettingsActivityEvent;
 import com.finovara.contracts.model.SortType;
 import com.finovara.contracts.model.activity.SettingActivityStatus;
