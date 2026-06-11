05418368df952b8d5be3095609b55688abd745e6
Marcin Parśniak <marcinekpar92@op.pl>
2026-06-11 12:49:08 +0200

    rename CustomUserDetails package to activitylogservice

--- activity-log-backend/src/main/java/com/finovara/activitylogservice/security/CustomUserDetails.java
+++ activity-log-backend/src/main/java/com/finovara/activitylogservice/security/CustomUserDetails.java
@@ -1,4 +1,4 @@
-package com.finovara.activityservice.security;
+package com.finovara.activitylogservice.security;
 
 import lombok.Getter;
 import org.springframework.security.core.GrantedAuthority;
