f41a7be27e61fb7401b3f718d8873248a6529c2e
Marcin Parśniak <marcinekpar92@op.pl>
2026-06-11 12:49:08 +0200

    rename KafkaConfig package to activitylogservice

--- activity-log-backend/src/main/java/com/finovara/activitylogservice/kafka/config/KafkaConfig.java
+++ activity-log-backend/src/main/java/com/finovara/activitylogservice/kafka/config/KafkaConfig.java
@@ -1,4 +1,4 @@
-package com.finovara.activityservice.kafka.config;
+package com.finovara.activitylogservice.kafka.config;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
