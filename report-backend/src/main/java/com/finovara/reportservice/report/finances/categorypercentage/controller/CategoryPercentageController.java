8914a871a48963fa50cb363bb1efd02f42f81912
Marcin Parśniak <marcinekpar92@op.pl>
2026-06-11 12:49:33 +0200

    move CategoryPercentageController from core-backend to report-backend

--- report-backend/src/main/java/com/finovara/reportservice/report/finances/categorypercentage/controller/CategoryPercentageController.java
+++ report-backend/src/main/java/com/finovara/reportservice/report/finances/categorypercentage/controller/CategoryPercentageController.java
@@ -1,13 +1,13 @@
-package com.finovara.corebackend.report.finances.categorypercentage.controller;
+package com.finovara.reportservice.report.finances.categorypercentage.controller;
 
 import com.finovara.contracts.model.transaction.ExpenseCategory;
-import com.finovara.corebackend.report.finances.categorypercentage.revenue.dto.RevenueCategoryPercentageDto;
-import com.finovara.corebackend.report.finances.categorypercentage.revenue.service.RevenueCategoryPercentageService;
-import com.finovara.corebackend.report.finances.categorypercentage.expense.dto.ExpenseCategoryPercentageDto;
-import com.finovara.corebackend.report.finances.categorypercentage.expense.service.ExpenseCategoryPercentageService;
+import com.finovara.reportservice.report.finances.categorypercentage.revenue.dto.RevenueCategoryPercentageDto;
+import com.finovara.reportservice.report.finances.categorypercentage.revenue.service.RevenueCategoryPercentageService;
+import com.finovara.reportservice.report.finances.categorypercentage.expense.dto.ExpenseCategoryPercentageDto;
+import com.finovara.reportservice.report.finances.categorypercentage.expense.service.ExpenseCategoryPercentageService;
 import com.finovara.contracts.model.transaction.RevenueCategory;
-import com.finovara.corebackend.security.SecurityUtils;
 import com.finovara.contracts.model.PeriodType;
+import com.finovara.reportservice.security.SecurityUtils;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
