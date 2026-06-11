cbf322f5ef7afba95dc6d9b5eac167a6948b15df
Marcin Parśniak <marcinekpar92@op.pl>
2026-06-11 12:49:12 +0200

    rename ExpenseActivityServiceTest package to activitylogservice

--- activity-log-backend/src/test/java/com/finovara/activitylogservice/accountactivity/expense/service/ExpenseActivityServiceTest.java
+++ activity-log-backend/src/test/java/com/finovara/activitylogservice/accountactivity/expense/service/ExpenseActivityServiceTest.java
@@ -1,10 +1,10 @@
-package com.finovara.activityservice.accountactivity.expense.service;
+package com.finovara.activitylogservice.accountactivity.expense.service;
 
-import com.finovara.activityservice.activitylog.accountactivity.expense.dto.ExpenseActivityDto;
-import com.finovara.activityservice.activitylog.accountactivity.expense.mapper.ExpenseActivityMapper;
-import com.finovara.activityservice.activitylog.accountactivity.expense.model.ExpenseActivity;
-import com.finovara.activityservice.activitylog.accountactivity.expense.repository.ExpenseActivityRepository;
-import com.finovara.activityservice.activitylog.accountactivity.expense.service.ExpenseActivityService;
+import com.finovara.activitylogservice.activitylog.accountactivity.expense.dto.ExpenseActivityDto;
+import com.finovara.activitylogservice.activitylog.accountactivity.expense.mapper.ExpenseActivityMapper;
+import com.finovara.activitylogservice.activitylog.accountactivity.expense.model.ExpenseActivity;
+import com.finovara.activitylogservice.activitylog.accountactivity.expense.repository.ExpenseActivityRepository;
+import com.finovara.activitylogservice.activitylog.accountactivity.expense.service.ExpenseActivityService;
 import com.finovara.contracts.event.activity.expense.ExpenseActivityEvent;
 import com.finovara.contracts.model.SortType;
 import com.finovara.contracts.model.activity.ExpenseActivityType;
