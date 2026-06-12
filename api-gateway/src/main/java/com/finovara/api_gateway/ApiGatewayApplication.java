684706b75f53bd3a7457d4ffd2aacf6689b1584d
Marcin Parśniak <marcinekpar92@op.pl>
2026-06-12 14:10:06 +0200

    enable security properties binding in api gateway application

--- api-gateway/src/main/java/com/finovara/api_gateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/finovara/api_gateway/ApiGatewayApplication.java
@@ -1,9 +1,12 @@
 package com.finovara.api_gateway;
 
+import com.finovara.api_gateway.security.SecurityProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(SecurityProperties.class)
 public class ApiGatewayApplication {
 
     public static void main(String[] args) {
