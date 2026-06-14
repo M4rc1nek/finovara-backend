7df0163583b36837aa657f10364923cdbb710f89
Marcin Parśniak <marcinekpar92@op.pl>
2026-06-14 15:04:06 +0200

    add new module finance-backend

---
+++ auth-backend/src/main/java/com/finovara/authbackend/usersetting/finances/expense/repository/ExpenseSettingsRepository.java
@@ -0,0 +1,20 @@
+package com.finovara.authbackend.usersetting.finances.expense.repository;
+
+import com.finovara.contracts.exception.notfound.RequestedEntityNotFoundException;
+import com.finovara.authbackend.usersetting.finances.expense.model.ExpenseSettings;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ExpenseSettingsRepository extends JpaRepository<ExpenseSettings, Long> {
+
+    ExpenseSettings findByUserId(Long userId);
+
+    default ExpenseSettings findByUserIdOrThrow(Long userId) {
+        ExpenseSettings settings = findByUserId(userId);
+        if (settings == null) {
+            throw new RequestedEntityNotFoundException("Expense settings not found for user");
+        }
+        return settings;
+    }
+}
