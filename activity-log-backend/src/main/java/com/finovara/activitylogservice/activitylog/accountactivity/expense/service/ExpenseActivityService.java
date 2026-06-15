9735f983761da895723d1d89bfc794e70b0326d1
Marcin Parśniak <marcinekpar92@op.pl>
2026-06-15 15:12:08 +0200

    update UserDataDeletable import to contracts package in ExpenseActivityService

--- activity-log-backend/src/main/java/com/finovara/activitylogservice/activitylog/accountactivity/expense/service/ExpenseActivityService.java
+++ activity-log-backend/src/main/java/com/finovara/activitylogservice/activitylog/accountactivity/expense/service/ExpenseActivityService.java
@@ -5,7 +5,7 @@ import com.finovara.activitylogservice.activitylog.accountactivity.expense.dto.E
 import com.finovara.activitylogservice.activitylog.accountactivity.expense.mapper.ExpenseActivityMapper;
 import com.finovara.activitylogservice.activitylog.accountactivity.expense.model.ExpenseActivity;
 import com.finovara.activitylogservice.activitylog.accountactivity.expense.repository.ExpenseActivityRepository;
-import com.finovara.activitylogservice.activitylog.datadeletable.UserDataDeletable;
+import com.finovara.contracts.datadeletable.UserDataDeletable;
 import com.finovara.contracts.event.activity.expense.ExpenseActivityEvent;
 import com.finovara.contracts.model.SortType;
 import lombok.RequiredArgsConstructor;
