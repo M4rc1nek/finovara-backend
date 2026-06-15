a7d43162722dc01723642d97447cd342ddb506ad
Marcin Parśniak <marcinekpar92@op.pl>
2026-06-15 15:12:11 +0200

    update UserDataDeletable import to contracts package in LoginActivityService

--- activity-log-backend/src/main/java/com/finovara/activitylogservice/activitylog/accountactivity/secure/login/activity/service/LoginActivityService.java
+++ activity-log-backend/src/main/java/com/finovara/activitylogservice/activitylog/accountactivity/secure/login/activity/service/LoginActivityService.java
@@ -6,7 +6,7 @@ import com.finovara.activitylogservice.activitylog.accountactivity.secure.login.
 import com.finovara.activitylogservice.activitylog.accountactivity.secure.login.activity.repository.LoginActivityRepository;
 import com.finovara.activitylogservice.activitylog.accountactivity.secure.login.archive.model.LoginActivityArchive;
 import com.finovara.activitylogservice.activitylog.accountactivity.secure.login.archive.service.LoginActivityArchiveService;
-import com.finovara.activitylogservice.activitylog.datadeletable.UserDataDeletable;
+import com.finovara.contracts.datadeletable.UserDataDeletable;
 import com.finovara.activitylogservice.feignclient.AuthBackendClient;
 import com.finovara.contracts.auth.dto.ConfirmPasswordDto;
 import com.finovara.contracts.event.activity.secure.login.activity.LoginActivityEvent;
