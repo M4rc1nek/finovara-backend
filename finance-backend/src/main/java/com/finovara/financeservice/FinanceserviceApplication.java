3440ce390a6a6e3351f1f3883f1a4963f5f83781
Marcin Parśniak <marcinekpar92@op.pl>
2026-06-15 15:12:21 +0200

    add EnableFeignClients and EnableScheduling annotations to FinanceserviceApplication

--- finance-backend/src/main/java/com/finovara/financeservice/FinanceserviceApplication.java
+++ finance-backend/src/main/java/com/finovara/financeservice/FinanceserviceApplication.java
@@ -2,8 +2,12 @@ package com.finovara.financeservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableFeignClients
+@EnableScheduling
 public class FinanceserviceApplication {
 
 	public static void main(String[] args) {
