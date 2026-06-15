dceb62d4e9dd1a1fdc3bf28871ae7809ba361ef7
Marcin Parśniak <marcinekpar92@op.pl>
2026-06-15 15:12:29 +0200

    add deleteByUserId method to WalletRepository

--- finance-backend/src/main/java/com/finovara/financeservice/wallet/repository/WalletRepository.java
+++ finance-backend/src/main/java/com/finovara/financeservice/wallet/repository/WalletRepository.java
@@ -10,4 +10,6 @@ import java.util.Optional;
 public interface WalletRepository extends JpaRepository<Wallet, Long> {
 
     Optional<Wallet> findByUserId(Long userId);
+
+    void deleteByUserId(Long userId);
 }
