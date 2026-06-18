d2b4b13c26c64bd824fe2b7d2e72e53e714ba035
Marcin Parśniak <marcinekpar92@op.pl>
2026-06-18 17:22:04 +0200

    rewrite RedisConfig to use GenericJackson2JsonRedisSerializer with default typing and RedisCacheManagerBuilderCustomizer

--- finance-backend/src/main/java/com/finovara/financeservice/config/redis/RedisConfig.java
+++ finance-backend/src/main/java/com/finovara/financeservice/config/redis/RedisConfig.java
@@ -1,48 +1,46 @@
-package com.finovara.financeservice.config;
+package com.finovara.financeservice.config.redis;
 
-import lombok.Getter;
-import lombok.Setter;
-import org.springframework.boot.context.properties.ConfigurationProperties;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.jsontype.impl.LaissezFaireSubTypeValidator;
+import com.finovara.financeservice.config.redis.properties.CacheProperties;
+import org.springframework.boot.cache.autoconfigure.RedisCacheManagerBuilderCustomizer;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
-import org.springframework.data.redis.cache.RedisCacheManager;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 import java.time.Duration;
-import java.util.HashMap;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 @Configuration
+@EnableConfigurationProperties(CacheProperties.class)
 @EnableCaching
-@Getter
-@Setter
-@ConfigurationProperties(prefix = "cache")
 public class RedisConfig {
 
-    private Map<String, Long> ttlMinutes = new HashMap<>();
+    @Bean
+    public RedisCacheConfiguration redisCacheConfiguration(ObjectMapper objectMapper) {
+        ObjectMapper cacheObjectMapper = objectMapper.copy();
+
+        cacheObjectMapper.activateDefaultTyping(LaissezFaireSubTypeValidator.instance, ObjectMapper.DefaultTyping.EVERYTHING, JsonTypeInfo.As.PROPERTY);
+
+        return RedisCacheConfiguration.defaultCacheConfig()
+                .disableCachingNullValues()
+                .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer()))
+                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer(cacheObjectMapper)));
+    }
 
     @Bean
-    public RedisCacheManager cacheManager(RedisConnectionFactory factory) {
+    public RedisCacheManagerBuilderCustomizer redisCacheManagerBuilderCustomizer(CacheProperties cacheProperties, RedisCacheConfiguration defaultConfiguration) {
 
-        RedisCacheConfiguration defaultConfig = RedisCacheConfiguration.defaultCacheConfig()
-                .serializeKeysWith(RedisSerializationContext.SerializationPair
-                        .fromSerializer(new Jackson2JsonRedisSerializer<>()))
-                .disableCachingNullValues();
-
-        Map<String, RedisCacheConfiguration> cacheConfigs = ttlMinutes.entrySet().stream()
-                .collect(Collectors.toMap(
-                        Map.Entry::getKey,
-                        e -> defaultConfig.entryTtl(Duration.ofMinutes(e.getValue()))
-                ));
-
-        return RedisCacheManager.builder(factory)
-                .cacheDefaults(defaultConfig)
-                .withInitialCacheConfigurations(cacheConfigs)
-                .build();
+        return builder -> {
+            for (Map.Entry<String, Long> entry : cacheProperties.getTtlMinutes().entrySet()) {
+                builder.withCacheConfiguration(entry.getKey(), defaultConfiguration.entryTtl(Duration.ofMinutes(entry.getValue())));
+            }
+        };
     }
 }
