ecef54cfa6a12a6738a2b83453271f6fdb297988
Marcin Parśniak <marcinekpar92@op.pl>
2026-06-18 17:22:09 +0200

    add PATCH endpoint setGoalCompletion and rename PUT handler to saveGoalCompletion in GoalCompletionController

--- finance-backend/src/main/java/com/finovara/financeservice/settings/piggybank/completion/controller/GoalCompletionController.java
+++ finance-backend/src/main/java/com/finovara/financeservice/settings/piggybank/completion/controller/GoalCompletionController.java
@@ -15,21 +15,21 @@ public class GoalCompletionController {
 
     private final GoalCompletionService goalCompletionService;
 
+    @PatchMapping("/{piggyBankId}")
+    public ResponseEntity<Void> setGoalCompletion(@RequestBody @Valid GoalCompletionDto goalCompletionDto, @PathVariable Long piggyBankId) {
+        goalCompletionService.setGoalCompletion(SecurityUtils.getCurrentUserId(), piggyBankId, goalCompletionDto);
+        return ResponseEntity.noContent().build();
+    }
+
     @PutMapping("/{piggyBankId}")
-    public ResponseEntity<Void> addGoalCompletion(@PathVariable Long piggyBankId, @RequestBody GoalCompletionDto goalCompletionDto) {
-        goalCompletionService.addGoalCompletion(piggyBankId, SecurityUtils.getCurrentUserId(), goalCompletionDto);
+    public ResponseEntity<Void> saveGoalCompletion(@PathVariable Long piggyBankId, @RequestBody GoalCompletionDto goalCompletionDto) {
+        goalCompletionService.saveGoalCompletion(piggyBankId, SecurityUtils.getCurrentUserId(), goalCompletionDto);
         return ResponseEntity.noContent().build();
     }
 
     @GetMapping("/{piggyBankId}")
     public ResponseEntity<GoalCompletionDto> getCompletionDto(@PathVariable Long piggyBankId) {
         return ResponseEntity.ok(goalCompletionService.getCompletionDto(SecurityUtils.getCurrentUserId(), piggyBankId));
-    }
-
-    @PatchMapping("/{piggyBankId}")
-    public ResponseEntity<Void> saveGoalCompletion(@RequestBody @Valid GoalCompletionDto goalCompletionDto, @PathVariable Long piggyBankId) {
-        goalCompletionService.saveGoalCompletion(SecurityUtils.getCurrentUserId(), piggyBankId, goalCompletionDto);
-        return ResponseEntity.noContent().build();
     }
 
 }
