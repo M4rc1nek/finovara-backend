d2ebdfc24cb1cacd8e2ff3152556783d4076859d
Marcin Parśniak <marcinekpar92@op.pl>
2026-06-23 13:06:03 +0200

    extend RuntimeException and add message constructor to InvalidCredentialsException

--- auth-backend/src/main/java/com/finovara/authservice/exception/unauthorized/InvalidCredentialsException.java
+++ auth-backend/src/main/java/com/finovara/authservice/exception/unauthorized/InvalidCredentialsException.java
@@ -1,4 +1,8 @@
 package com.finovara.authservice.exception.unauthorized;
 
-public class InvalidCredentialsException {
+public class InvalidCredentialsException extends RuntimeException {
+    public InvalidCredentialsException(String message) {
+        super(message);
+    }
 }
+
