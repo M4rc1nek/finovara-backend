9792147446c63caae2bf4e246ff2210e5b575db6
Marcin Parśniak <marcinekpar92@op.pl>
2026-06-23 13:06:03 +0200

    add handler for LocalPasswordNotSetException returning 409 in auth GlobalExceptionHandler

--- auth-backend/src/main/java/com/finovara/authservice/exception/GlobalExceptionHandler.java
+++ auth-backend/src/main/java/com/finovara/authservice/exception/GlobalExceptionHandler.java
@@ -1,7 +1,11 @@
 package com.finovara.authservice.exception;
 
 import com.finovara.authservice.exception.badrequest.InvalidVerificationCodeException;
+import com.finovara.authservice.exception.conflict.LocalPasswordNotSetException;
 import com.finovara.authservice.exception.tomanyrequest.VerificationAttemptsExceededException;
+import com.finovara.authservice.exception.unauthorized.InvalidCredentialsException;
+import com.finovara.contracts.exception.ErrorResponseDto;
+import com.finovara.contracts.exception.forbidden.InvalidPasswordException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -35,4 +39,41 @@ public class GlobalExceptionHandler {
         );
         return new ResponseEntity<>(body, HttpStatus.TOO_MANY_REQUESTS);
     }
+
+
+    @ExceptionHandler(LocalPasswordNotSetException.class)
+    public ResponseEntity<ErrorResponseDto> handleLocalPasswordNotSetException(RuntimeException exception, WebRequest webRequest) {
+        ErrorResponseDto body = new ErrorResponseDto(
+                HttpStatus.CONFLICT.value(),
+                HttpStatus.CONFLICT.getReasonPhrase(),
+                exception.getMessage(),
+                webRequest.getDescription(false).replace("uri=", "")
+        );
+        return new ResponseEntity<>(body, HttpStatus.CONFLICT);
+    }
+
+
+    @ExceptionHandler(InvalidCredentialsException.class)
+    public ResponseEntity<ErrorResponseDto> handleIncorrectCredentials(InvalidCredentialsException exception, WebRequest webRequest) {
+        ErrorResponseDto body = new ErrorResponseDto(
+                HttpStatus.UNAUTHORIZED.value(),
+                HttpStatus.UNAUTHORIZED.getReasonPhrase(),
+                exception.getMessage(),
+                webRequest.getDescription(false).replace("uri=", "")
+        );
+        return new ResponseEntity<>(body, HttpStatus.UNAUTHORIZED);
+    }
+
+    @ExceptionHandler(InvalidPasswordException.class)
+    public ResponseEntity<ErrorResponseDto> handleInvalidPassword(InvalidPasswordException exception, WebRequest webRequest) {
+        ErrorResponseDto body = new ErrorResponseDto(
+                HttpStatus.FORBIDDEN.value(),
+                HttpStatus.FORBIDDEN.getReasonPhrase(),
+                exception.getMessage(),
+                webRequest.getDescription(false).replace("uri=", "")
+        );
+        return new ResponseEntity<>(body, HttpStatus.FORBIDDEN);
+    }
+
+
 }
