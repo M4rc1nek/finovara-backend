5901b2df137de0bab1dba04abf834aba3d049351
Marcin Parśniak <marcinekpar92@op.pl>
2026-06-24 13:44:00 +0200

    fix package declaration in auth-backend RedisConfig to config.redis

--- auth-backend/src/main/java/com/finovara/authservice/config/redis/RedisConfig.java
+++ auth-backend/src/main/java/com/finovara/authservice/config/redis/RedisConfig.java
@@ -1,4 +1,4 @@
-package com.finovara.authservice.config;
+package com.finovara.authservice.config.redis;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
