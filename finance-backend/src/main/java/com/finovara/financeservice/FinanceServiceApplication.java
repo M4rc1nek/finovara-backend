1929b1f46a9846f356f6c5fb93e6ba8d1013f052
Marcin Parśniak <marcinekpar92@op.pl>
2026-06-24 15:12:37 +0200

    add EntityScan, JpaRepositories and ComponentScan for outbox package in finance backend

--- finance-backend/src/main/java/com/finovara/financeservice/FinanceServiceApplication.java
+++ finance-backend/src/main/java/com/finovara/financeservice/FinanceServiceApplication.java
@@ -2,16 +2,22 @@ package com.finovara.financeservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.persistence.autoconfigure.EntityScan;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication
 @EnableFeignClients
 @EnableScheduling
-public class FinanceserviceApplication {
+@EntityScan(basePackages = {"com.finovara.financeservice", "com.finovara.contracts.outbox"})
+@EnableJpaRepositories(basePackages = {"com.finovara.financeservice", "com.finovara.contracts.outbox"})
+@ComponentScan(basePackages = {"com.finovara.financeservice", "com.finovara.contracts.outbox"})
+@SpringBootApplication
+public class FinanceServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FinanceserviceApplication.class, args);
+		SpringApplication.run(FinanceServiceApplication.class, args);
 	}
 
 }
