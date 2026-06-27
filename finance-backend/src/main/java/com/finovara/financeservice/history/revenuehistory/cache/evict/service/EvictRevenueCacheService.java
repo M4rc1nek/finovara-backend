9b3e492f0096e7692ee3e18347291304450ef5fb
Marcin Parśniak <marcinekpar92@op.pl>
2026-06-27 12:50:06 +0200

    rename RefreshRevenueCacheService to EvictRevenueCacheService and move to cache.evict package

--- finance-backend/src/main/java/com/finovara/financeservice/history/revenuehistory/cache/evict/service/EvictRevenueCacheService.java
+++ finance-backend/src/main/java/com/finovara/financeservice/history/revenuehistory/cache/evict/service/EvictRevenueCacheService.java
@@ -1,17 +1,23 @@
-package com.finovara.financeservice.history.revenuehistory.cache.refresh.service;
+package com.finovara.financeservice.history.revenuehistory.cache.evict.service;
 
 import com.finovara.contracts.cache.RedisCacheEvictor;
+import com.finovara.contracts.event.user.UserAccountDeletedEvent;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class RefreshRevenueCacheService {
+public class EvictRevenueCacheService {
 
     private final RedisCacheEvictor redisCacheEvictor;
 
     public void evictDataForUser(Long userId) {
         redisCacheEvictor.evictByPattern("revenue:historyByCategory::*" + userId + ":*");
+    }
+
+    @KafkaListener(topics = "user-account.deleted", groupId = "revenue-history.delete-cache")
+    public void deleteRevenueHistoryCache(UserAccountDeletedEvent event){
+        evictDataForUser(event.userId());
     }
 }
