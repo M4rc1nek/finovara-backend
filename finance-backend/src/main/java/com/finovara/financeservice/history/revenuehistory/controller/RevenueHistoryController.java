282bcafec0a1e751306c1bbb096bae1b5010db45
Marcin Parśniak <marcinekpar92@op.pl>
2026-06-27 10:42:40 +0200

    fix package declaration in RevenueHistoryController from revenuehistory to history.revenuehistory

--- finance-backend/src/main/java/com/finovara/financeservice/history/revenuehistory/controller/RevenueHistoryController.java
+++ finance-backend/src/main/java/com/finovara/financeservice/history/revenuehistory/controller/RevenueHistoryController.java
@@ -1,8 +1,8 @@
-package com.finovara.financeservice.revenuehistory.controller;
+package com.finovara.financeservice.history.revenuehistory.controller;
 
 import com.finovara.financeservice.revenue.dto.RevenueDto;
 import com.finovara.contracts.model.transaction.RevenueCategory;
-import com.finovara.financeservice.revenuehistory.service.RevenueHistoryService;
+import com.finovara.financeservice.history.revenuehistory.service.RevenueHistoryService;
 import com.finovara.contracts.model.PeriodType;
 import com.finovara.financeservice.security.SecurityUtils;
 import lombok.RequiredArgsConstructor;
