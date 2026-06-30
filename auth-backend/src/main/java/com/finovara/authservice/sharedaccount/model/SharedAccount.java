08c6c06e2a27dc3357f7e4a221ea9c5e5a3d55b7
Marcin Parśniak <marcinekpar92@op.pl>
2026-06-30 20:44:40 +0200

    add SharedAccount entity fields and JPA mapping

--- auth-backend/src/main/java/com/finovara/authservice/sharedaccount/model/SharedAccount.java
+++ auth-backend/src/main/java/com/finovara/authservice/sharedaccount/model/SharedAccount.java
@@ -1,4 +1,33 @@
 package com.finovara.authservice.sharedaccount.model;
 
+import com.finovara.authservice.sharedaccount.model.status.SharedAccountStatus;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Table(name = "shared_accounts")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Entity
 public class SharedAccount {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    private SharedAccountStatus status;
+
+    private LocalDateTime createdAt;
+
+    @OneToMany(mappedBy = "sharedAccount", cascade = CascadeType.ALL)
+    private List<SharedAccountMember> members;
 }
+
