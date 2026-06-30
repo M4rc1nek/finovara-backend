0a4235294d8fb827ade4d08c64cb714147364b2b
Marcin Parśniak <marcinekpar92@op.pl>
2026-06-30 20:53:29 +0200

    add kafka listener for shared account invitation reject event

--- notification-backend/src/main/java/com/finovara/notificationservice/notification/service/sharedaccount/invitation/UserRejectSharedAccountInvitationService.java
+++ notification-backend/src/main/java/com/finovara/notificationservice/notification/service/sharedaccount/invitation/UserRejectSharedAccountInvitationService.java
@@ -1,4 +1,33 @@
 package com.finovara.notificationservice.notification.service.sharedaccount.invitation;
 
+import com.finovara.contracts.event.notification.sharedaccount.invitation.UserRejectSharedAccountInvitationEvent;
+import com.finovara.contracts.model.NotificationType;
+import com.finovara.notificationservice.notification.dto.sharedaccount.invitation.UserRejectSharedAccountInvitationDto;
+import com.finovara.notificationservice.notification.service.NotificationPersistenceService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
 public class UserRejectSharedAccountInvitationService {
+
+    private final NotificationPersistenceService notificationPersistenceService;
+
+    @KafkaListener(topics = "user.shared-account.reject-invitation", groupId = "notification-shared-account-invitation-reject")
+    public void handle(UserRejectSharedAccountInvitationEvent event) {
+
+        notificationPersistenceService.save(event.userId(), new UserRejectSharedAccountInvitationDto(
+                NotificationType.USER_REJECT_SHARED_ACCOUNT_INVITATION,
+                LocalDateTime.now(),
+                event.inviteeUsername()
+        ));
+        log.info("Shared account invitation rejected: userId={}, inviteeUsername='{}'", event.userId(), event.inviteeUsername());
+    }
 }
+
