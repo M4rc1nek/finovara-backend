3e17f92e63d0c0dfce206503d135f9324200367d
Marcin Parśniak <marcinekpar92@op.pl>
2026-07-03 12:25:16 +0200

    rename to UserAcceptSharedAccountInvitationConsumer and fix kafka topic and group id

--- notification-backend/src/main/java/com/finovara/notificationservice/notification/consumer/sharedaccount/invitation/UserAcceptSharedAccountInvitationConsumer.java
+++ notification-backend/src/main/java/com/finovara/notificationservice/notification/consumer/sharedaccount/invitation/UserAcceptSharedAccountInvitationConsumer.java
@@ -1,9 +1,9 @@
-package com.finovara.notificationservice.notification.service.sharedaccount.invitation;
+package com.finovara.notificationservice.notification.consumer.sharedaccount.invitation;
 
 import com.finovara.contracts.event.notification.sharedaccount.invitation.UserAcceptSharedAccountInvitationEvent;
 import com.finovara.contracts.model.NotificationType;
 import com.finovara.notificationservice.notification.dto.sharedaccount.invitation.UserAcceptSharedAccountInvitationDto;
-import com.finovara.notificationservice.notification.service.NotificationPersistenceService;
+import com.finovara.notificationservice.notification.NotificationPersistenceService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
@@ -14,11 +14,11 @@ import java.time.LocalDateTime;
 @Service
 @Slf4j
 @RequiredArgsConstructor
-public class UserAcceptSharedAccountInvitationService {
+public class UserAcceptSharedAccountInvitationConsumer {
 
     private final NotificationPersistenceService notificationPersistenceService;
 
-    @KafkaListener(topics = "user.shared-account.accept-invitation", groupId = "notification-shared-account-invitation-accept")
+    @KafkaListener(topics = "notification.shared-account.invitation-accepted", groupId = "notification-shared-account")
     public void handle(UserAcceptSharedAccountInvitationEvent event) {
 
         notificationPersistenceService.save(event.userId(), new UserAcceptSharedAccountInvitationDto(
