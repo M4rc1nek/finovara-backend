fff21547347cd39910866093132a0b87d184f6eb
Marcin Parśniak <marcinekpar92@op.pl>
2026-07-03 12:25:02 +0200

    add hasSharedAccount flag and getProfileImageUrl helper to User

--- auth-backend/src/main/java/com/finovara/authservice/user/model/User.java
+++ auth-backend/src/main/java/com/finovara/authservice/user/model/User.java
@@ -1,6 +1,7 @@
 package com.finovara.authservice.user.model;
 
 import com.finovara.authservice.settings.account.model.AccountSettings;
+import com.finovara.authservice.util.profile.ProfileImageUrlBuilder;
 import jakarta.persistence.*;
 import lombok.*;
 
@@ -21,7 +22,12 @@ public class User {
 
     private String username;
     private String password;
+
+    @Column(nullable = false)
     private boolean passwordSet;
+    @Column(nullable = false)
+    private boolean hasSharedAccount;
+
     private String email;
     private LocalDateTime createdAt;
     @Column(name = "profile_image_path")
@@ -34,4 +40,9 @@ public class User {
 
     @OneToOne(mappedBy = "userAssigned", cascade = CascadeType.ALL)
     private AccountSettings accountSettings;
+
+
+    public String getProfileImageUrl() {
+        return ProfileImageUrlBuilder.buildProfileImageUrl(this.profileImagePath);
+    }
 }
