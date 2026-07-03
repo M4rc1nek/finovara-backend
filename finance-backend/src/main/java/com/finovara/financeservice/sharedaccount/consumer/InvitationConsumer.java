50ee28a77141cb8a70e8cc25352fd1fef3e3cf68
Marcin Parśniak <marcinekpar92@op.pl>
2026-07-03 12:25:07 +0200

    implement InvitationConsumer to create shared wallet on invitation accepted event

--- finance-backend/src/main/java/com/finovara/financeservice/sharedaccount/consumer/InvitationConsumer.java
+++ finance-backend/src/main/java/com/finovara/financeservice/sharedaccount/consumer/InvitationConsumer.java
@@ -1,4 +1,21 @@
 package com.finovara.financeservice.sharedaccount.consumer;
 
-public class sdgsdg {
+import com.finovara.contracts.event.finance.sharedaccount.UsersCreatedSharedAccountEvent;
+import com.finovara.financeservice.sharedaccount.service.wallet.SharedWalletService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class InvitationConsumer {
+    private final SharedWalletService sharedWalletService;
+
+
+    @KafkaListener(topics = "finance.shared-account.invitation-accepted")
+    public void createDefault(UsersCreatedSharedAccountEvent event){
+        sharedWalletService.createSharedWallet(event.inviterUserId(), event.inviteeUserId());
+    }
+
+
 }
