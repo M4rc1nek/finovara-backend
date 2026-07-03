e8e324df3a73a6664aa22f0fca3a8bfaa3ebd00a
Marcin Parśniak <marcinekpar92@op.pl>
2026-07-03 12:25:08 +0200

    add GET endpoint for retrieving shared wallet

--- finance-backend/src/main/java/com/finovara/financeservice/sharedaccount/controller/SharedWalletController.java
+++ finance-backend/src/main/java/com/finovara/financeservice/sharedaccount/controller/SharedWalletController.java
@@ -1,4 +1,22 @@
 package com.finovara.financeservice.sharedaccount.controller;
 
+import com.finovara.financeservice.security.SecurityUtils;
+import com.finovara.financeservice.sharedaccount.dto.wallet.SharedWalletDto;
+import com.finovara.financeservice.sharedaccount.service.wallet.SharedWalletService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
 public class SharedWalletController {
+    private final SharedWalletService sharedWalletService;
+
+
+    @GetMapping("/shared-wallet")
+    public ResponseEntity<SharedWalletDto> getSharedWallet(){
+        return  ResponseEntity.ok(sharedWalletService.getWallet(SecurityUtils.getCurrentUserId()));
+    }
+
 }
