3240f13fab71bf96b5757c4bf62ce72be951c102
Marcin Parśniak <marcinekpar92@op.pl>
2026-07-03 12:24:56 +0200

    fix SharedRole import path and add builder to SharedAccountMember

--- auth-backend/src/main/java/com/finovara/authservice/sharedaccount/model/SharedAccountMember.java
+++ auth-backend/src/main/java/com/finovara/authservice/sharedaccount/model/SharedAccountMember.java
@@ -1,11 +1,8 @@
 package com.finovara.authservice.sharedaccount.model;
 
-import com.finovara.authservice.sharedaccount.model.status.SharedRole;
+import com.finovara.authservice.sharedaccount.model.role.SharedRole;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.time.LocalDateTime;
 
@@ -14,6 +11,7 @@ import java.time.LocalDateTime;
 @Getter
 @Setter
 @Entity
+@Builder
 @Table(name = "shared_account_members")
 public class SharedAccountMember {
 
