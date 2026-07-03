a557161adeb826ee6a8a401e851d02b2ced35e6f
Marcin Parśniak <marcinekpar92@op.pl>
2026-07-03 12:24:53 +0200

    add fields to SharedAccountMemberDto record

--- auth-backend/src/main/java/com/finovara/authservice/sharedaccount/dto/SharedAccountMemberDto.java
+++ auth-backend/src/main/java/com/finovara/authservice/sharedaccount/dto/SharedAccountMemberDto.java
@@ -1,4 +1,13 @@
 package com.finovara.authservice.sharedaccount.dto;
 
-public class SharedAccountMemberDto {
-}
+import com.finovara.authservice.sharedaccount.model.role.SharedRole;
+
+import java.time.LocalDateTime;
+
+public record SharedAccountMemberDto (
+        Long userId,
+        String username,
+        String profileImagePath,
+        SharedRole role,
+        LocalDateTime joinedAt
+) {}
