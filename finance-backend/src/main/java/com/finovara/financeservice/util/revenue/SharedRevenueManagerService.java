bcc0e852299c56d304b5d2d33924cf50108d2281
Marcin Parśniak <marcinekpar92@op.pl>
2026-07-07 10:15:47 +0200

    implement SharedRevenueManagerService with getSharedRevenueOrThrow

--- finance-backend/src/main/java/com/finovara/financeservice/util/revenue/SharedRevenueManagerService.java
+++ finance-backend/src/main/java/com/finovara/financeservice/util/revenue/SharedRevenueManagerService.java
@@ -1,4 +1,18 @@
 package com.finovara.financeservice.util.revenue;
 
+import com.finovara.contracts.exception.notfound.RequestedEntityNotFoundException;
+import com.finovara.financeservice.sharedaccount.model.revenue.SharedRevenue;
+import com.finovara.financeservice.sharedaccount.repository.revenue.SharedRevenueRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class SharedRevenueManagerService {
+    private final SharedRevenueRepository sharedRevenueRepository;
+
+    public SharedRevenue getSharedRevenueOrThrow(Long revenueId) {
+        return sharedRevenueRepository.findById(revenueId)
+                .orElseThrow(() -> new RequestedEntityNotFoundException("Revenue not found"));
+    }
 }
