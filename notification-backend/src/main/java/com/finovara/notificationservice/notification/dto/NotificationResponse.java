d33f9c12d89e6a3ae8a10cc7a3371a1550e80057
Marcin Parśniak <marcinekpar92@op.pl>
2026-07-07 10:16:01 +0200

    register SharedAccountDeletedDto, SharedAccountLeftDto, UserSentSharedAccountInvitationDto as @JsonSubTypes in NotificationResponse

--- notification-backend/src/main/java/com/finovara/notificationservice/notification/dto/NotificationResponse.java
+++ notification-backend/src/main/java/com/finovara/notificationservice/notification/dto/NotificationResponse.java
@@ -6,8 +6,11 @@ import com.finovara.notificationservice.notification.dto.limit.LimitExceededDto;
 import com.finovara.notificationservice.notification.dto.limit.LimitWarningDto;
 import com.finovara.notificationservice.notification.dto.piggybank.PiggyBankWarningDto;
 import com.finovara.contracts.model.NotificationType;
+import com.finovara.notificationservice.notification.dto.sharedaccount.deletion.SharedAccountDeletedDto;
+import com.finovara.notificationservice.notification.dto.sharedaccount.deletion.SharedAccountLeftDto;
 import com.finovara.notificationservice.notification.dto.sharedaccount.invitation.UserAcceptSharedAccountInvitationDto;
 import com.finovara.notificationservice.notification.dto.sharedaccount.invitation.UserRejectSharedAccountInvitationDto;
+import com.finovara.notificationservice.notification.dto.sharedaccount.invitation.UserSentSharedAccountInvitationDto;
 
 import java.time.LocalDateTime;
 
@@ -18,7 +21,10 @@ import java.time.LocalDateTime;
         @JsonSubTypes.Type(value = PiggyBankWarningDto.class, name = "PIGGY_BANK_GOAL_APPROACHING"),
         @JsonSubTypes.Type(value = PiggyBankWarningDto.class, name = "PIGGY_BANK_GOAL_REACHED"),
         @JsonSubTypes.Type(value = UserAcceptSharedAccountInvitationDto.class, name = "USER_ACCEPT_SHARED_ACCOUNT_INVITATION"),
-        @JsonSubTypes.Type(value = UserRejectSharedAccountInvitationDto.class, name = "USER_REJECT_SHARED_ACCOUNT_INVITATION")
+        @JsonSubTypes.Type(value = UserRejectSharedAccountInvitationDto.class, name = "USER_REJECT_SHARED_ACCOUNT_INVITATION"),
+        @JsonSubTypes.Type(value = UserSentSharedAccountInvitationDto.class, name = "USER_SENT_SHARED_ACCOUNT_INVITATION"),
+        @JsonSubTypes.Type(value = SharedAccountDeletedDto.class, name = "SHARED_ACCOUNT_DELETION"),
+        @JsonSubTypes.Type(value = SharedAccountLeftDto.class, name = "SHARED_ACCOUNT_LEFT")
 })
 public interface NotificationResponse {
     NotificationType type();
