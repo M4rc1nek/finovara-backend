f38e2d9baac4a7c7b17a44d41eb01226612ae1ec
Marcin Parśniak <marcinekpar92@op.pl>
2026-07-07 10:15:57 +0200

    update UserAccountDeletedEvent import to new package in ActivityConsumers

--- activity-log-backend/src/main/java/com/finovara/activitylogservice/kafka/ActivityConsumers.java
+++ activity-log-backend/src/main/java/com/finovara/activitylogservice/kafka/ActivityConsumers.java
@@ -16,7 +16,7 @@ import com.finovara.contracts.event.activity.revenue.RevenueActivityEvent;
 import com.finovara.contracts.event.activity.secure.accountchange.activity.AccountChangesActivityEvent;
 import com.finovara.contracts.event.activity.secure.login.activity.LoginActivityEvent;
 import com.finovara.contracts.event.activity.settings.SettingsActivityEvent;
-import com.finovara.contracts.event.user.UserAccountDeletedEvent;
+import com.finovara.contracts.event.user.delete.account.UserAccountDeletedEvent;
 import lombok.RequiredArgsConstructor;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
