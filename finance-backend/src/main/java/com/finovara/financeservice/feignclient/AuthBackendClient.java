898482ab984d93befcc799b3b6cd224d3d69e06e
Marcin Parśniak <marcinekpar92@op.pl>
2026-07-07 10:15:47 +0200

    add getUsername endpoint to AuthBackendClient

--- finance-backend/src/main/java/com/finovara/financeservice/feignclient/AuthBackendClient.java
+++ finance-backend/src/main/java/com/finovara/financeservice/feignclient/AuthBackendClient.java
@@ -2,6 +2,7 @@ package com.finovara.financeservice.feignclient;
 
 import com.finovara.contracts.auth.dto.ConfirmPasswordDto;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -11,4 +12,8 @@ public interface AuthBackendClient {
 
     @PostMapping("/internal/verify-password")
     Void verifyPassword(@RequestHeader("X-User-Id") Long userId, @RequestBody ConfirmPasswordDto dto);
+
+    @GetMapping("/internal/username")
+    String getUsername(@RequestHeader("X-User-Id") Long id);
+
 }
