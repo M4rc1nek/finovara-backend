69a3359a299f4239b306762690698867b4e3acb6
Marcin Parśniak <marcinekpar92@op.pl>
2026-07-07 10:16:03 +0200

    implement UserSentSharedAccountInvitationConsumer listening on notification.shared-account.invitation-sent topic

--- notification-backend/src/main/java/com/finovara/notificationservice/notification/consumer/sharedaccount/invitation/UserSentSharedAccountInvitationConsumer.java
+++ notification-backend/src/main/java/com/finovara/notificationservice/notification/consumer/sharedaccount/invitation/UserSentSharedAccountInvitationConsumer.java
@@ -1,9 +1,9 @@
 package com.finovara.notificationservice.notification.consumer.sharedaccount.invitation;
 
-import com.finovara.contracts.event.notification.sharedaccount.invitation.UserAcceptSharedAccountInvitationEvent;
+import com.finovara.contracts.event.notification.sharedaccount.invitation.UserSentSharedAccountInvitationEvent;
 import com.finovara.contracts.model.NotificationType;
-import com.finovara.notificationservice.notification.dto.sharedaccount.invitation.UserAcceptSharedAccountInvitationDto;
 import com.finovara.notificationservice.notification.NotificationPersistenceService;
+import com.finovara.notificationservice.notification.dto.sharedaccount.invitation.UserSentSharedAccountInvitationDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
@@ -14,19 +14,19 @@ import java.time.LocalDateTime;
 @Service
 @Slf4j
 @RequiredArgsConstructor
-public class UserAcceptSharedAccountInvitationConsumer {
+public class UserSentSharedAccountInvitationConsumer {
 
     private final NotificationPersistenceService notificationPersistenceService;
 
-    @KafkaListener(topics = "notification.shared-account.invitation-accepted", groupId = "notification-shared-account")
-    public void handle(UserAcceptSharedAccountInvitationEvent event) {
+    @KafkaListener(topics = "notification.shared-account.invitation-sent", groupId = "notification-shared-account-invitation-sent")
+    public void handle(UserSentSharedAccountInvitationEvent event) {
 
-        notificationPersistenceService.save(event.userId(), new UserAcceptSharedAccountInvitationDto(
-                NotificationType.USER_ACCEPT_SHARED_ACCOUNT_INVITATION,
+        notificationPersistenceService.save(event.userId(), new UserSentSharedAccountInvitationDto(
+                NotificationType.USER_SENT_SHARED_ACCOUNT_INVITATION,
                 LocalDateTime.now(),
                 event.inviteeUsername()
         ));
-        log.info("Shared account invitation accepted: userId={}, inviteeUsername='{}'", event.userId(), event.inviteeUsername());
+        log.info("Shared account invitation sent to: userId={}, inviteeUsername='{}'", event.userId(), event.inviteeUsername());
     }
 }
 
