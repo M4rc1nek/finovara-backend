14cfad7ad2fe412fd2b164e634ddd19b02f1233a
Marcin Parśniak <marcinekpar92@op.pl>
2026-07-07 10:16:07 +0200

    implement SharedExpenseManagerServiceTest covering found and not found scenarios

--- finance-backend/src/test/java/com/finovara/financeservice/util/expense/SharedExpenseManagerServiceTest.java
+++ finance-backend/src/test/java/com/finovara/financeservice/util/expense/SharedExpenseManagerServiceTest.java
@@ -1,4 +1,50 @@
-import static org.junit.jupiter.api.Assertions.*;
+package com.finovara.financeservice.util.expense;
+
+import com.finovara.contracts.exception.notfound.RequestedEntityNotFoundException;
+import com.finovara.financeservice.sharedaccount.model.expense.SharedExpense;
+import com.finovara.financeservice.sharedaccount.repository.expense.SharedExpenseRepository;
+import org.junit.jupiter.api.Test;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
 class SharedExpenseManagerServiceTest {
-  
+
+    @Mock
+    private SharedExpenseRepository sharedExpenseRepository;
+
+    @InjectMocks
+    private SharedExpenseManagerService sharedExpenseManagerService;
+
+    private final Long EXPENSE_ID = 1L;
+
+    @Test
+    void shouldReturnExpenseById() {
+        SharedExpense expense = new SharedExpense();
+
+        when(sharedExpenseRepository.findById(EXPENSE_ID)).thenReturn(Optional.of(expense));
+
+        sharedExpenseManagerService.getSharedExpenseOrThrow(EXPENSE_ID);
+
+        verify(sharedExpenseRepository).findById(EXPENSE_ID);
+    }
+
+    @Test
+    void shouldThrowExceptionWhenExpenseByIdNotFound() {
+        when(sharedExpenseRepository.findById(EXPENSE_ID)).thenReturn(Optional.empty());
+
+        assertThrows(RequestedEntityNotFoundException.class, () -> sharedExpenseManagerService.getSharedExpenseOrThrow(EXPENSE_ID));
+
+        verify(sharedExpenseRepository).findById(EXPENSE_ID);
+    }
+
 }
