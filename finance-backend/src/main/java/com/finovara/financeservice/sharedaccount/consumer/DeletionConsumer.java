c66f00370d91aaff70b8722dbe59eb098922c4ce
Marcin Parśniak <marcinekpar92@op.pl>
2026-07-07 10:15:50 +0200

    implement DeletionConsumer listening on shared-account.deleted Kafka topic

--- finance-backend/src/main/java/com/finovara/financeservice/sharedaccount/consumer/DeletionConsumer.java
+++ finance-backend/src/main/java/com/finovara/financeservice/sharedaccount/consumer/DeletionConsumer.java
@@ -1,4 +1,19 @@
 package com.finovara.financeservice.sharedaccount.consumer;
 
+import com.finovara.contracts.event.finance.sharedaccount.SharedAccountDeletedEvent;
+import com.finovara.financeservice.sharedaccount.service.deletion.SharedAccountDeletionService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
 public class DeletionConsumer {
+
+    private final SharedAccountDeletionService sharedAccountDeletionService;
+
+    @KafkaListener(topics = "shared-account.deleted")
+    public void deleteDataFromSharedAccount(SharedAccountDeletedEvent event) {
+        sharedAccountDeletionService.deleteData(event);
+    }
 }
