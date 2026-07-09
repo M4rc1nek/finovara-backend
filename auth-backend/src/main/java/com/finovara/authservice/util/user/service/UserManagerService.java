8605c2e8cf086863340d184c286d2b0a3d710c1f
Marcin Parśniak <marcinekpar92@op.pl>
2026-07-09 12:54:18 +0200

    add getUserEmailById method to UserManagerService

--- auth-backend/src/main/java/com/finovara/authservice/util/user/service/UserManagerService.java
+++ auth-backend/src/main/java/com/finovara/authservice/util/user/service/UserManagerService.java
@@ -1,8 +1,8 @@
 package com.finovara.authservice.util.user.service;
 
-import com.finovara.contracts.exception.notfound.RequestedEntityNotFoundException;
 import com.finovara.authservice.user.model.User;
 import com.finovara.authservice.user.repository.UserRepository;
+import com.finovara.contracts.exception.notfound.RequestedEntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -17,13 +17,18 @@ public class UserManagerService {
                 .orElseThrow(() -> new RequestedEntityNotFoundException("User not found"));
     }
 
-    public  User getUserByEmailOrThrow(String email) {
+    public User getUserByEmailOrThrow(String email) {
         return userRepository.findByEmail(email)
                 .orElseThrow(() -> new RequestedEntityNotFoundException("User not found"));
     }
 
-    public String getUsernameByIdOrThrow(Long userId){
+    public String getUsernameByIdOrThrow(Long userId) {
         return userRepository.findUsernameById(userId)
+                .orElseThrow(() -> new RequestedEntityNotFoundException("User not found"));
+    }
+
+    public String getUserEmailById(Long userId) {
+        return userRepository.findEmailById(userId)
                 .orElseThrow(() -> new RequestedEntityNotFoundException("User not found"));
     }
 }
