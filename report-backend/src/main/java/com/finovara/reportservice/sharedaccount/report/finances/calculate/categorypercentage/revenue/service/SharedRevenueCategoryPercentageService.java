7faa48cbbd64de94de43e26cf43db3c637ddc1a3
Marcin Parśniak <marcinekpar92@op.pl>
2026-07-11 17:10:21 +0200

    add ownerId and memberId params to SharedRevenueCategoryPercentageService

--- report-backend/src/main/java/com/finovara/reportservice/sharedaccount/report/finances/calculate/categorypercentage/revenue/service/SharedRevenueCategoryPercentageService.java
+++ report-backend/src/main/java/com/finovara/reportservice/sharedaccount/report/finances/calculate/categorypercentage/revenue/service/SharedRevenueCategoryPercentageService.java
@@ -1,10 +1,10 @@
-package com.finovara.reportservice.sharedaccount.report.finances.categorypercentage.revenue.service;
+package com.finovara.reportservice.sharedaccount.report.finances.calculate.categorypercentage.revenue.service;
 
 import com.finovara.contracts.model.PeriodType;
 import com.finovara.contracts.model.transaction.RevenueCategory;
 import com.finovara.contracts.percentage.CalculatePercentage;
 import com.finovara.reportservice.feignclient.FinanceBackendSharedReportClient;
-import com.finovara.reportservice.sharedaccount.report.finances.categorypercentage.revenue.dto.SharedRevenueCategoryPercentageDto;
+import com.finovara.reportservice.sharedaccount.report.finances.calculate.categorypercentage.revenue.dto.SharedRevenueCategoryPercentageDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
@@ -18,14 +18,14 @@ public class SharedRevenueCategoryPercentageService {
 
     private final FinanceBackendSharedReportClient reportClient;
 
-    @Cacheable(value = "report:sharedRevenuePercentageByCategory", key = "#userId + ':' + #category + ':' + #periodType")
-    public SharedRevenueCategoryPercentageDto getRevenuePercentageByCategoryReport(Long userId, RevenueCategory category, PeriodType periodType) {
+    @Cacheable(value = "report:sharedRevenuePercentageByCategory", key = "#ownerId + ':' + #memberId + ':' + #category + ':' + #periodType")
+    public SharedRevenueCategoryPercentageDto getRevenuePercentageByCategoryReport(Long ownerId, Long memberId, RevenueCategory category, PeriodType periodType) {
 
         LocalDate to = LocalDate.now();
         LocalDate from = periodType.getStartDate(to);
 
-        BigDecimal total = reportClient.sumRevenues(userId, from, to);
-        BigDecimal inCategory = reportClient.revenuesByCategory(userId, from, to, category);
+        BigDecimal total = reportClient.sumRevenues(ownerId, memberId, from, to);
+        BigDecimal inCategory = reportClient.revenuesByCategory(ownerId, memberId, from, to, category);
         BigDecimal percentage = CalculatePercentage.calculatePercentage(inCategory, total);
 
         return new SharedRevenueCategoryPercentageDto(percentage, category);
