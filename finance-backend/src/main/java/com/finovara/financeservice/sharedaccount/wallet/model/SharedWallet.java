31c8b3903a156c97c010338af922c4f10752be03
Marcin Parśniak <marcinekpar92@op.pl>
2026-07-13 12:32:25 +0200

    move SharedWallet into sharedaccount.wallet.model package

--- finance-backend/src/main/java/com/finovara/financeservice/sharedaccount/wallet/model/SharedWallet.java
+++ finance-backend/src/main/java/com/finovara/financeservice/sharedaccount/wallet/model/SharedWallet.java
@@ -1,4 +1,4 @@
-package com.finovara.financeservice.sharedaccount.model.wallet;
+package com.finovara.financeservice.sharedaccount.wallet.model;
 
 import com.finovara.contracts.exception.badrequest.InvalidInputException;
 import jakarta.persistence.*;
