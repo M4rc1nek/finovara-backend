30d7e29d8ac07311d04b2e2edaab5b11009aa055
Marcin Parśniak <marcinekpar92@op.pl>
2026-07-13 12:32:29 +0200

    move SharedPiggyBankManager and add getPiggyBankByUserId method

--- finance-backend/src/main/java/com/finovara/financeservice/util/piggybank/manager/SharedPiggyBankManager.java
+++ finance-backend/src/main/java/com/finovara/financeservice/util/piggybank/manager/SharedPiggyBankManager.java
@@ -1,4 +1,18 @@
-package com.finovara.financeservice.util.piggybank;
+package com.finovara.financeservice.util.piggybank.manager;
 
+import com.finovara.contracts.exception.notfound.RequestedEntityNotFoundException;
+import com.finovara.financeservice.sharedaccount.piggybank.model.SharedPiggyBank;
+import com.finovara.financeservice.sharedaccount.piggybank.repository.SharedPiggyBankRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class SharedPiggyBankManager {
+    private final SharedPiggyBankRepository piggyBankRepository;
+
+    public SharedPiggyBank getPiggyBankByUserId(Long piggyBankId, Long userId) {
+        return piggyBankRepository.findByIdAndUserId(piggyBankId, userId)
+                .orElseThrow(() -> new RequestedEntityNotFoundException("Piggy Bank not found"));
+    }
 }
