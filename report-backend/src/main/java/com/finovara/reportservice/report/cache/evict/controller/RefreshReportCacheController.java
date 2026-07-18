ec1052272b62b7f958501d7befc0baee199ec150
Marcin Parśniak <marcinekpar92@op.pl>
2026-07-18 14:44:16 +0200

    fix RefreshReportCacheController request mapping to start with leading slash

--- report-backend/src/main/java/com/finovara/reportservice/report/cache/evict/controller/RefreshReportCacheController.java
+++ report-backend/src/main/java/com/finovara/reportservice/report/cache/evict/controller/RefreshReportCacheController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("api/report-cache")
+@RequestMapping("/api/report-cache")
 @RequiredArgsConstructor
 public class RefreshReportCacheController {
 
