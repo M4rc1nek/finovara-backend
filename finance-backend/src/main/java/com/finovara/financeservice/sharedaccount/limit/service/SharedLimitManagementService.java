b79159ea2499b8dc5a2628aa26367f125e6629a5
Marcin Parśniak <marcinekpar92@op.pl>
2026-07-18 14:44:11 +0200

    rename LimitManagementService to SharedLimitManagementService and rename all public methods to use Shared prefix

--- finance-backend/src/main/java/com/finovara/financeservice/sharedaccount/limit/service/SharedLimitManagementService.java
+++ finance-backend/src/main/java/com/finovara/financeservice/sharedaccount/limit/service/SharedLimitManagementService.java
@@ -1,41 +1,46 @@
 package com.finovara.financeservice.sharedaccount.limit.service;
 
-import com.finovara.contracts.datadeletable.UserDataDeletable;
-import com.finovara.contracts.event.activity.limit.LimitActivityEvent;
 import com.finovara.contracts.exception.conflict.EntityAlreadyExistsException;
 import com.finovara.contracts.exception.notfound.RequestedEntityNotFoundException;
-import com.finovara.contracts.model.activity.LimitActivityType;
-import com.finovara.contracts.outbox.OutboxService;
 import com.finovara.financeservice.sharedaccount.limit.dto.SharedLimitDto;
 import com.finovara.financeservice.sharedaccount.limit.dto.SharedLimitStatsDto;
 import com.finovara.financeservice.sharedaccount.limit.model.SharedLimit;
-import com.finovara.financeservice.sharedaccount.limit.repository.LimitRepository;
+import com.finovara.financeservice.sharedaccount.limit.repository.SharedLimitRepository;
+import com.finovara.financeservice.sharedaccount.participants.SharedAccountParticipantsResponse;
+import com.finovara.financeservice.sharedaccount.participants.SharedAccountParticipantsService;
+import com.finovara.financeservice.util.limit.validator.LimitExpensesValidator;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.math.BigDecimal;
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.util.List;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
-public class LimitManagementService implements UserDataDeletable {
-    private final LimitRepository limitRepository;
-    private final LimitCalculateService limitCalculateService;
-    private final OutboxService outboxService;
+public class SharedLimitManagementService {
+    private final SharedLimitRepository sharedLimitRepository;
+    private final SharedLimitCalculateService sharedLimitCalculateService;
+    private final SharedAccountParticipantsService sharedAccountParticipantsService;
+    private final LimitExpensesValidator limitExpensesValidator;
 
     @Transactional
-    public Long createLimit(SharedLimitDto limitDto, Long userId) {
+    public Long createSharedLimit(SharedLimitDto limitDto, Long userId) {
+        limitExpensesValidator.validateCurrentSharedExpensesDoNotExceedLimit(userId, limitDto);
+
+        SharedAccountParticipantsResponse sharedAccountParticipantsResponse = sharedAccountParticipantsService.getParticipants(userId);
         if (limitDto.category() == null) {
-            limitRepository.findGeneralLimit(userId, limitDto.periodType())
-                    .ifPresent(limit -> { throw new EntityAlreadyExistsException("General limit already exists"); });
+            sharedLimitRepository.findGeneralLimit(userId, limitDto.periodType())
+                    .ifPresent(limit -> {
+                        throw new EntityAlreadyExistsException("General limit already exists");
+                    });
         } else {
-            limitRepository.findCategoryLimit(userId, limitDto.periodType(), limitDto.category())
-                    .ifPresent(limit -> { throw new EntityAlreadyExistsException("Category limit already exists"); });
+            sharedLimitRepository.findCategoryLimit(userId, limitDto.periodType(), limitDto.category())
+                    .ifPresent(limit -> {
+                        throw new EntityAlreadyExistsException("Category limit already exists");
+                    });
         }
 
         SharedLimit limit = SharedLimit.builder()
@@ -43,63 +48,47 @@ public class LimitManagementService implements UserDataDeletable {
                 .category(limitDto.category())
                 .amount(limitDto.amount())
                 .isActive(true)
-                .userId(userId)
+                .ownerId(sharedAccountParticipantsResponse.ownerId())
+                .memberId(sharedAccountParticipantsResponse.memberId())
                 .build();
 
-        SharedLimit savedLimit = limitRepository.save(limit);
-        outboxService.save("Limit", savedLimit.getId().toString(), "activity.limit",
-                new LimitActivityEvent(userId, LimitActivityType.ADDED_LIMIT, limit.getPeriodType() == null ? null : limit.getPeriodType().name(), limit.getAmount(), null, LocalDateTime.now()));
+        SharedLimit savedLimit = sharedLimitRepository.save(limit);
 
         return savedLimit.getId();
     }
 
     @Transactional
-    public Long editLimit(SharedLimitDto limitDto, Long limitId, Long userId) {
-        SharedLimit limit = limitRepository.findByIdAndUserId(userId, limitId)
+    public Long editSharedLimit(SharedLimitDto limitDto, Long limitId, Long userId) {
+        SharedLimit limit = sharedLimitRepository.findByIdAndUserId(userId, limitId)
                 .orElseThrow(() -> new RequestedEntityNotFoundException("Active limit not found"));
 
-        if (!limit.getUserId().equals(userId)) {
-            throw new RequestedEntityNotFoundException("Active Limit not found for this user");
-        }
-
-        BigDecimal oldLimitAmount = limit.getAmount();
+        limitExpensesValidator.validateCurrentSharedExpensesDoNotExceedLimit(userId, limitDto);
 
         limit.setPeriodType(limitDto.periodType());
         limit.setCategory(limitDto.category());
         limit.setAmount(limitDto.amount());
 
-        limitRepository.save(limit);
-        outboxService.save("Limit", limitId.toString(), "activity.limit",
-                new LimitActivityEvent(userId, LimitActivityType.EDITED_LIMIT, limit.getPeriodType() == null ? null : limit.getPeriodType().name(), limit.getAmount(), oldLimitAmount, LocalDateTime.now()));
+        sharedLimitRepository.save(limit);
 
         return limitId;
     }
 
-    public List<SharedLimitStatsDto> getLimitStats(Long userId) {
-        List<SharedLimit> limits = limitRepository.findAllByUserId(userId);
+    public List<SharedLimitStatsDto> getSharedLimitStats(Long userId) {
+        List<SharedLimit> limits = sharedLimitRepository.findAllByUserId(userId);
         LocalDate today = LocalDate.now();
 
         return limits.stream()
                 .map(limit ->
-                        limitCalculateService.calculateLimitStats(userId, limit.getId(), today))
+                        sharedLimitCalculateService.calculateLimitStats(userId, limit.getId(), today))
                 .toList();
     }
 
     @Transactional
-    public void deleteLimit(Long userId, Long limitId) {
-        SharedLimit limit = limitRepository.findByIdAndUserId(userId, limitId)
+    public void deleteSharedLimit(Long userId, Long limitId) {
+        SharedLimit limit = sharedLimitRepository.findByIdAndUserId(userId, limitId)
                 .orElseThrow(() -> new RequestedEntityNotFoundException("Active limit not found"));
 
-        outboxService.save("Limit", limitId.toString(), "activity.limit",
-                new LimitActivityEvent(userId, LimitActivityType.DELETED_LIMIT, limit.getPeriodType() == null ? null : limit.getPeriodType().name(), limit.getAmount(), null, LocalDateTime.now()));
-        limitRepository.delete(limit);
-    }
-
-    @Override
-    @Transactional
-    public void deleteByUserId(Long userId) {
-        limitRepository.deleteByUserId(userId);
-        log.info("Deleted limit for userId={}", userId);
+        sharedLimitRepository.delete(limit);
     }
 
 }
