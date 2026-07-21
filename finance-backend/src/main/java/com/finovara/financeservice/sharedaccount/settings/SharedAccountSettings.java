cf5965d355123905911dbf093647ee4def2c62a5
Marcin Parśniak <marcinekpar92@op.pl>
2026-07-21 14:45:39 +0200

    rename walletSpendLimitEnabled to spendControlEnabled, walletSpendLimitPercentage to spendControlPercentage and add expenseAnalysisEnabled field

--- finance-backend/src/main/java/com/finovara/financeservice/sharedaccount/settings/SharedAccountSettings.java
+++ finance-backend/src/main/java/com/finovara/financeservice/sharedaccount/settings/SharedAccountSettings.java
@@ -1,25 +1,30 @@
-package com.finovara.financeservice.sharedaccount.settings.model;
+package com.finovara.financeservice.sharedaccount.settings;
 
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.math.BigDecimal;
+
+@Builder
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
 @Setter
 @Getter
 @Table(name = "shared_settings")
-public class SharedAccountSettings{
+public class SharedAccountSettings {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(nullable = false)
-    private boolean walletSpendLimitEnabled;
+    private boolean spendControlEnabled;
 
-    private int walletSpendLimitPercentage;
+    private BigDecimal spendControlPercentage;
 
+    @Column(nullable = false)
+    private boolean expenseAnalysisEnabled;
 
     @Column(nullable = false)
     private Long ownerId;
