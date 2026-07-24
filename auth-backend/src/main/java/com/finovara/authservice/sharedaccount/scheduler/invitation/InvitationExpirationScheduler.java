4a726ed85ac4486d6ea6deac6819cc832b2f9c7f
Marcin Parśniak <marcinekpar92@op.pl>
2026-07-24 15:06:03 +0200

    implement scheduled job for expiring shared account invitations

--- auth-backend/src/main/java/com/finovara/authservice/sharedaccount/scheduler/invitation/InvitationExpirationScheduler.java
+++ auth-backend/src/main/java/com/finovara/authservice/sharedaccount/scheduler/invitation/InvitationExpirationScheduler.java
@@ -1,4 +1,23 @@
 package com.finovara.authservice.sharedaccount.scheduler.invitation;
 
+import com.finovara.authservice.sharedaccount.processor.invitation.InvitationExpirationProcessor;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import net.javacrumbs.shedlock.spring.annotation.SchedulerLock;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+@RequiredArgsConstructor
 public class InvitationExpirationScheduler {
+
+    private final InvitationExpirationProcessor invitationExpirationProcessor;
+
+    @Scheduled(cron = "${scheduler.shared-account.invitation.expire-cron}")
+    @SchedulerLock(name = "expireOverdueInvitations", lockAtMostFor = "10m", lockAtLeastFor = "30s")
+    public void expireOverdueInvitations() {
+        invitationExpirationProcessor.expireOverdueInvitations();
+        log.info("Invitations have expired and have been deleted");
+    }
 }
