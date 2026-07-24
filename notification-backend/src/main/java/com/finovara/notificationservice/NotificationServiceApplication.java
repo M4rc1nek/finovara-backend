41c8579a2f98979d5e6fcdf4f1c1f420241d406c
Marcin Parśniak <marcinekpar92@op.pl>
2026-07-24 15:06:33 +0200

    enable feign clients in notification service application

--- notification-backend/src/main/java/com/finovara/notificationservice/NotificationServiceApplication.java
+++ notification-backend/src/main/java/com/finovara/notificationservice/NotificationServiceApplication.java
@@ -2,6 +2,7 @@ package com.finovara.notificationservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
@@ -9,6 +10,7 @@ import java.util.TimeZone;
 
 @EnableAsync
 @EnableScheduling
+@EnableFeignClients
 @SpringBootApplication
 public class NotificationServiceApplication {
 
