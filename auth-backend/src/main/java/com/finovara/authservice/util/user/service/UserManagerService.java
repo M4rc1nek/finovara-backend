d29ae0efd0ef148cec1039262a23ad7aff761ece
Marcin Parśniak <marcinekpar92@op.pl>
2026-07-24 15:06:08 +0200

    add getUserData and getUserDataWithProfileImg to UserManagerService

--- auth-backend/src/main/java/com/finovara/authservice/util/user/service/UserManagerService.java
+++ auth-backend/src/main/java/com/finovara/authservice/util/user/service/UserManagerService.java
@@ -1,16 +1,34 @@
 package com.finovara.authservice.util.user.service;
 
+import com.finovara.authservice.user.dto.UserDataDto;
 import com.finovara.authservice.user.model.User;
 import com.finovara.authservice.user.repository.UserRepository;
+import com.finovara.contracts.auth.dto.UserDataResponse;
 import com.finovara.contracts.exception.notfound.RequestedEntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class UserManagerService {
 
     private final UserRepository userRepository;
+
+    public UserDataResponse getUserData(Long userId){
+        Optional<String> email = userRepository.findEmailById(userId);
+        Optional<String> username = userRepository.findUsernameById(userId);
+        return new UserDataResponse(userId, username, email);
+    }
+
+    public UserDataDto getUserDataWithProfileImg(Long userId) {
+        return userRepository.findBasicInfoByIds(List.of(userId))
+                .stream()
+                .findFirst()
+                .orElseThrow(() -> new RequestedEntityNotFoundException("User not found"));
+    }
 
     public User getUserByIdOrThrow(Long userId) {
         return userRepository.findById(userId)
