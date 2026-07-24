b54162b092b503745643e846e2d9182e6ed604b2
Marcin Parśniak <marcinekpar92@op.pl>
2026-07-24 15:06:27 +0200

    implement goal achieved notification settings get/patch endpoints

--- finance-backend/src/main/java/com/finovara/financeservice/sharedaccount/settings/piggybank/goalachieved/controller/GoalAchievedNotificationController.java
+++ finance-backend/src/main/java/com/finovara/financeservice/sharedaccount/settings/piggybank/goalachieved/controller/GoalAchievedNotificationController.java
@@ -1,4 +1,27 @@
 package com.finovara.financeservice.sharedaccount.settings.piggybank.goalachieved.controller;
 
+import com.finovara.financeservice.security.SecurityUtils;
+import com.finovara.financeservice.sharedaccount.settings.piggybank.goalachieved.dto.GoalAchievedNotificationDto;
+import com.finovara.financeservice.sharedaccount.settings.piggybank.goalachieved.service.GoalAchievedNotificationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/shared-accounts/settings/piggy-bank-goal-achieved-notification")
+@RequiredArgsConstructor
 public class GoalAchievedNotificationController {
+
+    private final GoalAchievedNotificationService goalAchievedNotificationService;
+
+    @PatchMapping
+    public ResponseEntity<Void> saveGoalAchievedNotification(@RequestBody GoalAchievedNotificationDto goalAchievedNotificationDto) {
+        goalAchievedNotificationService.saveGoalAchievedNotification(SecurityUtils.getCurrentUserId(), goalAchievedNotificationDto);
+        return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping
+    public ResponseEntity<GoalAchievedNotificationDto> getGoalAchievedNotification() {
+        return ResponseEntity.ok(goalAchievedNotificationService.getGoalAchievedNotification(SecurityUtils.getCurrentUserId()));
+    }
 }
