7092f2d1e4cdc9d122715baa01a4d4bc1f35caa9
Marcin Parśniak <marcinekpar92@op.pl>
2026-07-24 15:06:37 +0200

    implement GoalAchievedNotificationConsumer to dispatch email handler

--- notification-backend/src/main/java/com/finovara/notificationservice/notificationemail/sharedaccount/piggybank/goalachieved/consumer/GoalAchievedNotificationConsumer.java
+++ notification-backend/src/main/java/com/finovara/notificationservice/notificationemail/sharedaccount/piggybank/goalachieved/consumer/GoalAchievedNotificationConsumer.java
@@ -1,4 +1,20 @@
 package com.finovara.notificationservice.notificationemail.sharedaccount.piggybank.goalachieved.consumer;
 
+import com.finovara.contracts.event.finance.sharedaccount.GoalAchievedNotificationEvent;
+import com.finovara.notificationservice.notificationemail.sharedaccount.piggybank.goalachieved.service.GoalAchievedNotificationHandler;
+import lombok.RequiredArgsConstructor;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
 public class GoalAchievedNotificationConsumer {
+
+    private final GoalAchievedNotificationHandler goalAchievedNotificationHandler;
+
+    @KafkaListener(topics = "notification.shared-account.piggy-bank-goal-achieved", groupId = "notification-email-service")
+    public void consume(GoalAchievedNotificationEvent event) {
+        goalAchievedNotificationHandler.handle(event);
+    }
+
 }
