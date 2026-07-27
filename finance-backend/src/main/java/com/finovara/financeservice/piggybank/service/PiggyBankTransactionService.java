5e001591699c21af8e86937abf543f7dc26a7a51
Marcin Parśniak <marcinekpar92@op.pl>
2026-07-27 11:14:20 +0200

    inject GoalPlannerService into PiggyBankTransactionService

--- finance-backend/src/main/java/com/finovara/financeservice/piggybank/service/PiggyBankTransactionService.java
+++ finance-backend/src/main/java/com/finovara/financeservice/piggybank/service/PiggyBankTransactionService.java
@@ -4,6 +4,7 @@ import com.finovara.contracts.event.activity.piggybank.PiggyBankActivityEvent;
 import com.finovara.contracts.event.notification.piggybank.PiggyBankProgressEvent;
 import com.finovara.contracts.model.activity.PiggyBankActivityType;
 import com.finovara.contracts.outbox.OutboxService;
+import com.finovara.financeservice.piggybank.goalplanner.service.GoalPlannerService;
 import com.finovara.financeservice.piggybank.model.PiggyBank;
 import com.finovara.financeservice.settings.piggybank.completion.service.GoalCompletionService;
 import com.finovara.financeservice.util.piggybank.PiggyBankCalculator;
@@ -25,6 +26,7 @@ public class PiggyBankTransactionService {
     private final PiggyBankManagerService piggyBankManagerService;
     private final OutboxService outboxService;
     private final GoalCompletionService goalCompletionService;
+    private final GoalPlannerService goalPlannerService;
     private final WalletService walletService;
 
     @Transactional
@@ -46,6 +48,7 @@ public class PiggyBankTransactionService {
         if (completed) {
             goalCompletionService.handleGoalCompletion(userId);
         }
+        goalPlannerService.checkAndMarkGoalCompletion(piggyBank.getGoalPlanner());
     }
 
     @Transactional
@@ -64,6 +67,7 @@ public class PiggyBankTransactionService {
                 new PiggyBankActivityEvent(userId, PiggyBankActivityType.AMOUNT_REMOVED_FROM_PIGGY_BANK, piggyBank.getName(), piggyBank.getGoalType(), piggyBank.getGoalAmount(), amount, LocalDateTime.now()));
         outboxService.save("PiggyBank", piggyBankId.toString(), "piggybank.calculate-progress",
                 new PiggyBankProgressEvent(userId, piggyBankId, percentage, piggyBank.getGoalType(), piggyBank.getName()));
+        goalPlannerService.checkAndMarkGoalCompletion(piggyBank.getGoalPlanner());
     }
 
     private BigDecimal calculatePercentage(PiggyBank piggyBank) {
