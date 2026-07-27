51026f507af45c8cabcb84a7f3fc758f185285cb
Marcin Parśniak <marcinekpar92@op.pl>
2026-07-27 11:14:51 +0200

    implement GoalPlanner entity with topic, targetDate, createdAt, completedAt, userId and piggyBankAssigned fields

--- finance-backend/src/main/java/com/finovara/financeservice/piggybank/goalplanner/model/GoalPlanner.java
+++ finance-backend/src/main/java/com/finovara/financeservice/piggybank/goalplanner/model/GoalPlanner.java
@@ -1,4 +1,34 @@
 package com.finovara.financeservice.piggybank.goalplanner.model;
 
+import com.finovara.financeservice.piggybank.model.PiggyBank;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Setter
+@Getter
+@Table(name = "goalplanner")
 public class GoalPlanner {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String topic;
+    private LocalDate targetDate;
+    private LocalDateTime createdAt;
+    private LocalDateTime completedAt;
+
+    @Column(nullable = false)
+    private Long userId;
+
+    @OneToOne
+    @JoinColumn(name = "piggy_bank_id", unique = true)
+    private PiggyBank piggyBankAssigned;
 }
