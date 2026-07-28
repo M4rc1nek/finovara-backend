162da3e2f919a0ae7c80f33e490380b35de90241
Marcin Parśniak <marcinekpar92@op.pl>
2026-07-28 13:33:23 +0200

    implement RecurringOccurrenceDto fields

--- finance-backend/src/main/java/com/finovara/financeservice/settings/finances/recurring/dto/RecurringOccurrenceDto.java
+++ finance-backend/src/main/java/com/finovara/financeservice/settings/finances/recurring/dto/RecurringOccurrenceDto.java
@@ -1,4 +1,18 @@
 package com.finovara.financeservice.settings.finances.recurring.dto;
 
-public record RecurringOccurrenceDto() {
-}
+import com.finovara.contracts.model.transaction.ExpenseCategory;
+import com.finovara.contracts.model.transaction.RevenueCategory;
+import com.finovara.financeservice.settings.finances.recurring.model.RecurringType;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public record RecurringOccurrenceDto(
+        LocalDate date,
+        RecurringType type,
+        BigDecimal amount,
+        ExpenseCategory expenseCategory,
+        RevenueCategory revenueCategory,
+        Long recurringSettingsId,
+        Long piggyBankId
+) {}
