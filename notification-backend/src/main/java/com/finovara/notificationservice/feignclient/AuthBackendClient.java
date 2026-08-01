59adfcd0ea0ed392967e1d19abf32a64e20c2d78
Marcin Parśniak <marcinekpar92@op.pl>
2026-08-01 16:36:23 +0200

    add confirm authorization code feign client method

--- notification-backend/src/main/java/com/finovara/notificationservice/feignclient/AuthBackendClient.java
+++ notification-backend/src/main/java/com/finovara/notificationservice/feignclient/AuthBackendClient.java
@@ -1,8 +1,11 @@
 package com.finovara.notificationservice.feignclient;
 
 import com.finovara.contracts.auth.dto.UserDataResponse;
+import com.finovara.contracts.auth.dto.ConfirmAuthorizationCodeDto;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 
 @FeignClient(name = "auth-backend", url = "${auth-backend.url}")
@@ -10,4 +13,7 @@ public interface AuthBackendClient {
 
     @GetMapping("internal/user-data")
     UserDataResponse getUserEmailData(@RequestHeader("X-User-Id") Long userId);
+
+    @PostMapping("/internal/confirm-authorization-code")
+    Void confirmAuthorizationCode(@RequestHeader("X-User-Id") Long userId, @RequestBody ConfirmAuthorizationCodeDto dto);
 }
