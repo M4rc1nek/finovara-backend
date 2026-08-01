382d9b748711df2884aa7a62255a94efb92748c5
Marcin Parśniak <marcinekpar92@op.pl>
2026-08-01 16:35:41 +0200

    implement additional authorization controller endpoints

--- auth-backend/src/main/java/com/finovara/authservice/settings/security/operationauthorization/controller/AdditionalAuthorizationController.java
+++ auth-backend/src/main/java/com/finovara/authservice/settings/security/operationauthorization/controller/AdditionalAuthorizationController.java
@@ -1,4 +1,36 @@
 package com.finovara.authservice.settings.security.operationauthorization.controller;
 
+import com.finovara.authservice.security.SecurityUtils;
+import com.finovara.authservice.settings.security.operationauthorization.dto.AdditionalAuthorizationDto;
+import com.finovara.authservice.settings.security.operationauthorization.dto.AdditionalAuthorizationRequest;
+import com.finovara.contracts.auth.dto.ConfirmAuthorizationCodeDto;
+import com.finovara.authservice.settings.security.operationauthorization.service.AdditionalAuthorizationService;
+import com.finovara.contracts.auth.dto.ConfirmPasswordDto;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/security-settings/additional-authorization")
+@RequiredArgsConstructor
 public class AdditionalAuthorizationController {
+
+    private final AdditionalAuthorizationService additionalAuthorizationService;
+
+    @PostMapping
+    public ResponseEntity<ConfirmAuthorizationCodeDto> saveAdditionalAuthorization(@Valid @RequestBody AdditionalAuthorizationRequest request) {
+        return ResponseEntity.ok(additionalAuthorizationService.saveAdditionalAuthorization(SecurityUtils.getCurrentUserId(), request));
+    }
+
+    @GetMapping
+    public ResponseEntity<AdditionalAuthorizationDto> getAdditionalAuthorizationSettings() {
+        return ResponseEntity.ok(additionalAuthorizationService.getAdditionalAuthorizationSettings(SecurityUtils.getCurrentUserId()));
+    }
+
+    @PatchMapping("/regenerate")
+    public ResponseEntity<ConfirmAuthorizationCodeDto> regenerateCode(@RequestBody ConfirmPasswordDto confirmPasswordDto) {
+        return ResponseEntity.ok(additionalAuthorizationService.regenerateCode(SecurityUtils.getCurrentUserId(), confirmPasswordDto));
+    }
+
 }
