b881d391d5486b9de878c164558c9fda20b8e937
Marcin Parśniak <marcinekpar92@op.pl>
2026-08-01 16:35:54 +0200

    add confirm authorization code dto field to expense request

--- finance-backend/src/main/java/com/finovara/financeservice/expense/dto/ExpenseRequestDto.java
+++ finance-backend/src/main/java/com/finovara/financeservice/expense/dto/ExpenseRequestDto.java
@@ -1,12 +1,14 @@
 package com.finovara.financeservice.expense.dto;
 
 import com.finovara.contracts.auth.dto.ConfirmPasswordDto;
+import com.finovara.contracts.auth.dto.ConfirmAuthorizationCodeDto;
 import com.finovara.financeservice.settings.finances.expense.quantitylimit.dto.CountQuantityLimitDto;
 import jakarta.validation.Valid;
 
 public record ExpenseRequestDto(
         @Valid ExpenseDto expenseDto,
         ConfirmPasswordDto confirmPasswordDto,
+        ConfirmAuthorizationCodeDto confirmAuthorizationCodeDto,
         @Valid CountQuantityLimitDto countQuantityLimitDto
 ) {
 }
