70943aaa6ef142b3041b684294736e46c3ef0b0b
Marcin Parśniak <marcinekpar92@op.pl>
2026-08-01 16:36:03 +0200

    add authorizationCode field to count quantity limit dto

--- finance-backend/src/main/java/com/finovara/financeservice/settings/finances/expense/quantitylimit/dto/CountQuantityLimitDto.java
+++ finance-backend/src/main/java/com/finovara/financeservice/settings/finances/expense/quantitylimit/dto/CountQuantityLimitDto.java
@@ -7,6 +7,7 @@ import jakarta.validation.constraints.Min;
 public record CountQuantityLimitDto(
         Boolean expenseCountLimitEnabled,
         PeriodType periodType,
-        @Min(1) @Max(100) int numberOfQuantityLimit
+        @Min(1) @Max(100) int numberOfQuantityLimit,
+        String authorizationCode
 ) {
 }
