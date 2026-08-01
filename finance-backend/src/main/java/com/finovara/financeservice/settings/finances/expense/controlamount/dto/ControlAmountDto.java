f57f96ea2eb4a0b3c4e297c8bb097dbf3e4699f7
Marcin Parśniak <marcinekpar92@op.pl>
2026-08-01 16:36:02 +0200

    add authorizationCode field to control amount dto

--- finance-backend/src/main/java/com/finovara/financeservice/settings/finances/expense/controlamount/dto/ControlAmountDto.java
+++ finance-backend/src/main/java/com/finovara/financeservice/settings/finances/expense/controlamount/dto/ControlAmountDto.java
@@ -7,6 +7,7 @@ import java.math.BigDecimal;
 
 public record ControlAmountDto(
         Boolean expenseAmountThresholdEnabled,
-       @DecimalMin("1") @DecimalMax("1000000") BigDecimal blockedAmount
+        @DecimalMin("1") @DecimalMax("1000000") BigDecimal blockedAmount,
+        String authorizationCode
 ) {
 }
