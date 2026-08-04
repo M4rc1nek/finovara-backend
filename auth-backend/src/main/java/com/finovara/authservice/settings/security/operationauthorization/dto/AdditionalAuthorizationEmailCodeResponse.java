b319a992960fd2e54cf33b6bd4ced6808cc6cc5e
Marcin Parśniak <marcinekpar92@op.pl>
2026-08-04 13:37:12 +0200

    rename ConfirmAuthorizationEmailCodeResultDto to AdditionalAuthorizationEmailCodeResponse

--- auth-backend/src/main/java/com/finovara/authservice/settings/security/operationauthorization/dto/AdditionalAuthorizationEmailCodeResponse.java
+++ auth-backend/src/main/java/com/finovara/authservice/settings/security/operationauthorization/dto/AdditionalAuthorizationEmailCodeResponse.java
@@ -3,7 +3,7 @@ package com.finovara.authservice.settings.security.operationauthorization.dto;
 import com.finovara.authservice.settings.account.dto.AttemptsDto;
 import com.finovara.contracts.auth.dto.ConfirmAuthorizationCodeDto;
 
-public record ConfirmAuthorizationEmailCodeResultDto(
+public record AdditionalAuthorizationEmailCodeResponse(
         ConfirmAuthorizationCodeDto authorizationCode,
         AttemptsDto attempts
 ) {}
