3486ea134e16fc62c6a38a84a069f7b0189a59f5
Marcin Parśniak <marcinekpar92@op.pl>
2026-08-10 14:35:02 +0200

    update import to authorization.dto package in AdditionalAuthorizationEmailCodeResponse

--- auth-backend/src/main/java/com/finovara/authservice/settings/security/operationauthorization/dto/AdditionalAuthorizationEmailCodeResponse.java
+++ auth-backend/src/main/java/com/finovara/authservice/settings/security/operationauthorization/dto/AdditionalAuthorizationEmailCodeResponse.java
@@ -1,7 +1,7 @@
 package com.finovara.authservice.settings.security.operationauthorization.dto;
 
 import com.finovara.authservice.settings.account.dto.AttemptsDto;
-import com.finovara.contracts.auth.dto.ConfirmAuthorizationCodeDto;
+import com.finovara.contracts.authorization.dto.ConfirmAuthorizationCodeDto;
 
 public record AdditionalAuthorizationEmailCodeResponse(
         ConfirmAuthorizationCodeDto authorizationCode,
