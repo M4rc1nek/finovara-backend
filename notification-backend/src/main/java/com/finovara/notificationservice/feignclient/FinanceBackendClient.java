0bc4258799ebfb0d6800384268513d9e1e22c367
Marcin Parśniak <marcinekpar92@op.pl>
2026-08-19 20:51:03 +0200

    implement FinanceBackendClient to fetch weekly digest reports

--- notification-backend/src/main/java/com/finovara/notificationservice/feignclient/FinanceBackendClient.java
+++ notification-backend/src/main/java/com/finovara/notificationservice/feignclient/FinanceBackendClient.java
@@ -1,4 +1,14 @@
 package com.finovara.notificationservice.feignclient;
 
+import com.finovara.contracts.notification.email.digest.report.WeeklyDigestReportDto;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@FeignClient(name = "finance-backend", url = "${finance-backend.url}")
 public interface FinanceBackendClient {
+
+    @GetMapping("/internal/digest/weekly-email/report")
+    List<WeeklyDigestReportDto> getWeeklyDigestReports();
 }
