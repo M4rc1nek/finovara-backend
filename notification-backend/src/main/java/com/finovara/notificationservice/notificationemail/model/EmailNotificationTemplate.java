912cc59b29b14a0d03b46b8f9c9b1233289f17b2
Marcin Parśniak <marcinekpar92@op.pl>
2026-08-19 20:51:05 +0200

    define EmailNotificationTemplate interface with subject, template path and opt-in logic

--- notification-backend/src/main/java/com/finovara/notificationservice/notificationemail/model/EmailNotificationTemplate.java
+++ notification-backend/src/main/java/com/finovara/notificationservice/notificationemail/model/EmailNotificationTemplate.java
@@ -1,4 +1,14 @@
 package com.finovara.notificationservice.notificationemail.model;
 
 public interface EmailNotificationTemplate {
+
+    String getSubject();
+
+    String getTemplatePath();
+
+    EmailNotificationCategory getCategory();
+
+    default boolean requiresUserOptIn() {
+        return getCategory() == EmailNotificationCategory.PREFERENCE_BASED;
+    }
 }
