9ccde04917f85ad80dc26d1902d9308b0e5a0f2f
Marcin Parśniak <marcinekpar92@op.pl>
2026-08-19 20:51:11 +0200

    implement scheduled weekly digest report email sending

--- notification-backend/src/main/java/com/finovara/notificationservice/notificationemail/service/digest/report/scheduler/DigestReportEmailScheduler.java
+++ notification-backend/src/main/java/com/finovara/notificationservice/notificationemail/service/digest/report/scheduler/DigestReportEmailScheduler.java
@@ -1,4 +1,21 @@
 package com.finovara.notificationservice.notificationemail.service.digest.report.scheduler;
 
+import com.finovara.notificationservice.notificationemail.service.digest.report.processor.WeeklyDigestReportEmailProcessor;
+import lombok.RequiredArgsConstructor;
+import net.javacrumbs.shedlock.spring.annotation.SchedulerLock;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
 public class DigestReportEmailScheduler {
+
+    private final WeeklyDigestReportEmailProcessor weeklyDigestReportEmailProcessor;
+
+    @Scheduled(cron = "${scheduler.email-notification.weekly-digest-report-send}", zone = "Europe/Warsaw")
+    @SchedulerLock(name = "sendDigestEmailReport", lockAtMostFor = "10m", lockAtLeastFor = "30s")
+    public void sendDigestReportEmail(){
+        weeklyDigestReportEmailProcessor.sendDigestEmailReport();
+    }
+
 }
