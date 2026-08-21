a279fecc4f6752298927ad73b895fc2be2de682b
Marcin Parśniak <marcinekpar92@op.pl>
2026-08-21 18:09:12 +0200

    repurpose feign client to call activity-log service for security digest reports

--- notification-backend/src/main/java/com/finovara/notificationservice/feignclient/ActivityLogBackendClient.java
+++ notification-backend/src/main/java/com/finovara/notificationservice/feignclient/ActivityLogBackendClient.java
@@ -1,14 +1,14 @@
 package com.finovara.notificationservice.feignclient;
 
-import com.finovara.contracts.notification.email.digest.report.finance.WeeklyFinanceDigestReportDto;
+import com.finovara.contracts.notification.email.digest.report.security.WeeklySecurityDigestReportDto;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
 
-@FeignClient(name = "finance-backend", url = "${finance-backend.url}")
-public interface FinanceBackendClient {
+@FeignClient(name = "activity-log-backend", url = "${activity-log-backend.url}")
+public interface ActivityLogBackendClient {
 
-    @GetMapping("/internal/finance/digest/weekly-email/report")
-    List<WeeklyFinanceDigestReportDto> getWeeklyFinanceDigestReports();
+    @GetMapping("/internal/activity/security/digest/weekly-email/log-summary")
+    List<WeeklySecurityDigestReportDto> getSecurityDigestReport();
 }
