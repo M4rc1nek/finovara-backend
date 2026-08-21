324ace403bea657c6898b108611760be790a0f10
Marcin Parśniak <marcinekpar92@op.pl>
2026-08-21 18:09:13 +0200

    add getWeeklySecurityDigestReports endpoint to auth feign client

--- notification-backend/src/main/java/com/finovara/notificationservice/feignclient/AuthBackendClient.java
+++ notification-backend/src/main/java/com/finovara/notificationservice/feignclient/AuthBackendClient.java
@@ -2,11 +2,15 @@ package com.finovara.notificationservice.feignclient;
 
 import com.finovara.contracts.authorization.dto.UserDataResponse;
 import com.finovara.contracts.authorization.dto.ConfirmAuthorizationCodeDto;
+import com.finovara.contracts.notification.email.digest.report.finance.WeeklyFinanceDigestReportDto;
+import com.finovara.contracts.notification.email.digest.report.security.WeeklySecurityDigestReportDto;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
+
+import java.util.List;
 
 @FeignClient(name = "auth-backend", url = "${auth-backend.url}")
 public interface AuthBackendClient {
@@ -16,4 +20,7 @@ public interface AuthBackendClient {
 
     @PostMapping("/internal/confirm-authorization-code")
     Void confirmAuthorizationCode(@RequestHeader("X-User-Id") Long userId, @RequestBody ConfirmAuthorizationCodeDto dto);
+
+    @GetMapping("/internal/security/digest/weekly-email/report")
+    List<WeeklySecurityDigestReportDto> getWeeklySecurityDigestReports();
 }
