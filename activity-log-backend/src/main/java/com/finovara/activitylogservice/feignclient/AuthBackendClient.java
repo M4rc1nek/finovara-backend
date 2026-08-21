17d0bb56094074bab0f532fba19ec1cb45528db8
Marcin Parśniak <marcinekpar92@op.pl>
2026-08-21 18:09:01 +0200

    add getAllUserIds endpoint to activity-log auth feign client

--- activity-log-backend/src/main/java/com/finovara/activitylogservice/feignclient/AuthBackendClient.java
+++ activity-log-backend/src/main/java/com/finovara/activitylogservice/feignclient/AuthBackendClient.java
@@ -4,9 +4,14 @@ import com.finovara.contracts.authorization.dto.ConfirmPasswordDto;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @FeignClient(name = "auth-backend", url = "${auth-backend.url}")
 public interface AuthBackendClient {
 
     @PostMapping("/internal/verify-password")
     Void verifyPassword(@RequestHeader("X-User-Id") Long userId, @RequestBody ConfirmPasswordDto dto);
+
+    @GetMapping("/internal/user-ids")
+    List<Long> getAllUserIds();
 }
