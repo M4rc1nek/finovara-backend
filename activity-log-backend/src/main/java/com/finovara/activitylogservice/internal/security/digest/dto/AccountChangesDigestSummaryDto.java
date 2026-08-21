46c5ad64afb1baebde63e9661606502d359a9c0e
Marcin Parśniak <marcinekpar92@op.pl>
2026-08-21 18:09:03 +0200

    add password/email/username/profile-image change fields to AccountChangesDigestSummaryDto

--- activity-log-backend/src/main/java/com/finovara/activitylogservice/internal/security/digest/dto/AccountChangesDigestSummaryDto.java
+++ activity-log-backend/src/main/java/com/finovara/activitylogservice/internal/security/digest/dto/AccountChangesDigestSummaryDto.java
@@ -1,4 +1,17 @@
 package com.finovara.activitylogservice.internal.security.digest.dto;
 
-public record AccountChangesDigestSummaryDto() {
+import java.time.LocalDateTime;
+
+public record AccountChangesDigestSummaryDto(
+        long passwordChanges,
+        LocalDateTime lastPasswordChangeDate,
+        long emailChanges,
+        LocalDateTime lastEmailChangeDate,
+        long usernameChanges,
+        LocalDateTime lastUsernameChangeDate,
+        long profileImageChanges,
+        LocalDateTime lastProfileImageChangeDate,
+        long profileImageDeleted,
+        LocalDateTime lastProfileImageDeleteDate
+) {
 }
