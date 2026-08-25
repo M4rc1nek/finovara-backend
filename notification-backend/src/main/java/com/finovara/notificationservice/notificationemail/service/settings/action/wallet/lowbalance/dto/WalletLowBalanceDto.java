0bda5af1c8296ecf6c1858a3260c4e4770eddf80
Marcin Parśniak <marcinekpar92@op.pl>
2026-08-25 21:35:38 +0200

    add enabled, authorizationCode and amountThreshold fields to WalletLowBalanceDto

--- notification-backend/src/main/java/com/finovara/notificationservice/notificationemail/service/settings/action/wallet/lowbalance/dto/WalletLowBalanceDto.java
+++ notification-backend/src/main/java/com/finovara/notificationservice/notificationemail/service/settings/action/wallet/lowbalance/dto/WalletLowBalanceDto.java
@@ -1,4 +1,15 @@
 package com.finovara.notificationservice.notificationemail.service.settings.action.wallet.lowbalance.dto;
 
-public record WalletLowBalanceDto() {
+import com.finovara.notificationservice.notificationemail.model.EmailNotificationSettingRequest;
+import jakarta.validation.constraints.DecimalMax;
+import jakarta.validation.constraints.DecimalMin;
+
+import java.math.BigDecimal;
+
+public record WalletLowBalanceDto(
+        Boolean enabled,
+        String authorizationCode,
+        @DecimalMin("1") @DecimalMax("999999")
+        BigDecimal amountThreshold
+) implements EmailNotificationSettingRequest {
 }
