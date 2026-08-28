4d69abc745dbe23d29f660089daf938af15efe68
Marcin Parśniak <marcinekpar92@op.pl>
2026-08-28 13:20:22 +0200

    register recurring execution skipped and piggy bank reached notification dtos

--- notification-backend/src/main/java/com/finovara/notificationservice/notification/dto/NotificationResponse.java
+++ notification-backend/src/main/java/com/finovara/notificationservice/notification/dto/NotificationResponse.java
@@ -5,7 +5,9 @@ import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import com.finovara.contracts.model.NotificationType;
 import com.finovara.notificationservice.notification.dto.limit.LimitExceededDto;
 import com.finovara.notificationservice.notification.dto.limit.LimitWarningDto;
+import com.finovara.notificationservice.notification.dto.piggybank.PiggyBankReachedDto;
 import com.finovara.notificationservice.notification.dto.piggybank.PiggyBankWarningDto;
+import com.finovara.notificationservice.notification.dto.recurring.RecurringExecutionSkippedDto;
 import com.finovara.notificationservice.notification.dto.sharedaccount.deletion.SharedAccountDeletedDto;
 import com.finovara.notificationservice.notification.dto.sharedaccount.deletion.SharedAccountLeftDto;
 import com.finovara.notificationservice.notification.dto.sharedaccount.invitation.SharedAccountInvitationExpiredDto;
@@ -20,13 +22,14 @@ import java.time.LocalDateTime;
         @JsonSubTypes.Type(value = LimitWarningDto.class, name = "LIMIT_EXCEEDED_WARNING"),
         @JsonSubTypes.Type(value = LimitExceededDto.class, name = "LIMIT_EXCEEDED"),
         @JsonSubTypes.Type(value = PiggyBankWarningDto.class, name = "PIGGY_BANK_GOAL_APPROACHING"),
-        @JsonSubTypes.Type(value = PiggyBankWarningDto.class, name = "PIGGY_BANK_GOAL_REACHED"),
+        @JsonSubTypes.Type(value = PiggyBankReachedDto.class, name = "PIGGY_BANK_GOAL_REACHED"),
         @JsonSubTypes.Type(value = UserAcceptSharedAccountInvitationDto.class, name = "USER_ACCEPT_SHARED_ACCOUNT_INVITATION"),
         @JsonSubTypes.Type(value = UserRejectSharedAccountInvitationDto.class, name = "USER_REJECT_SHARED_ACCOUNT_INVITATION"),
         @JsonSubTypes.Type(value = UserSentSharedAccountInvitationDto.class, name = "USER_SENT_SHARED_ACCOUNT_INVITATION"),
         @JsonSubTypes.Type(value = SharedAccountDeletedDto.class, name = "SHARED_ACCOUNT_DELETION"),
         @JsonSubTypes.Type(value = SharedAccountLeftDto.class, name = "SHARED_ACCOUNT_LEFT"),
-        @JsonSubTypes.Type(value = SharedAccountInvitationExpiredDto.class, name = "SHARED_ACCOUNT_INVITATION_EXPIRED")
+        @JsonSubTypes.Type(value = SharedAccountInvitationExpiredDto.class, name = "SHARED_ACCOUNT_INVITATION_EXPIRED"),
+        @JsonSubTypes.Type(value = RecurringExecutionSkippedDto.class, name = "RECURRING_EXECUTION_SKIPPED")
 })
 public interface NotificationResponse {
     NotificationType type();
