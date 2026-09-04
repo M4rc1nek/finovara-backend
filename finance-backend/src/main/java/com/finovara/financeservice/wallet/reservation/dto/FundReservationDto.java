bf2ab551d8c5667984d7e6aac3a2f1e420b65866
Marcin Parśniak <marcinekpar92@op.pl>
2026-09-04 18:21:45 +0200

    add fields to FundReservationDto record

--- finance-backend/src/main/java/com/finovara/financeservice/wallet/reservation/dto/FundReservationDto.java
+++ finance-backend/src/main/java/com/finovara/financeservice/wallet/reservation/dto/FundReservationDto.java
@@ -1,4 +1,12 @@
 package com.finovara.financeservice.wallet.reservation.dto;
 
-public class FundReservationDto {
+import com.finovara.contracts.model.transaction.ExpenseCategory;
+
+import java.math.BigDecimal;
+
+public record FundReservationDto(
+        Long id,
+        ExpenseCategory category,
+        BigDecimal amount
+) {
 }
