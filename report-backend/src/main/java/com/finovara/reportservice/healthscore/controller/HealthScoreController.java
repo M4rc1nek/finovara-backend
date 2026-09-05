4be8bf6d30ba0fa54f83d36c22ca95fdacd1bb96
Marcin Parśniak <marcinekpar92@op.pl>
2026-09-05 14:33:07 +0200

    fix typo in HealthScoreController class name

--- report-backend/src/main/java/com/finovara/reportservice/healthscore/controller/HealthScoreController.java
+++ report-backend/src/main/java/com/finovara/reportservice/healthscore/controller/HealthScoreController.java
@@ -1,4 +1,22 @@
 package com.finovara.reportservice.healthscore.controller;
 
-public class HealthScoreContoller {
+import com.finovara.reportservice.healthscore.dto.HealthScoreDto;
+import com.finovara.reportservice.healthscore.service.HealthScoreService;
+import com.finovara.reportservice.security.SecurityUtils;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/finance-health-score")
+public class HealthScoreController {
+    private final HealthScoreService healthScoreService;
+
+    @GetMapping
+    public ResponseEntity<HealthScoreDto> getHealthScore() {
+        return ResponseEntity.ok(healthScoreService.getHealthScore(SecurityUtils.getCurrentUserId()));
+    }
 }
