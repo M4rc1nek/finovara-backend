978bf9782ad71eb69ef65a65a12001b9fabf5c54
Marcin Parśniak <marcinekpar92@op.pl>
2026-09-08 10:01:25 +0200

    add GET endpoint for expense suggestion at /api/suggestion/expense

--- finance-backend/src/main/java/com/finovara/financeservice/expense/suggestions/controller/ExpenseSuggestionController.java
+++ finance-backend/src/main/java/com/finovara/financeservice/expense/suggestions/controller/ExpenseSuggestionController.java
@@ -1,4 +1,25 @@
 package com.finovara.financeservice.expense.suggestions.controller;
 
+import com.finovara.financeservice.expense.suggestions.dto.ExpenseSuggestionDto;
+import com.finovara.financeservice.expense.suggestions.service.ExpenseSuggestionService;
+import com.finovara.financeservice.security.SecurityUtils;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/api/suggestion/expense")
+@RequiredArgsConstructor
 public class ExpenseSuggestionController {
+
+    private final ExpenseSuggestionService expenseSuggestionService;
+
+    @GetMapping
+    public ResponseEntity<ExpenseSuggestionDto> getSuggestion() {
+        return ResponseEntity.ok(expenseSuggestionService.getExpenseSuggestion(SecurityUtils.getCurrentUserId()));
+    }
+
 }
