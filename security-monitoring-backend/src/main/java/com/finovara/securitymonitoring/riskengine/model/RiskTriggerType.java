9b1ab60b04d43c9d9a9cdc69fadba00d0c029439
Marcin Parśniak <marcinekpar92@op.pl>
2026-09-13 18:22:25 +0200

    add enum values for risk engine trigger types

--- security-monitoring-backend/src/main/java/com/finovara/securitymonitoring/riskengine/model/RiskTriggerType.java
+++ security-monitoring-backend/src/main/java/com/finovara/securitymonitoring/riskengine/model/RiskTriggerType.java
@@ -1,4 +1,11 @@
 package com.finovara.securitymonitoring.riskengine.model;
 
 public enum RiskTriggerType {
+    LOGIN,
+    EXPENSE,
+    REVENUE,
+    PASSWORD_CHANGED,
+    EMAIL_CHANGED,
+    USERNAME_CHANGED,
+    PROFILE_IMAGE_CHANGED
 }
