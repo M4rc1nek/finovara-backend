057f37bd2827e1d11c6cbef4a434dae04d8e79b7
Marcin Parśniak <marcinekpar92@op.pl>
2026-09-13 18:22:24 +0200

    add enum values for risk engine actions

--- security-monitoring-backend/src/main/java/com/finovara/securitymonitoring/riskengine/model/RiskAction.java
+++ security-monitoring-backend/src/main/java/com/finovara/securitymonitoring/riskengine/model/RiskAction.java
@@ -1,4 +1,8 @@
 package com.finovara.securitymonitoring.riskengine.model;
 
-public class RiskAction {
+public enum RiskAction {
+    LOG_ONLY,
+    SOFT_CHALLENGE,
+    AUTHORIZATION_REQUIRED,
+    BLOCK_AND_REVIEW
 }
