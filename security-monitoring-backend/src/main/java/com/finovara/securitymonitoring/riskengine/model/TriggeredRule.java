d5c5f970ed60755e1e8b0f38efa3738e0db5f493
Marcin Parśniak <marcinekpar92@op.pl>
2026-09-13 18:22:25 +0200

    add JPA entity fields and mapping for TriggeredRule

--- security-monitoring-backend/src/main/java/com/finovara/securitymonitoring/riskengine/model/TriggeredRule.java
+++ security-monitoring-backend/src/main/java/com/finovara/securitymonitoring/riskengine/model/TriggeredRule.java
@@ -1,4 +1,29 @@
 package com.finovara.securitymonitoring.riskengine.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "triggered_rule")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class TriggeredRule {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String ruleCode;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "risk_operation_id", nullable = false)
+    private RiskOperation riskOperation;
 }
