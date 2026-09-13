fffa4251c9593855dbb8a43c203e75d2fa624971
Marcin Parśniak <marcinekpar92@op.pl>
2026-09-13 18:22:25 +0200

    fix move CustomUserDetails to securitymonitoring.security package

--- security-monitoring-backend/src/main/java/com/finovara/securitymonitoring/security/CustomUserDetails.java
+++ security-monitoring-backend/src/main/java/com/finovara/securitymonitoring/security/CustomUserDetails.java
@@ -1,4 +1,4 @@
-package com.finovara.activitylogservice.security;
+package com.finovara.securitymonitoring.security;
 
 import lombok.Getter;
 import org.springframework.security.core.GrantedAuthority;
