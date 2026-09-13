3b9c82fb76328f99272a465f44d9864b8e2d5a76
Marcin Parśniak <marcinekpar92@op.pl>
2026-09-13 18:22:17 +0200

    update settings activity kafka topic to settings.changed in NotifyEmailChangeServiceAction

--- notification-backend/src/main/java/com/finovara/notificationservice/notificationemail/service/settings/action/emailchange/service/NotifyEmailChangeServiceAction.java
+++ notification-backend/src/main/java/com/finovara/notificationservice/notificationemail/service/settings/action/emailchange/service/NotifyEmailChangeServiceAction.java
@@ -40,7 +40,7 @@ public class NotifyEmailChangeServiceAction
 
     @Override
     protected void handleActivity(Long userId, boolean enabled) {
-        kafkaTemplate.send("activity.settings", new SettingsActivityEvent(userId, SettingType.NOTIFICATION_EMAIL_CHANGED,
+        kafkaTemplate.send("settings.changed", new SettingsActivityEvent(userId, SettingType.NOTIFICATION_EMAIL_CHANGED,
                 enabled ? SettingActivityStatus.ENABLED : SettingActivityStatus.DISABLED, LocalDateTime.now()));
     }
 }
