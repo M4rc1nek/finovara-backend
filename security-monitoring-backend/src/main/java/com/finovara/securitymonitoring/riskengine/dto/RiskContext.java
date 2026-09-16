b2f50ae6f49b4739b8198860038ab85a9b9dc57b
Marcin Parśniak <marcinekpar92@op.pl>
2026-09-16 19:03:07 +0200

    move RiskContext to dto package and change category type from Object to String

--- security-monitoring-backend/src/main/java/com/finovara/securitymonitoring/riskengine/dto/RiskContext.java
+++ security-monitoring-backend/src/main/java/com/finovara/securitymonitoring/riskengine/dto/RiskContext.java
@@ -1,15 +1,17 @@
-package com.finovara.securitymonitoring.riskengine.model;
+package com.finovara.securitymonitoring.riskengine.dto;
 
 import com.finovara.securitymonitoring.accountchange.model.AccountChangeProfile;
 import com.finovara.securitymonitoring.login.model.LoginProfile;
+import com.finovara.securitymonitoring.riskengine.model.RiskTriggerType;
 import com.finovara.securitymonitoring.transaction.model.TransactionProfile;
 
 import java.math.BigDecimal;
+
 public record RiskContext(
         Long userId,
         RiskTriggerType triggerType,
         BigDecimal amount,
-        Object category,
+        String category,
         String ipAddress,
         String location,
         String browser,
