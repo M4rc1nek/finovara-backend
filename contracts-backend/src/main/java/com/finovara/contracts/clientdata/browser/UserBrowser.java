bc98f0476a85501e68b099b8f9eacbad4cb63df7
Marcin Parśniak <marcinekpar92@op.pl>
2026-09-21 14:31:07 +0200

    remove unused getBrowsers method from UserBrowser

--- contracts-backend/src/main/java/com/finovara/contracts/clientdata/browser/UserBrowser.java
+++ contracts-backend/src/main/java/com/finovara/contracts/clientdata/browser/UserBrowser.java
@@ -5,8 +5,6 @@ import lombok.experimental.UtilityClass;
 import ua_parser.Client;
 import ua_parser.Parser;
 
-import java.util.List;
-
 @UtilityClass
 public class UserBrowser {
 
@@ -21,9 +19,5 @@ public class UserBrowser {
 
         Client client = parser.parse(userAgent);
         return client.userAgent.family;
-    }
-
-    public static List<String> getBrowsers(HttpServletRequest request) {
-        return List.of(getBrowser(request));
     }
 }
