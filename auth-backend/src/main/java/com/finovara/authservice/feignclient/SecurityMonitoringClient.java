8ce8bad22860d6a3a2c2dbc92f6e312b713e13bc
Marcin Parśniak <marcinekpar92@op.pl>
2026-09-21 14:31:11 +0200

    add evaluate endpoint call to auth SecurityMonitoringClient

--- auth-backend/src/main/java/com/finovara/authservice/feignclient/SecurityMonitoringClient.java
+++ auth-backend/src/main/java/com/finovara/authservice/feignclient/SecurityMonitoringClient.java
@@ -1,4 +1,14 @@
 package com.finovara.authservice.feignclient;
 
+import com.finovara.contracts.securitymonitoring.dto.RiskEvaluationRequest;
+import com.finovara.contracts.securitymonitoring.dto.RiskEvaluationResponse;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@FeignClient(name = "security-monitoring-backend", url = "${security-monitoring-backend.url}")
 public interface SecurityMonitoringClient {
+
+    @PostMapping("/internal/risk/evaluate")
+    RiskEvaluationResponse evaluate(@RequestBody RiskEvaluationRequest request);
 }
