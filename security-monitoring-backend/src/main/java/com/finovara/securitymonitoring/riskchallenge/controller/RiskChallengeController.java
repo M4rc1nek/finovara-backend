c5a4247d9a9e75f75da276b36065a9ebc4b3747a
Marcin Parśniak <marcinekpar92@op.pl>
2026-09-21 14:31:08 +0200

    update RiskChallengeController mapping from /internal/risk to /api/security-monitoring/

--- security-monitoring-backend/src/main/java/com/finovara/securitymonitoring/riskchallenge/controller/RiskChallengeController.java
+++ security-monitoring-backend/src/main/java/com/finovara/securitymonitoring/riskchallenge/controller/RiskChallengeController.java
@@ -8,7 +8,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/internal/risk")
+@RequestMapping("/api/security-monitoring/")
 @RequiredArgsConstructor
 public class RiskChallengeController {
 
