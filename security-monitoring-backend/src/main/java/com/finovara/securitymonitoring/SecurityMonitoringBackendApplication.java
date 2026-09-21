99f0e50875c00a480677e9bf41ea80f25d89be40
Marcin Parśniak <marcinekpar92@op.pl>
2026-09-21 14:31:08 +0200

    add @EnableAsync and @EnableFeignClients to SecurityMonitoringBackendApplication

--- security-monitoring-backend/src/main/java/com/finovara/securitymonitoring/SecurityMonitoringBackendApplication.java
+++ security-monitoring-backend/src/main/java/com/finovara/securitymonitoring/SecurityMonitoringBackendApplication.java
@@ -2,9 +2,13 @@ package com.finovara.securitymonitoring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 import java.util.TimeZone;
 
+@EnableAsync
+@EnableFeignClients
 @SpringBootApplication
 public class SecurityMonitoringBackendApplication {
 
