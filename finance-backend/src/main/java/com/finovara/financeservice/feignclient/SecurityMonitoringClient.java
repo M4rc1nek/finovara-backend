0be8b096613f3fc63d56630f2a18c3f49b379f9c
Marcin Parśniak <marcinekpar92@op.pl>
2026-09-21 14:31:16 +0200

    add url property to finance SecurityMonitoringClient feign client

--- finance-backend/src/main/java/com/finovara/financeservice/feignclient/SecurityMonitoringClient.java
+++ finance-backend/src/main/java/com/finovara/financeservice/feignclient/SecurityMonitoringClient.java
@@ -6,7 +6,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name = "security-monitoring-backend")
+@FeignClient(name = "security-monitoring-backend", url = "${security-monitoring-backend.url}")
 public interface SecurityMonitoringClient {
 
     @PostMapping("/internal/risk/evaluate")
