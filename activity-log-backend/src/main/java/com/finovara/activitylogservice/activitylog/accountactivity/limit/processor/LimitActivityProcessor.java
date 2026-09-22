f8f6e397def76a69fed90445ef492cb8faa32b3c
Marcin Parśniak <marcinekpar92@op.pl>
2026-09-22 18:06:08 +0200

    make limit activity deletion transactional and add logging

--- activity-log-backend/src/main/java/com/finovara/activitylogservice/activitylog/accountactivity/limit/processor/LimitActivityProcessor.java
+++ activity-log-backend/src/main/java/com/finovara/activitylogservice/activitylog/accountactivity/limit/processor/LimitActivityProcessor.java
@@ -2,16 +2,21 @@ package com.finovara.activitylogservice.activitylog.accountactivity.limit.proces
 
 import com.finovara.activitylogservice.activitylog.accountactivity.limit.repository.LimitActivityRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 @Component
+@Slf4j
 @RequiredArgsConstructor
 public class LimitActivityProcessor {
 
     private final LimitActivityRepository limitActivityRepository;
 
+    @Transactional
     public void deleteLimitActivity(){
         limitActivityRepository.deleteAllInBatch();
+        log.info("Limit Activity has been deleted.");
     }
 
 }
