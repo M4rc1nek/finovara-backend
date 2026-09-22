2a4c4bb6164d2e3817287148377834d987b176da
Marcin Parśniak <marcinekpar92@op.pl>
2026-09-22 18:06:09 +0200

    make shared account activity deletion transactional and add logging

--- activity-log-backend/src/main/java/com/finovara/activitylogservice/activitylog/accountactivity/sharedaccount/processor/SharedAccountActivityProcessor.java
+++ activity-log-backend/src/main/java/com/finovara/activitylogservice/activitylog/accountactivity/sharedaccount/processor/SharedAccountActivityProcessor.java
@@ -2,15 +2,20 @@ package com.finovara.activitylogservice.activitylog.accountactivity.sharedaccoun
 
 import com.finovara.activitylogservice.activitylog.accountactivity.sharedaccount.repository.SharedAccountActivityRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 @Component
+@Slf4j
 @RequiredArgsConstructor
 public class SharedAccountActivityProcessor {
 
     private final SharedAccountActivityRepository sharedAccountActivityRepository;
 
-    public void deleteSharedAccountActivity(){
+    @Transactional
+    public void deleteSharedAccountActivity() {
         sharedAccountActivityRepository.deleteAllInBatch();
+        log.info("Shared account activity has been deleted.");
     }
 }
