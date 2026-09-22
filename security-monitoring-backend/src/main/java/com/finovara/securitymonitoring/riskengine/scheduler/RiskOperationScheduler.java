10769984dfa3d17d8acf477a9a5bf4c664cb3a13
Marcin Parśniak <marcinekpar92@op.pl>
2026-09-22 18:06:13 +0200

    add scheduled job to delete old risk operation data

--- security-monitoring-backend/src/main/java/com/finovara/securitymonitoring/riskengine/scheduler/RiskOperationScheduler.java
+++ security-monitoring-backend/src/main/java/com/finovara/securitymonitoring/riskengine/scheduler/RiskOperationScheduler.java
@@ -1,4 +1,19 @@
 package com.finovara.securitymonitoring.riskengine.scheduler;
 
-public class RiskEngineScheduler {
+import com.finovara.securitymonitoring.riskengine.processor.RiskOperationProcessor;
+import lombok.RequiredArgsConstructor;
+import net.javacrumbs.shedlock.spring.annotation.SchedulerLock;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class RiskOperationScheduler {
+    private  final RiskOperationProcessor riskOperationProcessor;
+
+    @Scheduled(cron = "${scheduler.risk-operation.delete-cron}", zone = "Europe/Warsaw")
+    @SchedulerLock(name = "deleteRiskOperation", lockAtMostFor = "10m", lockAtLeastFor = "30s")
+    public void deleteAllRiskOperationData() {
+        riskOperationProcessor.deleteAllRiskOperationData();
+    }
 }
