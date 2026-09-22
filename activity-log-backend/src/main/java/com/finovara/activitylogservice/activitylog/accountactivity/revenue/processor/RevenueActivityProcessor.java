bd6f17cd05cb4188736f7e5991dc290adb02e8cc
Marcin Parśniak <marcinekpar92@op.pl>
2026-09-22 18:06:08 +0200

    make revenue activity deletion transactional and add logging

--- activity-log-backend/src/main/java/com/finovara/activitylogservice/activitylog/accountactivity/revenue/processor/RevenueActivityProcessor.java
+++ activity-log-backend/src/main/java/com/finovara/activitylogservice/activitylog/accountactivity/revenue/processor/RevenueActivityProcessor.java
@@ -2,15 +2,20 @@ package com.finovara.activitylogservice.activitylog.accountactivity.revenue.proc
 
 import com.finovara.activitylogservice.activitylog.accountactivity.revenue.repository.RevenueActivityRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 @Component
+@Slf4j
 @RequiredArgsConstructor
 public class RevenueActivityProcessor {
 
     private final RevenueActivityRepository revenueActivityRepository;
 
+    @Transactional
     public void deleteRevenueActivity(){
         revenueActivityRepository.deleteAllInBatch();
+        log.info("Revenue Activity has been deleted.");
     }
 }
