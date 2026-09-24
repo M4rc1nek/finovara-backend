23cd010cfd423c2ca1fcf828b78232d601ffb385
Marcin Parśniak <marcinekpar92@op.pl>
2026-09-24 08:48:10 +0200

    add authorizationCode field to FundReservationDto

--- finance-backend/src/main/java/com/finovara/financeservice/wallet/reservation/dto/FundReservationDto.java
+++ finance-backend/src/main/java/com/finovara/financeservice/wallet/reservation/dto/FundReservationDto.java
@@ -7,6 +7,7 @@ import java.math.BigDecimal;
 public record FundReservationDto(
         Long id,
         ExpenseCategory category,
-        BigDecimal amount
+        BigDecimal amount,
+        String authorizationCode
 ) {
 }
