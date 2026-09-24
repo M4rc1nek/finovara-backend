efcfc9f62f1838e840f3d1fa05df61cc5161141d
Marcin Parśniak <marcinekpar92@op.pl>
2026-09-24 15:02:50 +0200

    implement mapping from RiskOperationActivity to dto

--- activity-log-backend/src/main/java/com/finovara/activitylogservice/activitylog/securitymonitoring/mapper/RiskOperationActivityMapper.java
+++ activity-log-backend/src/main/java/com/finovara/activitylogservice/activitylog/securitymonitoring/mapper/RiskOperationActivityMapper.java
@@ -1,4 +1,23 @@
 package com.finovara.activitylogservice.activitylog.securitymonitoring.mapper;
 
+import com.finovara.activitylogservice.activitylog.securitymonitoring.dto.RiskOperationActivityDto;
+import com.finovara.activitylogservice.activitylog.securitymonitoring.model.RiskOperationActivity;
+import com.finovara.activitylogservice.activitylog.securitymonitoring.model.RiskRuleCollectionActivity;
+import org.springframework.stereotype.Component;
+
+@Component
 public class RiskOperationActivityMapper {
+
+    public RiskOperationActivityDto mapToRiskOperationActivity(RiskOperationActivity activity) {
+        return new RiskOperationActivityDto(
+                activity.getSourceEventId(),
+                activity.getTriggerType(),
+                activity.getScore(),
+                activity.getAction(),
+                activity.getRiskRuleCollectionActivities().stream()
+                        .map(RiskRuleCollectionActivity::getRiskRule)
+                        .toList(),
+                activity.getCreatedAt(),
+                activity.getOperationDate());
+    }
 }
