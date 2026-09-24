4108677288af23468e1c7d65f6f430b02d66eec9
Marcin Parśniak <marcinekpar92@op.pl>
2026-09-24 15:02:51 +0200

    implement transactional deletion of risk operation activity

--- activity-log-backend/src/main/java/com/finovara/activitylogservice/activitylog/securitymonitoring/processor/RiskOperationActivityProcessor.java
+++ activity-log-backend/src/main/java/com/finovara/activitylogservice/activitylog/securitymonitoring/processor/RiskOperationActivityProcessor.java
@@ -1,4 +1,21 @@
 package com.finovara.activitylogservice.activitylog.securitymonitoring.processor;
 
+import com.finovara.activitylogservice.activitylog.securitymonitoring.repository.RiskOperationActivityRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+@Component
+@Slf4j
+@RequiredArgsConstructor
 public class RiskOperationActivityProcessor {
+
+    private final RiskOperationActivityRepository riskOperationActivityRepository;
+
+    @Transactional
+    public void deleteRiskOperationActivity() {
+        riskOperationActivityRepository.deleteAllInBatch();
+        log.info("Risk operation activity has been deleted.");
+    }
 }
