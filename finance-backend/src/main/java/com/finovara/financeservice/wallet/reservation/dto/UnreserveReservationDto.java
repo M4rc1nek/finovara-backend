3037c7e4aca9ad01861f99a033b5ddcf343b2fce
Marcin Parśniak <marcinekpar92@op.pl>
2026-09-24 08:48:10 +0200

    add authorizationCode field to UnreserveReservationDto

--- finance-backend/src/main/java/com/finovara/financeservice/wallet/reservation/dto/UnreserveReservationDto.java
+++ finance-backend/src/main/java/com/finovara/financeservice/wallet/reservation/dto/UnreserveReservationDto.java
@@ -3,6 +3,7 @@ package com.finovara.financeservice.wallet.reservation.dto;
 import java.math.BigDecimal;
 
 public record UnreserveReservationDto(
-        BigDecimal amount
+        BigDecimal amount,
+        String authorizationCode
 ) {
 }
