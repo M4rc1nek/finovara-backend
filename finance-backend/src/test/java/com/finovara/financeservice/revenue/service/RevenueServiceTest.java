74b4d40ea35d54d6e725dd0b9309961e3530ca01
Marcin Parśniak <marcinekpar92@op.pl>
2026-09-24 15:03:01 +0200

    update RiskTriggerType import in RevenueServiceTest

--- finance-backend/src/test/java/com/finovara/financeservice/revenue/service/RevenueServiceTest.java
+++ finance-backend/src/test/java/com/finovara/financeservice/revenue/service/RevenueServiceTest.java
@@ -7,7 +7,7 @@ import com.finovara.contracts.model.activity.RevenueActivityType;
 import com.finovara.contracts.exception.notfound.RequestedEntityNotFoundException;
 import com.finovara.contracts.model.transaction.RevenueCategory;
 import com.finovara.contracts.outbox.OutboxService;
-import com.finovara.contracts.securitymonitoring.dto.RiskTriggerType;
+import com.finovara.contracts.securitymonitoring.model.RiskTriggerType;
 import com.finovara.financeservice.revenue.dto.RevenueDto;
 import com.finovara.financeservice.revenue.mapper.RevenueMapper;
 import com.finovara.financeservice.revenue.model.Revenue;
