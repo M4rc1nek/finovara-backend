391bf678445b41acdea96da0228f4b57b0c71d49
Marcin Parśniak <marcinekpar92@op.pl>
2026-09-24 15:03:00 +0200

    update RiskAction import to securitymonitoring.model package

--- finance-backend/src/main/java/com/finovara/financeservice/exception/riskverification/RiskVerificationRequiredResponse.java
+++ finance-backend/src/main/java/com/finovara/financeservice/exception/riskverification/RiskVerificationRequiredResponse.java
@@ -1,6 +1,6 @@
 package com.finovara.financeservice.exception.riskverification;
 
-import com.finovara.contracts.securitymonitoring.dto.RiskAction;
+import com.finovara.contracts.securitymonitoring.model.RiskAction;
 
 public record RiskVerificationRequiredResponse(
         Long riskOperationId,
