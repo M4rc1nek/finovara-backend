281a1ab74712affe9ab590138ae26bfa5f716c30
Marcin Parśniak <marcinekpar92@op.pl>
2026-09-24 15:02:50 +0200

    add fields to RiskOperationActivityDto

--- activity-log-backend/src/main/java/com/finovara/activitylogservice/activitylog/securitymonitoring/dto/RiskOperationActivityDto.java
+++ activity-log-backend/src/main/java/com/finovara/activitylogservice/activitylog/securitymonitoring/dto/RiskOperationActivityDto.java
@@ -1,4 +1,20 @@
 package com.finovara.activitylogservice.activitylog.securitymonitoring.dto;
 
-public record RiskOperationActivityDto() {
+import com.finovara.contracts.securitymonitoring.model.RiskAction;
+import com.finovara.contracts.securitymonitoring.model.RiskRule;
+import com.finovara.contracts.securitymonitoring.model.RiskTriggerType;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+
+public record RiskOperationActivityDto(
+        String sourceEventId,
+        RiskTriggerType triggerType,
+        int score,
+        RiskAction action,
+        List<RiskRule> riskRuleList,
+        LocalDateTime createdAt,
+        LocalDate operationDate
+) {
 }
